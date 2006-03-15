c45cf2b9d7ea49731bfb8c9cb80730424521ea78
drummond <drummond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-03-15 15:24:59 +0000

    fix for  0000440: Exception when export to OWL
    Made sure extension of .owl applied if none given

--- src/edu/stanford/smi/protegex/owl/jena/JenaFilePanel.java
+++ src/edu/stanford/smi/protegex/owl/jena/JenaFilePanel.java
@@ -4,6 +4,7 @@ import edu.stanford.smi.protege.util.FileField;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
@@ -15,14 +16,23 @@ public class JenaFilePanel extends JPanel {
 
     public JenaFilePanel() {
         owlFileField = new FileField("OWL file name",
-                null, JenaKnowledgeBaseSourcesEditor.EXTENSION,
-                "Web Ontology Language (OWL) files");
+                                     null, JenaKnowledgeBaseSourcesEditor.EXTENSION,
+                                     "Web Ontology Language (OWL) files");
         setLayout(new BorderLayout(8, 8));
         add(BorderLayout.NORTH, owlFileField);
     }
 
 
     public String getOWLFilePath() {
-        return owlFileField.getPath();
+        String path = owlFileField.getPath();
+
+        // make sure the filename has an extension
+        File file = new File(path);
+        String filename = file.getName();
+        int extIndex = filename.indexOf('.');
+        if (extIndex < 0){
+            path = owlFileField.getPath() + "." + JenaKnowledgeBaseSourcesEditor.EXTENSION;
+        }
+        return path;
     }
 }
