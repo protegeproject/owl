018853a7799ad1229c2b0f2a23794bc770b568e9
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-04-07 05:17:03 +0000

    fixed printStackTraces

--- owl/trunk/src/edu/stanford/smi/protegex/owl/inference/protegeowl/tests/DisjointTests.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/inference/protegeowl/tests/DisjointTests.java
@@ -1,5 +1,8 @@
 package edu.stanford.smi.protegex.owl.inference.protegeowl.tests;
 
+import java.util.logging.Level;
+
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.inference.dig.exception.DIGReasonerException;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.ProtegeOWLReasoner;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.ReasonerManager;
@@ -32,7 +35,7 @@ public class DisjointTests extends AbstractJenaTestCase {
         }
         catch (DIGReasonerException e) {
             fail(e.getMessage());
-            e.printStackTrace();
+            Log.getLogger().log(Level.SEVERE, "Exception caught", e);
         }
     }
 }
