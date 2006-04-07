039df117948034cd38c9e1e7f1e4aaf699aa456e
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-04-07 05:52:42 +0000

    unreachable code

--- owl/trunk/src/edu/stanford/smi/protegex/owl/ui/triplestore/CreateTripleStorePanel.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/ui/triplestore/CreateTripleStorePanel.java
@@ -15,44 +15,11 @@ public abstract class CreateTripleStorePanel extends AddTripleStorePanel {
 
 
     protected TripleStore performAction() {
-//        URI uri = getURI();
-//        URL url = null;
-//        try {
-//            url = file.toURL();
-//            String prefix = getPrefix();
-//            String namespace = Jena.getNamespaceFromURI(uri.toString());
-//            OWLModel owlModel = getOWLModel();
-//            owlModel.getNamespaceManager().setPrefix(namespace, prefix);
-//            final TripleStoreModel tsm = owlModel.getTripleStoreModel();
-//            TripleStore tripleStore = tsm.createTripleStore(uri.toString());
-//            tsm.setActiveTripleStore(tripleStore);
-//            OWLOntology owlOntology = owlModel.createOWLOntology(prefix);
-//            RDFProperty property = owlModel.getRDFProperty(OWLNames.Slot.ONTOLOGY_PREFIXES);
-//            owlOntology.addPropertyValue(property, ":" + namespace);
-//            URIResolver uriResolver = owlModel.getURIResolver();
-//            uriResolver.setPhysicalURL(uri, url);
-//            tsm.setActiveTripleStore(tsm.getTopTripleStore());
-//            owlModel.getDefaultOWLOntology().addImports(uri.toString());
-//            return tripleStore;
-//        }
-//        catch (Exception ex) {
-//            ex.printStackTrace();
-//            return null;
-//        }
         return null;
     }
 
 
     public boolean validateContents() {
-//        if (super.validateContents()) {
-//            File file = getFile();
-//            if (file != null) {
-//                return true;
-//            }
-//            else {
-//                OWLUI.showErrorMessageDialog("Invalid file.");
-//            }
-//        }
         return false;
     }
 }
