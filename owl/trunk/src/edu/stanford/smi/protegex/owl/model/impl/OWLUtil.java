2550e413d2c73f69ed6fec6b14324e9ef001f23a
drummond <drummond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-04-11 13:30:42 +0000

    Fix for exceptions when imports are cancelled (as an unresolved import's java class is not OWLOntology) Allow for imports to be removed or corrected (external resources have a browser as their default form)

--- owl/trunk/src/edu/stanford/smi/protegex/owl/model/impl/OWLUtil.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/model/impl/OWLUtil.java
@@ -1,49 +1,12 @@
 package edu.stanford.smi.protegex.owl.model.impl;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.logging.Level;
-
-import edu.stanford.smi.protege.model.Cls;
-import edu.stanford.smi.protege.model.Frame;
-import edu.stanford.smi.protege.model.Instance;
-import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protege.model.Model;
-import edu.stanford.smi.protege.model.Project;
-import edu.stanford.smi.protege.model.Reference;
-import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.*;
 import edu.stanford.smi.protege.model.framestore.MergingNarrowFrameStore;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protege.ui.FrameComparator;
 import edu.stanford.smi.protege.ui.ProjectManager;
 import edu.stanford.smi.protege.util.CollectionUtilities;
-import edu.stanford.smi.protege.util.Log;
-import edu.stanford.smi.protegex.owl.model.OWLAnonymousClass;
-import edu.stanford.smi.protegex.owl.model.OWLEnumeratedClass;
-import edu.stanford.smi.protegex.owl.model.OWLIntersectionClass;
-import edu.stanford.smi.protegex.owl.model.OWLModel;
-import edu.stanford.smi.protegex.owl.model.OWLNAryLogicalClass;
-import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
-import edu.stanford.smi.protegex.owl.model.OWLNames;
-import edu.stanford.smi.protegex.owl.model.OWLOntology;
-import edu.stanford.smi.protegex.owl.model.OWLProperty;
-import edu.stanford.smi.protegex.owl.model.OWLRestriction;
-import edu.stanford.smi.protegex.owl.model.ProtegeNames;
-import edu.stanford.smi.protegex.owl.model.RDFList;
-import edu.stanford.smi.protegex.owl.model.RDFObject;
-import edu.stanford.smi.protegex.owl.model.RDFProperty;
-import edu.stanford.smi.protegex.owl.model.RDFResource;
-import edu.stanford.smi.protegex.owl.model.RDFSClass;
-import edu.stanford.smi.protegex.owl.model.RDFSLiteral;
-import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
-import edu.stanford.smi.protegex.owl.model.RDFUntypedResource;
+import edu.stanford.smi.protegex.owl.model.*;
 import edu.stanford.smi.protegex.owl.model.classparser.OWLClassParser;
 import edu.stanford.smi.protegex.owl.model.event.PropertyValueAdapter;
 import edu.stanford.smi.protegex.owl.model.event.PropertyValueListener;
@@ -54,6 +17,9 @@ import edu.stanford.smi.protegex.owl.model.triplestore.TripleStoreModel;
 import edu.stanford.smi.protegex.owl.ui.ProtegeUI;
 import edu.stanford.smi.protegex.owl.ui.profiles.OWLProfiles;
 import edu.stanford.smi.protegex.owl.ui.profiles.ProfilesManager;
+
+import java.net.URI;
+import java.util.*;
 
 /**
  * A collection of static utility methods for OWL classes.
@@ -229,7 +195,7 @@ public class OWLUtil {
         }
         catch (Exception ex) {
             System.err.println("Error in OWLUtil.createClone (" + expression + ")");
-            Log.getLogger().log(Level.SEVERE, "Exception caught", ex);
+            ex.printStackTrace();
             return null;
         }
     }
@@ -459,7 +425,7 @@ public class OWLUtil {
             throw new RuntimeException("Debugging purposes only");
         }
         catch (Exception ex) {
-          Log.getLogger().log(Level.SEVERE, "Exception caught", ex);
+            ex.printStackTrace();
         }
     }
 
@@ -1031,20 +997,30 @@ public class OWLUtil {
     }
 
 
+    /**
+     * Does an ontology import another through the dependency tree
+     *
+     * @param ontA the ontology we are querying
+     * @param ontB the ontology we are looking for
+     * @return true if ontA (indirectly) imports ontB
+     */
     public static boolean indirectlyImports(OWLOntology ontA, OWLOntology ontB) {
         return indirectlyImportsHelper(ontA, ontB, CollectionUtilities.createCollection(ontA));
     }
 
     private static boolean indirectlyImportsHelper(OWLOntology ontA, OWLOntology ontB, Collection accumulator) {
         boolean found = false;
-        for (Iterator<OWLOntology> i = ontA.getImportResources().iterator(); i.hasNext() && !found;) {
-            OWLOntology ontC = i.next();
-            if (ontC == ontB) {
-                found = true;
-            }
-            else if (!accumulator.contains(ontC)) {
-                accumulator.add(ontC);
-                found = indirectlyImportsHelper(ontC, ontB, accumulator);
+        for (Iterator<RDFResource> i = ontA.getImportResources().iterator(); i.hasNext() && !found;) {
+            RDFResource res = i.next();
+            if (res instanceof OWLOntology) {
+                OWLOntology ontC = (OWLOntology) res;
+                if (ontC == ontB) {
+                    found = true;
+                }
+                else if (!accumulator.contains(ontC)) {
+                    accumulator.add(ontC);
+                    found = indirectlyImportsHelper(ontC, ontB, accumulator);
+                }
             }
         }
         return found;
