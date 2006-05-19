dbd179eef6a85cb4597e98418676f8927dc5cb95
Martin O'Connor <martinjoconnor@users.noreply.github.com>
2006-05-19 16:55:24 +0000

    Initial check-in of SWRL/Jess/built-in support.

--- src/edu/stanford/smi/protegex/owl/swrl/ui/table/SWRLTablePanel.java
+++ src/edu/stanford/smi/protegex/owl/swrl/ui/table/SWRLTablePanel.java
@@ -1,5 +1,9 @@
 package edu.stanford.smi.protegex.owl.swrl.ui.table;
 
+import edu.stanford.smi.protegex.owl.swrl.bridge.SWRLRuleEngineManager;
+import edu.stanford.smi.protegex.owl.swrl.bridge.ui.ViewRuleEngineAction;
+import edu.stanford.smi.protegex.owl.swrl.ui.tab.SWRLTab;
+
 import edu.stanford.smi.protege.util.Disposable;
 import edu.stanford.smi.protege.util.LabeledComponent;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
@@ -9,12 +13,11 @@ import edu.stanford.smi.protegex.owl.ui.OWLLabeledComponent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 /**
  * A JPanel consisting of a SWRLTable and buttons to create
  * and delete rules.
- *
- * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class SWRLTablePanel extends JPanel implements Disposable {
 
@@ -25,29 +28,51 @@ public class SWRLTablePanel extends JPanel implements Disposable {
     private SWRLTableModel tableModel;
 
 
-    public SWRLTablePanel(OWLModel owlModel, RDFResource RDFResource) {
+  // Called when the table panel is in a results panel.
+  public SWRLTablePanel(OWLModel owlModel, RDFResource resource) 
+  {
+    initialize(owlModel, resource);
+  } // SWRLTablePanel
 
-        tableModel = RDFResource == null ?
-                new SWRLTableModel(owlModel) :
-                new SWRLTableModel(RDFResource);
-        table = new SWRLTable(tableModel, owlModel);
+  // Called when the table panel is within the SWRLTab.
+  public SWRLTablePanel(OWLModel owlModel, RDFResource resource, SWRLTab swrlTab) 
+  {
+    LabeledComponent lc = initialize(owlModel, resource);
 
-        JScrollPane scrollPane = new JScrollPane(table);
-        JViewport viewPort = scrollPane.getViewport();
-        viewPort.setBackground(table.getBackground());
+    // Iterate through all registered rule engine and add an enable button for each one.
+    Iterator iterator = SWRLRuleEngineManager.getRegisteredRuleEngines().iterator();
+    while (iterator.hasNext()) {
+      SWRLRuleEngineManager.RuleEngineRegistrationInfo info = (SWRLRuleEngineManager.RuleEngineRegistrationInfo)iterator.next();
+      lc.addHeaderButton(new ViewRuleEngineAction(info.getRuleEngineName(), info.getToolTip(), info.getIcon(), swrlTab, owlModel));
+      add(BorderLayout.CENTER, lc);
+    } // while
+  } // SWRLTablePanel
 
-        LabeledComponent lc = new OWLLabeledComponent("SWRL Rules", scrollPane);
-        lc.addHeaderButton(new ViewRuleAction(table));
-        lc.addHeaderButton(new CreateRuleAction(table, owlModel));
-        lc.addHeaderButton(new CloneRuleAction(table, owlModel));
-        lc.addHeaderButton(new DeleteRuleAction(table));
+  public void dispose() {
+    table.dispose();
+  }
 
-        setLayout(new BorderLayout());
-        add(BorderLayout.CENTER, lc);
-    }
+  private LabeledComponent initialize(OWLModel owlModel, RDFResource RDFResource) {
+    
+    tableModel = RDFResource == null ?
+      new SWRLTableModel(owlModel) :
+      new SWRLTableModel(RDFResource);
+    table = new SWRLTable(tableModel, owlModel);
+    
+    JScrollPane scrollPane = new JScrollPane(table);
+    JViewport viewPort = scrollPane.getViewport();
+    viewPort.setBackground(table.getBackground());
+    
+    LabeledComponent lc = new OWLLabeledComponent("SWRL Rules", scrollPane);
+    lc.addHeaderButton(new ViewRuleAction(table));
+    lc.addHeaderButton(new CreateRuleAction(table, owlModel));
+    lc.addHeaderButton(new CloneRuleAction(table, owlModel));
+    lc.addHeaderButton(new DeleteRuleAction(table));
+    
+    setLayout(new BorderLayout());
+    add(BorderLayout.CENTER, lc);
 
+    return lc;
+  } // initialize
 
-    public void dispose() {
-        table.dispose();
-    }
 }
