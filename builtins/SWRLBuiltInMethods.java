ce0a95813200cac072158c0349cc8a856783076c
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-05-20 16:43:33 +0000

    These files were committed to the wrong place

--- builtins/SWRLBuiltInMethods.java
+++
@@ -1,7 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.bridge.builtins;
-
-// A class defining any SWRL built-in methods must implement this interface. cf. comments on method invokeSWRLBuiltIn() in
-// SWRLRuleEngineBridge.java.
-
-public interface SWRLBuiltInMethods {}
