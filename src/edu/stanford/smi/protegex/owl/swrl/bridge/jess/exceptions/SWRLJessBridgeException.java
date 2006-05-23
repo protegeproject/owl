6135249f6c5c2f1fd7e644336cd4295e3f222c6d
Tania Tudorache <tudorache@users.noreply.github.com>
2006-05-23 18:22:52 +0000

    Moved the SWRL-Jess bridge in another SVN repository: swrl-jess-bridge

--- src/edu/stanford/smi/protegex/owl/swrl/bridge/jess/exceptions/SWRLJessBridgeException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.bridge.jess.exceptions;
-
-import edu.stanford.smi.protegex.owl.swrl.bridge.exceptions.*;
-
-public class SWRLJessBridgeException extends SWRLRuleEngineBridgeException {
-
-  public SWRLJessBridgeException(String message) { super(message); }
-} // SWRLJessBridgeException
