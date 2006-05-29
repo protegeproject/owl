e67b7c193cfcf9a2d6138ba39911fd5949c5d2ae
callendar <callendar@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-05-29 23:24:07 +0000

    Added a class hierarchy option to the SelectResourcesPanel. This option shows a class tree and hides the default instance list. It also disables the instance finder when the class tree is visible.

--- src/edu/stanford/smi/protegex/owl/ui/dialogs/SelectResourcesPanel.java
+++ src/edu/stanford/smi/protegex/owl/ui/dialogs/SelectResourcesPanel.java
@@ -1,17 +1,29 @@
 package edu.stanford.smi.protegex.owl.ui.dialogs;
 
-import edu.stanford.smi.protege.model.Cls;
-import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protege.ui.SelectInstancesPanel;
-import edu.stanford.smi.protege.util.ComponentFactory;
-import edu.stanford.smi.protege.util.LabeledComponent;
-import edu.stanford.smi.protegex.owl.model.OWLModel;
-import edu.stanford.smi.protegex.owl.model.RDFSClass;
-
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
+import java.util.Collections;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.ListSelectionModel;
+
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.ui.Finder;
+import edu.stanford.smi.protege.ui.FrameRenderer;
+import edu.stanford.smi.protege.ui.ParentChildRoot;
+import edu.stanford.smi.protege.ui.SelectInstancesPanel;
+import edu.stanford.smi.protege.util.ComponentFactory;
+import edu.stanford.smi.protege.util.ComponentUtilities;
+import edu.stanford.smi.protege.util.LabeledComponent;
+import edu.stanford.smi.protege.util.LazyTreeRoot;
+import edu.stanford.smi.protege.util.SelectableTree;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFSClass;
 
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
@@ -24,13 +36,18 @@ public class SelectResourcesPanel extends SelectInstancesPanel {
 
     private final static String ALL_ASSERTED = "All Asserted Instances";
 
+    private final static String CLASS_HIERARCHY = "Class Hierarchy";
+    
     private final static String ALL_INFERRED = "All Inferred Instances";
-
-    private JComboBox classComboBox;
 
     private JComboBox instancesComboBox;
 
-
+    private JTree _clsHierarchyTree;
+    
+    private LabeledComponent instanceLabelComponent;
+    
+    private Finder _instanceFinder;
+    
     public SelectResourcesPanel(OWLModel owlModel, Collection classes) {
         this(owlModel, classes, false);
     }
@@ -43,22 +60,15 @@ public class SelectResourcesPanel extends SelectInstancesPanel {
         }
     }
 
-
     protected LabeledComponent createClsesLabeledComponent(KnowledgeBase kb, Collection clses) {
         LabeledComponent lc = super.createClsesLabeledComponent(kb, clses);
         //lc.setHeaderLabel(null);
         //classComboBox = new JComboBox();
         //classComboBox.addItem("Asserted Hierarchy");
         //classComboBox.addItem("Inferred Hierarchy");
-        //lc.setHeaderComponent(classComboBox, BorderLayout.WEST);
+        //lc.setHeaderComponent(classComboBox, BorderLayout.WEST);        
         return lc;
     }
-
-
-    protected JComponent createClsTree(Collection clses) {
-        return super.createClsTree(clses);
-    }
-
 
     protected JComboBox createDirectAllInstanceComboBox() {
         super.createDirectAllInstanceComboBox(); // Only not to break stuff
@@ -67,6 +77,7 @@ public class SelectResourcesPanel extends SelectInstancesPanel {
         instancesComboBox.addItem(DIRECT_INFERRED);
         instancesComboBox.addItem(ALL_ASSERTED);
         instancesComboBox.addItem(ALL_INFERRED);
+        instancesComboBox.addItem(CLASS_HIERARCHY);
         instancesComboBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 loadInstances();
@@ -75,9 +86,40 @@ public class SelectResourcesPanel extends SelectInstancesPanel {
         return instancesComboBox;
     }
 
-
+    protected JComponent createClassHierarchyTree() {
+        LazyTreeRoot root = new ParentChildRoot(Collections.EMPTY_LIST);
+        _clsHierarchyTree = ComponentFactory.createSelectableTree(null, root);
+        _clsHierarchyTree.setRootVisible(false);
+        _clsHierarchyTree.setShowsRootHandles(false);
+        FrameRenderer renderer = FrameRenderer.createInstance();
+        renderer.setDisplayDirectInstanceCount(false);
+        _clsHierarchyTree.setCellRenderer(renderer);
+        return _clsHierarchyTree;
+    }    
+        
+    protected LabeledComponent createInstanceLabeledComponent() {
+    	instanceLabelComponent = super.createInstanceLabeledComponent();
+        createClassHierarchyTree();
+        // set the instance finder so that we can disable it later when
+        // the class hierarchy tree is shown
+        _instanceFinder = (Finder) instanceLabelComponent.getFooterComponent();
+        return instanceLabelComponent;
+    }
+    
     protected Collection getInstances(Cls cls) {
         Object selectedItem = instancesComboBox.getSelectedItem();
+        
+        // toggle the visibility of the instance list and the class hierarcy tree
+        boolean treeVisible = CLASS_HIERARCHY.equals(selectedItem);
+    	JScrollPane scrollPane = (JScrollPane) instanceLabelComponent.getCenterComponent();
+        if (treeVisible && (scrollPane.getViewport().getView() != _clsHierarchyTree)) {
+        	scrollPane.setViewportView(_clsHierarchyTree);
+        	_instanceFinder.setEnabled(false);
+        } else if (!treeVisible && (scrollPane.getViewport().getView() != _instanceList)) {
+        	scrollPane.setViewportView(_instanceList);
+        	_instanceFinder.setEnabled(true);
+        }
+        
         if (DIRECT_INFERRED.equals(selectedItem)) {
             if (cls instanceof RDFSClass) {
                 RDFSClass c = (RDFSClass) cls;
@@ -92,7 +134,27 @@ public class SelectResourcesPanel extends SelectInstancesPanel {
                 RDFSClass c = (RDFSClass) cls;
                 return c.getInferredInstances(true);
             }
+        } 
+        else if (CLASS_HIERARCHY.equals(selectedItem)) {
+        	LazyTreeRoot root = new ParentChildRoot(cls);
+        	((SelectableTree)_clsHierarchyTree).setRoot(root);
+        	// select the first class/instance
+        	_clsHierarchyTree.setSelectionInterval(0, 0);
+        	// return empty list of instances since the instance list is hidden
+        	return Collections.EMPTY_LIST;
         }
-        return cls.getDirectInstances();
+    	return cls.getDirectInstances();
     }
+    
+    public Collection getSelection() {
+        Object selectedItem = instancesComboBox.getSelectedItem();
+        // if the class hierarchy is shown, then return the selection 
+        // from the tree not the instance list 
+        if (CLASS_HIERARCHY.equals(selectedItem)) {
+        	return ComponentUtilities.getSelection(_clsHierarchyTree);
+        } else {
+        	return super.getSelection();
+        }
+    }
+    
 }
