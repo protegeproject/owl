384374495853a0ad3e2aa58df893d188f32a27ca
moconnor <moconnor@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-07-18 18:35:59 +0000

    Fixed for exception chaining.

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/bridge/exceptions/BuiltInException.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/bridge/exceptions/BuiltInException.java
@@ -3,9 +3,8 @@ package edu.stanford.smi.protegex.owl.swrl.bridge.exceptions;
 
 public class BuiltInException extends SWRLRuleEngineBridgeException 
 {
-  public BuiltInException(String message) 
-  { 
-    super(message);
-  } // BuiltInException
+  public BuiltInException(String message) { super(message); } 
+
+  public BuiltInException(String message, Throwable cause) { super(message, cause); }
 
 } // BuiltInException
