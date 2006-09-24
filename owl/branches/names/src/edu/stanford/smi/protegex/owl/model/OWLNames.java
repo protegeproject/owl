ba4a313a2a8f462691bb94f90fd55aacba8c7f4c
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-24 19:33:06 +0000

    Create the frame id from the name rather than the int.

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
@@ -155,8 +155,8 @@ public interface OWLNames {
 
     public interface ClsID {
 
-        FrameID NAMED_CLASS = FrameID.createSystem(9004);
+        FrameID NAMED_CLASS = new FrameID(Cls.NAMED_CLASS);
 
-        FrameID THING = Model.ClsID.THING;
+        FrameID THING = new FrameID(Cls.THING);
     }
 }
