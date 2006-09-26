a780b070a2ac43ec7a0172c015077147f747beeb
Tania Tudorache <tudorache@users.noreply.github.com>
2006-09-26 22:10:52 +0000

    fixed a bug that caused the values in the triple tables to shift at delete

--- src/edu/stanford/smi/protegex/owl/ui/components/triples/DeleteTripleAction.java
+++ src/edu/stanford/smi/protegex/owl/ui/components/triples/DeleteTripleAction.java
@@ -21,13 +21,13 @@ public class DeleteTripleAction extends AbstractAction {
     public DeleteTripleAction(TriplesTable table, String name, Icon icon) {
         super(name, icon);
         this.table = table;
-    }
+    };
 
 
-    ;
-
-
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {    	
+    	if (table.getCellEditor() != null)
+			table.getCellEditor().stopCellEditing();
+    	
         table.removeEditor();
         int row = table.getSelectedRow();
         table.getTableModel().deleteRow(row);
