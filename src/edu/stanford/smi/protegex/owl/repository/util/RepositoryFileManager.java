6cacff32e803827261968f3c106751bf5fcb35fb
Tania Tudorache <tudorache@users.noreply.github.com>
2006-10-05 21:31:20 +0000

    bug fix for the case that the project is a URL rather than a file

--- src/edu/stanford/smi/protegex/owl/repository/util/RepositoryFileManager.java
+++ src/edu/stanford/smi/protegex/owl/repository/util/RepositoryFileManager.java
@@ -2,10 +2,12 @@ package edu.stanford.smi.protegex.owl.repository.util;
 
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.plugin.PluginUtilities;
+import edu.stanford.smi.protege.util.FileUtilities;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.URIUtilities;
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
 import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
+import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.repository.Repository;
 import edu.stanford.smi.protegex.owl.repository.RepositoryManager;
@@ -13,6 +15,7 @@ import edu.stanford.smi.protegex.owl.repository.factory.RepositoryFactory;
 
 import java.io.*;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -44,11 +47,11 @@ public class RepositoryFileManager {
 
 
     public void loadProjectRepositories() {
-        File file = getProjectRepositoryFile();
-        if (file != null && file.exists()) {
+    	URI uri = getProjectRepositoryURI();
+    	if (uri != null) {
             manager.removeAllProjectRepositories();
-            loadRepositoriesFromFile(file, false);
-        }
+            loadRepositoriesFromURI(uri);    		
+    	}
     }
 
 
@@ -140,10 +143,11 @@ public class RepositoryFileManager {
         }
     }
 
+    
 
     private void loadRepositoriesFromFile(final File f,
                                           final boolean global) {
-        try {
+        try {       	
                     FileInputStream fis = new FileInputStream(f);
 					BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 					String line;
@@ -165,10 +169,34 @@ public class RepositoryFileManager {
 
         }
         catch (IOException e) {
-            System.err.println("[Repository Manager] Warning: Could not find repository file: " + f.toString());
+            Log.getLogger().warning("[Repository Manager] Could not find repository file: " + f.toString());
         }
     }
 
+    
+    private void loadRepositoriesFromURI(final URI uri) {
+		try {
+			URL url = new URL(uri.toString());
+			InputStream fis = ProtegeOWLParser.getInputStream(url);
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String line;
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				if (line.length() > 0) {
+					RepositoryFactory factory = RepositoryFactory.getInstance();
+					Repository rep = factory.createOntRepository(model, line);
+					if (rep != null) {						
+						manager.addProjectRepository(rep);
+					}
+				}
+			}			
+			fis.close();
+		} catch (IOException e) {
+			Log.getLogger().warning("[Repository Manager] Could not find repository file: "
+							+ uri.toString());
+		}
+	}
+    
 
     private File getProjectRepositoryFile() {
         Project project = model.getProject();
@@ -191,14 +219,40 @@ public class RepositoryFileManager {
         }
         return null;
     }
+    
+        
+    private URI getProjectRepositoryURI() {
+        Project project = model.getProject();
+        if (project != null) {
+            URI projectURI = project.getProjectURI();
+            
+            //If the project does not have a name, try to retrieve the repository file from the owl file path
+            try {
+                if (projectURI == null && project.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
+                	String owlFileUriString = JenaKnowledgeBaseFactory.getOWLFilePath(project.getSources());
+                	projectURI = URIUtilities.createURI(owlFileUriString);
+                }				
+			} catch (Exception e) {
+				Log.getLogger().warning("Failed to find repository file for " + project);
+			}
+            
+            if (projectURI != null) {
+                return getProjectRepositoryURI(projectURI);
+            }
+        }
+        return null;
+    }
 
 
-    private File getProjectRepositoryFile(URI owlFileURI) {
-        File f = new File(owlFileURI);
+    private URI getProjectRepositoryURI(URI owlFileURI) {
+    	return URIUtilities.replaceExtension(owlFileURI, REPOSITORY_EXTENTION);
+    }
+    
+    private File getProjectRepositoryFile(URI owlFileURI) {        
+    	File f = new File(owlFileURI);
         String repName = f.getName();
         repName = repName.substring(0, repName.lastIndexOf(".")) + REPOSITORY_EXTENTION;
         f = new File(f.getParentFile(), repName);
-        return f;
+        return f;       
     }
 }
-
