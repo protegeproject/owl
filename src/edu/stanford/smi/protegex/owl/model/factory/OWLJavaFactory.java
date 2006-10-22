eb056ad99a4f1efd4f0689940d925e999eab9f2f
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-10-22 18:55:54 +0000

    Bugs 185, 188. Fixed the java factory for classes as well as slots. But the simple instance case is still problematic and it is the most complex.

--- src/edu/stanford/smi/protegex/owl/model/factory/OWLJavaFactory.java
+++ src/edu/stanford/smi/protegex/owl/model/factory/OWLJavaFactory.java
@@ -211,6 +211,7 @@ public class OWLJavaFactory extends DefaultFrameFactory {
         }
         final Cls rdfsClsMetaCls = owlModel.getRDFSClassMetaClassCls();
         final Cls namedClsMetaCls = owlModel.getOWLNamedClassMetaClassCls();
+        boolean isRDFSNamedClass = false;
         for (Iterator it = directTypes.iterator(); it.hasNext();) {
             final Instance metaCls = (Instance) it.next();
             final String metaClsName = metaCls.getName();
@@ -224,16 +225,19 @@ public class OWLJavaFactory extends DefaultFrameFactory {
                         return new DefaultOWLNamedClass(owlModel, id); // handle subclasses of OWLNamedClass
                     }
                     if (((Cls) metaCls).hasSuperclass(rdfsClsMetaCls)) {
-                        return new DefaultRDFSNamedClass(owlModel, id); // handle subclasses of RDFSNamedClass
+                      isRDFSNamedClass = true;   // handle subclasses of RDFSNamedClass
                     }
                 }
-                else {
-                    return new DefaultOWLNamedClass(owlModel, id);  // Robust but ugly guess
+                else { // when do I get here exactly?
+                  return new DefaultOWLNamedClass(owlModel, id); // Robust but ugly guess
                 }
             }
             else if (metaCls.equals(owlModel.getOWLDeprecatedClassClass())) {
-                return new DefaultRDFSNamedClass(owlModel, id);
+                isRDFSNamedClass = true;
             }
+        }
+        if (isRDFSNamedClass) {
+          return new DefaultRDFSNamedClass(owlModel, id); 
         }
         return super.createCls(id, directTypes);
     }
