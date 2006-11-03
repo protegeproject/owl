4020344f0a21db6734340f034307c0bdd9aa9d7f
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-11-03 02:49:40 +0000

    reordered some of the help menu items to make the menu more intuitive, fixed the text of some of the help menu items, updated the version number in the properties file to be in sync with core protege.

--- owl/trunk/src/edu/stanford/smi/protegex/owl/ui/menu/OWLPluginVersionInfoAction.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/ui/menu/OWLPluginVersionInfoAction.java
@@ -10,24 +10,22 @@ import edu.stanford.smi.protegex.owl.ui.actions.AbstractOWLModelAction;
  */
 public class OWLPluginVersionInfoAction extends AbstractOWLModelAction {
 
-
     public String getMenubarPath() {
         return "Help";
     }
 
-
     public String getName() {
-        return "Protege-OWL Version Info...";
+        return "Prot\u00E9g\u00E9-OWL Version Info...";
     }
-
 
     public void run(OWLModel owlModel) {
         ProtegeUI.getModalDialogFactory().showMessageDialog(owlModel,
-                                                            "Protege-OWL Plugin Version " + OWLText.getVersion() +
-                                                            " (Build " + OWLText.getBuildNumber() + ")\n\n" +
-                                                            "The OWL Plugin is being developed at Stanford Medical Informatics in collaboration\n" +
-                                                            "with the Medical Informatics Group at the University of Manchester.\n" +
-                                                            "Some toolbar icons were inspired by those from Eclipse and IntelliJ IDEA.\n\n" +
-                                                            "Open source from: http://protege.stanford.edu/plugins/owl", "About...");
+        OWLText.getName() + " editor, version " + OWLText.getVersion() +
+        " (Build " + OWLText.getBuildNumber() + ")\n\n" + "The " + OWLText.getName() +
+       	" editor is being developed at Stanford Medical Informatics in collaboration\n" +
+        "with the Medical Informatics Group at the University of Manchester.\n" +
+        "Some toolbar icons were inspired by those from Eclipse and IntelliJ IDEA.\n\n" +
+        "Open source from: http://smi-protege.stanford.edu/repos/protege/owl/trunk\n\n",
+        "About Prot\u00E9g\u00E9-OWL");
     }
 }
