3950c99b3faaf5753e4f5999b611d0b8dbe68a48
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-07 22:39:11 +0000

    fix for relative folder repository for access through the Protege owl api

--- owl/trunk/src/edu/stanford/smi/protegex/owl/repository/impl/RelativeFolderRepositoryFactoryPlugin.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/repository/impl/RelativeFolderRepositoryFactoryPlugin.java
@@ -6,6 +6,8 @@ import java.net.URISyntaxException;
 import java.util.logging.Level;
 
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protege.util.URIUtilities;
+import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.repository.Repository;
 import edu.stanford.smi.protegex.owl.repository.factory.RepositoryFactoryPlugin;
@@ -24,7 +26,15 @@ public class RelativeFolderRepositoryFactoryPlugin implements RepositoryFactoryP
     public boolean isSuitable(OWLModel model,
                               String s) {
         if (model.getProject() != null) {
-            if (model.getProject().getProjectURI() != null) {
+        
+           URI owlFileUri = model.getProject().getProjectURI();
+        
+           if (owlFileUri == null && model.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
+        	   String uriString = JenaKnowledgeBaseFactory.getOWLFilePath(model.getProject().getSources());
+        	   owlFileUri = URIUtilities.createURI(uriString);
+           } 
+           
+        	if (owlFileUri != null) {
                 try {
                     URI uri = new URI(s.trim());
                     if (uri.isAbsolute() == false) {
@@ -46,9 +56,19 @@ public class RelativeFolderRepositoryFactoryPlugin implements RepositoryFactoryP
     public Repository createRepository(OWLModel model,
                                        String s) {
         try {
-            return new RelativeFolderRepository(model.getProject().getProjectURI().toURL(),
-                    s.trim());
-
+        	
+            URI owlFileUri = model.getProject().getProjectURI();
+            
+            if (owlFileUri == null && model.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
+         	   String uriString = JenaKnowledgeBaseFactory.getOWLFilePath(model.getProject().getSources());
+         	   owlFileUri = URIUtilities.createURI(uriString);
+            } 
+        	
+            if (owlFileUri == null) {
+            	return null;
+            }
+            
+            return new RelativeFolderRepository(owlFileUri.toURL(), s.trim());
         }
         catch (MalformedURLException e) {
             Log.getLogger().log(Level.SEVERE, "Exception caught", e);
