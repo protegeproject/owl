d6809383eadbb4035364838624e018b30ffa68ec
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-22 19:15:28 +0000

    We don't need the collab branch. It was the same as the trunk

--- owl/branches/collab/src/edu/stanford/smi/protegex/owl/swrl/bridge/exceptions/InvalidColumnNameException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.bridge.exceptions;
-
-public class InvalidColumnNameException extends ResultException
-{
-  public InvalidColumnNameException(String message) { super(message); }
-
-  public InvalidColumnNameException(String message, Throwable cause) { super(message, cause); }
-} // InvalidColumnNameException
