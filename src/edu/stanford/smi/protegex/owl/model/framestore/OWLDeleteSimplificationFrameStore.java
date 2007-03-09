3758a0f380f35f29ac4037c7fa0c06c1ccf2e8f9
Tania Tudorache <tudorache@users.noreply.github.com>
2007-03-09 01:44:32 +0000

    Added argument to beginTransaction for the name of the frame on which the transaction is applied to

--- src/edu/stanford/smi/protegex/owl/model/framestore/OWLDeleteSimplificationFrameStore.java
+++ src/edu/stanford/smi/protegex/owl/model/framestore/OWLDeleteSimplificationFrameStore.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protegex.owl.model.framestore;
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.DeleteSimplificationFrameStore;
+import edu.stanford.smi.protege.util.transaction.TransactionMonitor;
 
 /**
  * A modified DeleteSimplificationFrameStore that does not automatically delete
@@ -17,7 +18,9 @@ public class OWLDeleteSimplificationFrameStore extends DeleteSimplificationFrame
             super.removeDirectTemplateSlot(cls, slot);
         }
         else {
-            beginTransaction("Remove template slot");
+        	//TT: This is a little bit cheating, but harmless...
+            beginTransaction("Remove template slot from class " + cls + TransactionMonitor.APPLY_TO_TRAILER_STRING +
+            		(cls == null ? null : cls.getName()));
             getDelegate().removeDirectTemplateSlot(cls, slot);
             commitTransaction();
         }
