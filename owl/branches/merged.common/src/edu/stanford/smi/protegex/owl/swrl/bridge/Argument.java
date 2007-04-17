aa73248acb23796011063a71e5eae6d164876272
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-17 22:35:49 +0000

    removing the nci swrl

--- owl/branches/merged.common/src/edu/stanford/smi/protegex/owl/swrl/bridge/Argument.java
+++
@@ -1,9 +0,0 @@
-
-// Info object representing an argument (to a built-in or atom), which will be one of IndividualInfo, VariableInfo, or LiteralInfo.
-
-package edu.stanford.smi.protegex.owl.swrl.bridge;
-
-public interface Argument 
-{
-  String getName();
-} // Argument
