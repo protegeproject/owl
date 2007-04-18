9de69b8a75e6b7d9e6eae38999bd9dcdbd7042d0
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-18 17:56:29 +0000

    removing the nci version of swrl - it is no good...

--- owl/branches/merged/src/edu/stanford/smi/protegex/owl/swrl/bridge/exceptions/InvalidSWRLAtomException.java
+++
@@ -1,7 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.bridge.exceptions;
-
-public class InvalidSWRLAtomException extends SWRLRuleEngineBridgeException 
-{
-  public InvalidSWRLAtomException(String message) { super(message); }
-} // InvalidSWRLAtomException
