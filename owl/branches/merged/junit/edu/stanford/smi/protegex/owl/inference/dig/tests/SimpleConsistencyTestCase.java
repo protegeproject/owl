85bcf76a1ffd7816a612c546bb60f5438be0727a
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-19 23:48:07 +0000

    Removed the conflict

--- owl/branches/merged/junit/edu/stanford/smi/protegex/owl/inference/dig/tests/SimpleConsistencyTestCase.java
+++ owl/branches/merged/junit/edu/stanford/smi/protegex/owl/inference/dig/tests/SimpleConsistencyTestCase.java
@@ -22,27 +22,4 @@ public class SimpleConsistencyTestCase extends AbstractDIGReasonerTestCase {
         computeAndCheckInconsistentConcepts();
     }
 }
-=======
-/**
- * 
- */
-package edu.stanford.smi.protegex.owl.inference.dig.tests;
 
-import edu.stanford.smi.protege.util.URIUtilities;
-import edu.stanford.smi.protegex.owl.tests.AbstractDIGReasonerTestCase;
-
-/**
- * @author rouquett
- * @prowl.junit.dig
- */
-public class SimpleConsistencyTestCase extends AbstractDIGReasonerTestCase {
-    
-
-    public void testDisjointSubclassInconsistency() throws Exception {
-        if (!reasonerInitialized()) {
-          return;
-        }
-        loadTestOntology(URIUtilities.createURI("junit/projects/inconsistent.owl"));
-        computeAndCheckInconsistentConcepts();
-    }
-}
