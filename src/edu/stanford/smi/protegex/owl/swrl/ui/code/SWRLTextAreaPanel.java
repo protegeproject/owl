3e3847f6910c7ad678c145e04aca84fde91b2f11
Tania Tudorache <tudorache@users.noreply.github.com>
2007-04-27 18:24:51 +0000

    reimplemented the display (show) of SWRL rules

--- src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLTextAreaPanel.java
+++ src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLTextAreaPanel.java
@@ -2,20 +2,25 @@ package edu.stanford.smi.protegex.owl.swrl.ui.code;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.util.logging.Level;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.ui.InstanceDisplay;
+import edu.stanford.smi.protege.util.ComponentFactory;
+import edu.stanford.smi.protege.util.ComponentUtilities;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protege.util.ModalDialog;
+import edu.stanford.smi.protege.widget.ClsWidget;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLImp;
+import edu.stanford.smi.protegex.owl.swrl.model.SWRLNames;
 import edu.stanford.smi.protegex.owl.swrl.parser.SWRLParser;
-import edu.stanford.smi.protegex.owl.ui.ProtegeUI;
+import edu.stanford.smi.protegex.owl.swrl.ui.widget.SWRLRuleSlotWidget;
 import edu.stanford.smi.protegex.owl.ui.dialogs.ModalDialogFactory;
-import edu.stanford.smi.protegex.owl.ui.resourcedisplay.ResourceDisplay;
 
 /**
  * A panel which can be used to edit an OWL expression in a multi-line dialog.
@@ -24,7 +29,9 @@ import edu.stanford.smi.protegex.owl.ui.resourcedisplay.ResourceDisplay;
  */
 public class SWRLTextAreaPanel extends JPanel implements ModalDialogFactory.CloseCallback {
 
-    private OWLModel owlModel;
+    private static final String SWRL_RULE_PANEL_TITLE = "SWRL Rule";
+
+	private OWLModel owlModel;
 
     private SWRLSymbolPanel symbolPanel;
 
@@ -48,19 +55,8 @@ public class SWRLTextAreaPanel extends JPanel implements ModalDialogFactory.Clos
             String text = imp.getBrowserText();
             textArea.setText(text);
             textArea.reformatText();
-        }
+        }	
         symbolPanel.setSymbolEditor(textArea);
-        //symbolPanel.setPreferredSize(new Dimension(100,));
-
-        InstanceDisplay id = new InstanceDisplay(anOWLModel.getProject(), false, false);        
-        id.setInstance(imp);
-        setLayout(new BorderLayout(0, 8));
-        add(BorderLayout.NORTH, id);
-        JScrollPane sp = new JScrollPane(textArea);
-        sp.setPreferredSize(new Dimension(100,200));
-        add(BorderLayout.CENTER, sp);
-        add(BorderLayout.SOUTH, symbolPanel);
-        //setPreferredSize(new Dimension(600, 400));
     }
 
 
@@ -96,27 +92,93 @@ public class SWRLTextAreaPanel extends JPanel implements ModalDialogFactory.Clos
             return parser.parse(uniCodeText);
         }
         catch (Exception ex) {
+        	Log.getLogger().warning("Error at parsing SWRL rule " + (textArea == null ? "" : textArea.getText()));
             return null;
         }
     }
 
 
-    public String getResultAsString() { return textArea.getText(); }
-
-  public static boolean showEditDialog(Component parent, OWLModel owlModel, SWRLImp imp) 
-  {
-    SWRLTextAreaPanel panel = new SWRLTextAreaPanel(owlModel, imp);
-    String title = "Edit SWRL Rule";
-    if (ProtegeUI.getModalDialogFactory().showDialog(parent, panel, title, ModalDialogFactory.MODE_OK_CANCEL, panel) == ModalDialogFactory.OPTION_OK) {
-      try {
-        imp.setExpression(panel.getResultAsString());
-        return true;
-      }
-      catch (Exception ex) {
-        System.err.println("[SWRLTextAreaPanel]  Fatal error");
-        Log.getLogger().log(Level.SEVERE, "Exception caught", ex);
-      }
+    public String getResultAsString() { 
+    	return textArea.getText(); 
     }
-    return false;
-  }
+
+    public static boolean showEditDialog(Component parent, OWLModel owlModel, SWRLImp imp) {
+    	
+    	if (imp == null) {
+    		return false;
+    	}
+    	
+    	InstanceDisplay instanceDisplay = new InstanceDisplay(owlModel.getProject(), false, false);
+    	instanceDisplay.setInstance(imp);
+    	
+    	showFrame(instanceDisplay);
+    	
+    	//TT: what should we return?
+    	return true;
+    }
+    
+    private static JFrame showFrame(final InstanceDisplay display) {
+        final JFrame frame = ComponentFactory.createFrame();
+        
+        frame.addWindowListener(new WindowAdapter() {
+    		@Override
+    		public void windowClosing(WindowEvent e) {	
+    			
+    			if (hasChangedRule(display)) {
+    				int ret = ModalDialog.showMessageDialog(display, "Rule has not been saved. If you continue, " +
+    						"\nchanges will be lost.\n" +
+    						"Do you want to continue?", 
+    						"Rule not saved", ModalDialog.MODE_YES_NO);
+    				
+    				if (ret == ModalDialog.OPTION_NO) {
+    					return;
+    				}
+    			}
+    			
+    		    JFrame frame = (JFrame) e.getWindow();
+                frame.setVisible(false);                              
+                ComponentUtilities.dispose(frame);
+                edu.stanford.smi.protege.Application.repaint();
+    			
+    		}
+
+			private boolean hasChangedRule(InstanceDisplay display) {
+							 			 
+				 try {
+					ClsWidget clsWidget = display.getFirstClsWidget();
+					Slot swrlBodySlot = display.getCurrentInstance().getKnowledgeBase().getSlot(SWRLNames.Slot.BODY);
+					
+					SWRLRuleSlotWidget swrlWidget = (SWRLRuleSlotWidget) clsWidget.getSlotWidget(swrlBodySlot);
+					
+					if (swrlWidget.commitChanges()) {
+						return false;
+					}
+							
+					String newRuleText = swrlWidget.getSwrlTextAreaText();
+					
+					SWRLImp swrlImp = (SWRLImp) swrlWidget.getInstance();					
+					String oldRuleText = SWRLTextArea.reformatText(swrlImp.getBrowserText());
+					
+					return !(oldRuleText.equals(newRuleText));
+					
+				} catch (Exception e) {
+					// do nothing, probably not using the SWRLRuleSlotWidget
+					return false;
+				}				
+			}
+    	});
+        
+        frame.getContentPane().add(display, BorderLayout.CENTER);        
+        ComponentUtilities.pack(frame);
+        display.setResizeVertically(true);
+
+        ComponentUtilities.center(frame);       
+        
+        frame.setTitle(SWRL_RULE_PANEL_TITLE);
+        frame.setVisible(true);
+        
+        return frame;
+    }
+    
+
 }
