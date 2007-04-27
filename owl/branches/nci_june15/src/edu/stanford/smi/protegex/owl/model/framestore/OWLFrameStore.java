6ff53a7c0611d672861bbc87a168cd0625bf6c80
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-27 23:02:45 +0000

    replaced the code to reset the undo manager to make it do it right.

--- owl/branches/nci_june15/src/edu/stanford/smi/protegex/owl/model/framestore/OWLFrameStore.java
+++ owl/branches/nci_june15/src/edu/stanford/smi/protegex/owl/model/framestore/OWLFrameStore.java
@@ -255,13 +255,16 @@ public class OWLFrameStore extends FrameStoreAdapter {
     public void copyFacetValuesIntoNamedClses() {
         boolean oldUndo = owlModel.isUndoEnabled();
         owlModel.setUndoEnabled(false);
-        for (Iterator it = ((AbstractOWLModel) owlModel).getRDFSClasses().iterator(); it.hasNext();) {
-            Cls cls = (Cls) it.next();
-            if (cls instanceof OWLRestriction) {  // Convert restrictions into facet overrides
-                copyFacetValuesIntoOWLNamedClass((OWLRestriction) cls);
+        try {
+            for (Iterator it = ((AbstractOWLModel) owlModel).getRDFSClasses().iterator(); it.hasNext();) {
+                Cls cls = (Cls) it.next();
+                if (cls instanceof OWLRestriction) {  // Convert restrictions into facet overrides
+                    copyFacetValuesIntoOWLNamedClass((OWLRestriction) cls);
+                }
             }
+        } finally {
+            owlModel.setUndoEnabled(oldUndo);
         }
-        owlModel.setUndoEnabled(oldUndo);
     }
 
 
