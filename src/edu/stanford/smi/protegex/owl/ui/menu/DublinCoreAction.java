70003e30b1ee333a262e4ca60695bea0c85a8f5c
Tania Tudorache <tudorache@users.noreply.github.com>
2007-06-20 22:26:54 +0000

    centralizing menu handling

--- src/edu/stanford/smi/protegex/owl/ui/menu/DublinCoreAction.java
+++ src/edu/stanford/smi/protegex/owl/ui/menu/DublinCoreAction.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protegex.owl.ui.menu;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.ui.ProtegeUI;
 import edu.stanford.smi.protegex.owl.ui.actions.AbstractOWLModelAction;
+import edu.stanford.smi.protegex.owl.ui.actions.OWLModelActionConstants;
 import edu.stanford.smi.protegex.owl.ui.repository.ShowRepositoryEditorAction;
 
 import javax.swing.*;
@@ -13,7 +14,7 @@ import javax.swing.*;
 public class DublinCoreAction extends AbstractOWLModelAction {
 
     public String getMenubarPath() {
-        return OWL_MENU + PATH_SEPARATOR + ShowRepositoryEditorAction.GROUP;
+        return OWL_MENU + PATH_SEPARATOR + OWLModelActionConstants.REPOSITORY_GROUP;
     }
 
 
