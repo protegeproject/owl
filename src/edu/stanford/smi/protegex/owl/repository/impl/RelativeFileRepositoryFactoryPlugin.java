0f8fe63db39c13ac961970f948d699f670741d8f
Tania Tudorache <tudorache@users.noreply.github.com>
2007-06-22 16:46:29 +0000

    added the relative file repository

--- src/edu/stanford/smi/protegex/owl/repository/impl/RelativeFileRepositoryFactoryPlugin.java
+++ src/edu/stanford/smi/protegex/owl/repository/impl/RelativeFileRepositoryFactoryPlugin.java
@@ -1,10 +1,13 @@
 package edu.stanford.smi.protegex.owl.repository.impl;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.logging.Level;
 
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.URIUtilities;
 import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
@@ -23,49 +26,61 @@ import edu.stanford.smi.protegex.owl.repository.util.RepositoryUtil;
  */
 public class RelativeFileRepositoryFactoryPlugin implements RepositoryFactoryPlugin {
 
-    public final static String RELATIVE_FILE_PREFIX = LocalFileRepositoryFactoryPlugin.FILE_PREFIX + "./";
 
     public boolean isSuitable(OWLModel model, String s) {
-        return s.trim().startsWith(RelativeFileRepositoryFactoryPlugin.RELATIVE_FILE_PREFIX) &&
-                    getRepositoryFile(model, s) != null;
-    }
+		if (model.getProject() != null) {
 
+			URI owlFileUri = model.getProject().getProjectURI();
 
-    public Repository createRepository(OWLModel model, String s) {
-        try {
-            URI uri = new URI(s);
-            return new LocalFileRepository(getRepositoryFile(model, s), 
-                                           RepositoryUtil.isForcedToBeReadOnly(uri.getQuery()));
-        }
-        catch (URISyntaxException e) {
-            return null;
-        }
-    }
+			if (owlFileUri == null && model.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
+				String uriString = JenaKnowledgeBaseFactory.getOWLFilePath(model.getProject().getSources());
+				owlFileUri = URIUtilities.createURI(uriString);
+			}
+
+			if (owlFileUri != null) {
+				try {
+					URI uri = new URI(s.trim());
+					if (uri.isAbsolute() == false) {
+						File file = RepositoryUtil.getRepositoryFileFromRelativePath(model, s);
+						return (file != null);
+					} else {
+						return false;
+					}
+				} catch (URISyntaxException e) {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+  
     
-    private File getRepositoryFile(OWLModel model, String s) {
-        File repositoryFile = null;
-        try {
-            URI owlFileUri = model.getProject().getProjectURI();
+    public Repository createRepository(OWLModel model, String s) {
+		URI owlFileUri = model.getProject().getProjectURI();
 
-            if (owlFileUri == null && model.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
-                String uriString = JenaKnowledgeBaseFactory.getOWLFilePath(model.getProject().getSources());
-                owlFileUri = URIUtilities.createURI(uriString);
-            } 
+		if (owlFileUri == null
+				&& model.getKnowledgeBaseFactory() instanceof JenaKnowledgeBaseFactory) {
+			String uriString = JenaKnowledgeBaseFactory.getOWLFilePath(model.getProject().getSources());
+			owlFileUri = URIUtilities.createURI(uriString);
+		}
 
-            if (owlFileUri == null) {
-                return null;
-            }
-            URL absoluteURL = new URL(owlFileUri.toURL(), s.trim());
-            
-            repositoryFile = new File(absoluteURL.getPath());
-            if (repositoryFile.canRead() && repositoryFile.isFile()) {
-                return repositoryFile;
-            }
-        }
-        catch (Throwable t) {
-            return null;
-        }
-        return null;
-    }
+		if (owlFileUri == null) {
+			return null;
+		}
+		File file = RepositoryUtil.getRepositoryFileFromRelativePath(model, s);			
+		if (file != null) {			
+			try {
+				return new RelativeFileRepository(file, owlFileUri.toURL(), s.trim());
+			} catch (MalformedURLException e) {
+				return null;
+			} catch (URISyntaxException e) {
+				return null;			
+			}
+		}
+		return null;
+	}
+    
+    
+ 
 }
 
