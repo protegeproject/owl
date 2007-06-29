a18107fe75abd25f1c5793e8e050d01b38670461
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-06-29 23:15:30 +0000

    added code for searching references of annotation properties

---
+++ owl/branches/nci_june15/src/edu/stanford/smi/protegex/owl/ui/search/DefaultFindUsageTableItem.java
@@ -0,0 +1,10 @@
+package edu.stanford.smi.protegex.owl.ui.search;
+
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+
+public class DefaultFindUsageTableItem extends AbstractFindUsageTableItem {
+	
+	public DefaultFindUsageTableItem(int type, RDFResource host, Object usage) {
+		super(type, host, usage);		
+	}			
+}
