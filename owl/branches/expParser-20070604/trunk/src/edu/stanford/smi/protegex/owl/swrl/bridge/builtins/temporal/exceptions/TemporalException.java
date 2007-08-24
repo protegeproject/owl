f24932a59cf13049828e86acdb23af34f9fd47f5
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-08-24 22:28:56 +0000

    not used anymore. New one: expParser-200708

--- owl/branches/expParser-20070604/trunk/src/edu/stanford/smi/protegex/owl/swrl/bridge/builtins/temporal/exceptions/TemporalException.java
+++
@@ -1,9 +0,0 @@
-  
-package edu.stanford.smi.protegex.owl.swrl.bridge.builtins.temporal.exceptions;
-
-public class TemporalException extends Throwable
-{
-  public TemporalException(String message) { super(message); }
-
-  public TemporalException(String message, Throwable cause) { super(message, cause); }
-} // TemporalException
