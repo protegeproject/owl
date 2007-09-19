7ef06b6ad13157e4a12225257be2216b986e6dd5
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-09-19 22:26:28 +0000

    added accept method for the OWLModelVisitor

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/model/impl/AbstractSWRLIndividual.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/model/impl/AbstractSWRLIndividual.java
@@ -5,10 +5,12 @@ import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLIndividual;
+import edu.stanford.smi.protegex.owl.model.visitor.OWLModelVisitor;
+import edu.stanford.smi.protegex.owl.swrl.model.SWRLIndividual;
 
 import java.util.Set;
 
-public abstract class AbstractSWRLIndividual extends DefaultOWLIndividual
+public abstract class AbstractSWRLIndividual extends DefaultOWLIndividual implements SWRLIndividual
 {
   public AbstractSWRLIndividual(KnowledgeBase kb, FrameID id) {
     super(kb, id);
@@ -17,4 +19,8 @@ public abstract class AbstractSWRLIndividual extends DefaultOWLIndividual
   public AbstractSWRLIndividual() {}
 
   public abstract void getReferencedInstances(Set set);
+  
+  public void accept(OWLModelVisitor visitor) {
+      visitor.visitSWRLIndividual(this);
+  }
 }
