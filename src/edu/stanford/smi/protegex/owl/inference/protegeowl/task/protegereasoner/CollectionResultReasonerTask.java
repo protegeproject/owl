098a228b636e2e2b22563622dc335454a647a54a
Tania Tudorache <tudorache@users.noreply.github.com>
2007-11-10 02:49:55 +0000

    refactored reasoning code to plugin architecture

---
+++ src/edu/stanford/smi/protegex/owl/inference/protegeowl/task/protegereasoner/CollectionResultReasonerTask.java
@@ -0,0 +1,22 @@
+package edu.stanford.smi.protegex.owl.inference.protegeowl.task.protegereasoner;
+
+import java.util.Collection;
+
+import edu.stanford.smi.protegex.owl.inference.protegeowl.task.ReasonerTask;
+
+/**
+ * User: matthewhorridge<br>
+ * The Univeristy Of Manchester<br>
+ * Medical Informatics Group<br>
+ * Date: Aug 17, 2004<br><br>
+ * <p/>
+ * matthew.horridge@cs.man.ac.uk<br>
+ * www.cs.man.ac.uk/~horridgm<br><br>
+ * <p/>
+ * Implemented by tasks that return a <code>Collection</code>
+ * as a result.
+ */
+public interface CollectionResultReasonerTask extends ReasonerTask {
+
+    public Collection getResult();
+}
---
+++ src/edu/stanford/smi/protegex/owl/inference/protegeowl/task/protegereasoner/GetSuperConceptsTask.java
@@ -0,0 +1,30 @@
+package edu.stanford.smi.protegex.owl.inference.protegeowl.task.protegereasoner;
+
+import java.util.Collection;
+
+import edu.stanford.smi.protegex.owl.inference.reasoner.ProtegeReasoner;
+import edu.stanford.smi.protegex.owl.inference.reasoner.exception.ProtegeReasonerException;
+import edu.stanford.smi.protegex.owl.model.OWLClass;
+
+/**
+ * User: matthewhorridge<br>
+ * The Univeristy Of Manchester<br>
+ * Medical Informatics Group<br>
+ * Date: Aug 17, 2004<br><br>
+ * <p/>
+ * matthew.horridge@cs.man.ac.uk<br>
+ * www.cs.man.ac.uk/~horridgm<br><br>
+ */
+public class GetSuperConceptsTask extends AbstractSingleConceptWithConceptCollectionResultTask {
+
+    public GetSuperConceptsTask(OWLClass aClass,
+                                ProtegeReasoner protegeOWLReasoner) {
+        super("Computing superclasses", aClass, protegeOWLReasoner);
+    }
+
+    @Override
+    public Collection getQueryResults() throws ProtegeReasonerException {
+    	return getProtegeReasoner().getSuperclasses(getCls());
+    }
+}
+
