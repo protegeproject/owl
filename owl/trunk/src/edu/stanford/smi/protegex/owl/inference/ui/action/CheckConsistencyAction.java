a073a71b3101622528f06ac388e7526ffd71f9c9
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-11-10 02:55:49 +0000

    refactored reasoning code to plugin architecture

--- owl/trunk/src/edu/stanford/smi/protegex/owl/inference/ui/action/CheckConsistencyAction.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/inference/ui/action/CheckConsistencyAction.java
@@ -1,9 +1,9 @@
 package edu.stanford.smi.protegex.owl.inference.ui.action;
 
-import edu.stanford.smi.protegex.owl.inference.dig.exception.DIGReasonerException;
-import edu.stanford.smi.protegex.owl.inference.protegeowl.ProtegeOWLReasoner;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.ReasonerManager;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.task.ReasonerTaskListener;
+import edu.stanford.smi.protegex.owl.inference.reasoner.ProtegeReasoner;
+import edu.stanford.smi.protegex.owl.inference.reasoner.exception.ProtegeReasonerException;
 import edu.stanford.smi.protegex.owl.inference.ui.ReasonerActionRunner;
 import edu.stanford.smi.protegex.owl.inference.ui.RunnableReasonerAction;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
@@ -28,7 +28,7 @@ public class CheckConsistencyAction extends AbstractOWLModelAction {
 
 
     public String getMenubarPath() {
-        return OWL_MENU + PATH_SEPARATOR + ActionConstants.ACTION_GROUP;
+        return REASONING_MENU + PATH_SEPARATOR + ActionConstants.ACTION_GROUP;
     }
 
 
@@ -44,11 +44,11 @@ public class CheckConsistencyAction extends AbstractOWLModelAction {
 
     public void run(final OWLModel owlModel) {
         ReasonerActionRunner runner = new ReasonerActionRunner(new RunnableReasonerAction() {
-            public void executeReasonerActions(ReasonerTaskListener taskListener) throws DIGReasonerException {
-                ProtegeOWLReasoner reasoner = ReasonerManager.getInstance().getReasoner(owlModel);
-                reasoner.computeInconsistentConcepts(taskListener);
+            public void executeReasonerActions(ReasonerTaskListener taskListener) throws ProtegeReasonerException {
+                ProtegeReasoner reasoner = ReasonerManager.getInstance().getProtegeReasoner(owlModel);
+                reasoner.setReasonerTaskListener(taskListener);
+                reasoner.computeInconsistentConcepts(); 
             }
-
 
             public OWLModel getOWLModel() {
                 return owlModel;
--- owl/trunk/src/edu/stanford/smi/protegex/owl/inference/ui/action/GetInferredSubClassesAction.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/inference/ui/action/GetInferredSubClassesAction.java
@@ -1,9 +1,15 @@
 package edu.stanford.smi.protegex.owl.inference.ui.action;
 
-import edu.stanford.smi.protegex.owl.inference.dig.exception.DIGReasonerException;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
 import edu.stanford.smi.protegex.owl.inference.protegeowl.ProtegeOWLReasoner;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.ReasonerManager;
 import edu.stanford.smi.protegex.owl.inference.protegeowl.task.ReasonerTaskListener;
+import edu.stanford.smi.protegex.owl.inference.protegeowl.task.protegereasoner.GetSubConceptsTask;
+import edu.stanford.smi.protegex.owl.inference.reasoner.AbstractProtegeReasoner;
+import edu.stanford.smi.protegex.owl.inference.reasoner.ProtegeReasoner;
+import edu.stanford.smi.protegex.owl.inference.reasoner.exception.ProtegeReasonerException;
 import edu.stanford.smi.protegex.owl.inference.ui.ReasonerActionRunner;
 import edu.stanford.smi.protegex.owl.inference.ui.RunnableReasonerAction;
 import edu.stanford.smi.protegex.owl.model.OWLClass;
@@ -11,9 +17,6 @@ import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.ui.actions.ResourceAction;
 import edu.stanford.smi.protegex.owl.ui.icons.OWLIcons;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
 
 /**
  * User: matthewhorridge<br>
@@ -41,12 +44,24 @@ public class GetInferredSubClassesAction extends ResourceAction {
 
     public void actionPerformed(ActionEvent e) {
         final OWLModel owlModel = getResource().getOWLModel();
-        final ProtegeOWLReasoner reasoner = ReasonerManager.getInstance().getReasoner(owlModel);
+        final ProtegeReasoner reasoner = ReasonerManager.getInstance().getProtegeReasoner(owlModel);
+                
         ReasonerActionRunner runner = new ReasonerActionRunner(new RunnableReasonerAction() {
-            public void executeReasonerActions(ReasonerTaskListener taskListener) throws DIGReasonerException {
-                reasoner.getSubclasses((OWLClass) getResource(), taskListener);
-            }
+            public void executeReasonerActions(ReasonerTaskListener taskListener) throws ProtegeReasonerException {
+            	reasoner.setReasonerTaskListener(taskListener);
 
+            	//ugly handling of different dig vs. direct reasoner - for backwards compabtilibily reasons
+                if (reasoner instanceof AbstractProtegeReasoner && !(reasoner instanceof ProtegeOWLReasoner)) {
+                	AbstractProtegeReasoner protegeReasoner = (AbstractProtegeReasoner) reasoner;
+                	GetSubConceptsTask task = new GetSubConceptsTask((OWLClass) getResource(), reasoner);
+            		protegeReasoner.performTask(task);
+            		
+            		return;
+        		} else {
+        			reasoner.getSubclasses((OWLClass) getResource());
+                }
+                
+            }
 
             public OWLModel getOWLModel() {
                 return owlModel;
