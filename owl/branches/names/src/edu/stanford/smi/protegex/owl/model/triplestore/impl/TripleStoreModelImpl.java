1585018a0af50db0b6d3cfb1d9b4aa12e51882c3
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-10 20:36:30 +0000

    no need for separate triple stores and models for database and file

---
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/triplestore/impl/TripleStoreModelImpl.java
@@ -0,0 +1,356 @@
+package edu.stanford.smi.protegex.owl.model.triplestore.impl;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Model;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.framestore.MergingNarrowFrameStore;
+import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
+import edu.stanford.smi.protege.util.CollectionUtilities;
+import edu.stanford.smi.protegex.owl.jena.JenaOWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.factory.OWLJavaFactoryUpdater;
+import edu.stanford.smi.protegex.owl.model.impl.DefaultRDFSLiteral;
+import edu.stanford.smi.protegex.owl.model.triplestore.Triple;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStoreModel;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStoreUtil;
+import edu.stanford.smi.protegex.owl.util.OWLFrameStoreUtils;
+
+/**
+ * @author Holger Knublauch  <holger@knublauch.com>
+ */
+public class TripleStoreModelImpl implements TripleStoreModel {
+    
+    protected MergingNarrowFrameStore mnfs;
+    
+    private Slot nameSlot;
+    
+    private OWLModel owlModel;
+    
+    private List<TripleStore> ts = new ArrayList<TripleStore>();
+
+
+    public TripleStoreModelImpl(OWLModel owlModel) {
+        this.nameSlot = ((KnowledgeBase) owlModel).getSlot(Model.Slot.NAME);
+        this.mnfs = MergingNarrowFrameStore.get(owlModel);
+        this.owlModel = owlModel;
+        initTripleStores();
+    }
+    
+    public TripleStoreModelImpl(OWLModel owlModel, NarrowFrameStore frameStore) {
+      this(owlModel);
+      ts.add(new TripleStoreImpl(owlModel, frameStore, this));
+      owlModel.resetJenaModel();
+    }
+
+
+    public TripleStore createTripleStore(NarrowFrameStore frameStore) {
+        String parentName = getActiveTripleStore().getName();
+        mnfs.addActiveChildFrameStore(frameStore, parentName);
+        TripleStore tripleStore = new TripleStoreImpl(owlModel, frameStore, this);
+        ts.add(tripleStore);
+        updateRemoveFrameStores();
+        return tripleStore;
+    }
+
+
+    public void deleteTripleStore(TripleStore tripleStore) {
+        ts.remove(tripleStore);
+        // TODO! mnfs.delete(tripleStore.getNarrowFrameStore());
+    }
+
+
+    public static void ensureActiveTripleStore(RDFResource resource) {
+        TripleStoreUtil.ensureActiveTripleStore(resource);
+    }
+
+
+    public TripleStore getTripleStoreByDefaultNamespace(String namespace) {
+        for (Iterator<TripleStore> it = ts.iterator(); it.hasNext();) {
+            TripleStore tripleStore = it.next();
+            if (namespace.equals(tripleStore.getDefaultNamespace())) {
+                return tripleStore;
+            }
+        }
+        return null;
+    }
+
+
+    private void initTripleStores() {
+        ts = new ArrayList<TripleStore>();
+        NarrowFrameStore[] ss = (NarrowFrameStore[]) mnfs.getAvailableFrameStores().toArray(new NarrowFrameStore[0]);
+        for (int i = 0; i < ss.length; i++) {
+            NarrowFrameStore s = ss[i];
+            ts.add(new TripleStoreImpl(owlModel, s, this));
+        }
+        updateRemoveFrameStores();
+    }
+
+
+    private void updateRemoveFrameStores() {
+        Collection allFrameStores = mnfs.getAvailableFrameStores();
+        mnfs.setRemoveFrameStores(allFrameStores);
+        owlModel.resetJenaModel();
+    }
+    
+
+
+    public void endTripleStoreChanges() {
+        owlModel.flushCache();
+        final Collection resources = owlModel.getRDFResources();
+        for (Iterator it = resources.iterator(); it.hasNext();) {
+            RDFResource resource = (RDFResource) it.next();
+            if (resource.isSystem()) {
+                it.remove();
+            }
+        }
+        OWLJavaFactoryUpdater.run(owlModel, resources);
+
+        boolean enabled = owlModel.setGenerateEventsEnabled(false);
+        try {
+            TripleChangePostProcessor.postProcess(owlModel);
+        }
+        finally {
+            owlModel.setGenerateEventsEnabled(enabled);
+        }
+        owlModel.flushCache();
+    }
+
+
+    public TripleStore getActiveTripleStore() {
+        if (mnfs == null) {
+            /**
+             * Probably a client talking to a server.
+             */
+            return ts.get(ts.size() - 1);
+        }
+        NarrowFrameStore activeFrameStore = mnfs.getActiveFrameStore();
+        String name = activeFrameStore.getName();
+        return getTripleStore(name);
+    }
+
+
+    public TripleStore getHomeTripleStore(RDFResource resource) {
+        for (Iterator<TripleStore> it = ts.iterator(); it.hasNext();) {
+            TripleStore tripleStore = it.next();
+            if (tripleStore.getNarrowFrameStore().getValuesCount(resource, nameSlot, null, false) > 0) {
+                return tripleStore;
+            }
+        }
+        return null;
+    }
+
+
+    public Collection getPropertyValues(RDFResource resource, RDFProperty property) {
+        Collection values = mnfs.getValues(resource, property, null, false);
+        return OWLFrameStoreUtils.convertValueListToRDFLiterals(owlModel, values);
+    }
+
+
+    public Collection getSlotValues(Instance instance, Slot slot) {
+        return mnfs.getValues(instance, slot, null, false);
+    }
+
+
+    public TripleStore getTripleStore(String name) {
+        if (name == null) {
+            return getActiveTripleStore();
+        }
+        for (Iterator<TripleStore> it = ts.iterator(); it.hasNext();) {
+            TripleStore tripleStore = it.next();
+            if (name.equals(tripleStore.getName())) {
+                return tripleStore;
+            }
+        }
+        return null;
+    }
+
+
+    public TripleStore getTripleStore(int index) {
+        return (TripleStore) getTripleStores().get(index);
+    }
+
+
+    public List<TripleStore> getTripleStores() {
+        return new ArrayList<TripleStore>(ts);
+    }
+
+
+    public TripleStore getTopTripleStore() {
+        if (mnfs == null && ts.size() == 1) {
+            /*
+             * Server-client mode and the server is using file mode.
+             */
+            return ts.get(0);
+        }
+        else {
+            /*
+             * Either standalone or Server-client and the server is using database mode.
+             */
+            return ts.get(1);
+        }
+    }
+
+
+    public boolean isActiveTriple(RDFResource subject, RDFProperty predicate, Object object) {
+        return getActiveTripleStore().contains(subject, predicate, object);
+    }
+
+
+    public boolean isEditableTriple(RDFResource subject, RDFProperty predicate, Object object) {
+        object = DefaultRDFSLiteral.getPlainValueIfPossible(object);
+        Iterator it = listUserTripleStores();
+        while (it.hasNext()) {
+            TripleStore ts = (TripleStore) it.next();
+            if (ts.contains(subject, predicate, object)) {
+                return isEditableTripleStore(ts);
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isEditableTripleStore(TripleStore tripleStore) {
+        int index = ts.indexOf(tripleStore);
+        
+        //TT: This has to be checked whether it is working right.
+        if (mnfs == null) {
+            /**
+             * Probably a client talking to a server.
+             */
+            if (index == ts.size() - 1) {
+                return true;
+            } else  {
+                return false;
+            }
+        }
+                
+        if (index == 0) {
+            return false;
+        }
+        if (index == 1) {
+            return true;
+        }
+        try {
+            URI uri = new URI(tripleStore.getName());
+            return owlModel.getRepositoryManager().getRepository(uri).isWritable(uri);
+            //return owlModel.getURIResolver().isEditableImport(uri);
+        }
+        catch (Exception ex) {
+        }
+        return false;
+    }
+
+
+    public Iterator<Triple> listTriplesWithSubject(RDFResource subject) {
+        List<Triple> result = new ArrayList<Triple>();
+        Iterator<TripleStore> it = getTripleStores().iterator();
+        while (it.hasNext()) {
+            TripleStore ts = (TripleStore) it.next();
+            Iterator<Triple> triples = ts.listTriplesWithSubject(subject);
+            while (triples.hasNext()) {
+                Triple triple = (Triple) triples.next();
+                result.add(triple);
+            }
+        }
+        return result.iterator();
+    }
+    
+    
+    public Iterator<RDFResource> listSubjects(RDFProperty property) {       
+         Set<RDFResource> result = new HashSet<RDFResource>();
+         
+         Iterator<TripleStore> it = getTripleStores().iterator();
+         while (it.hasNext()) {
+             TripleStore ts = (TripleStore) it.next();    
+             Iterator subjects = ts.listSubjects(property);
+             while (subjects.hasNext()) {
+                 Frame frame = (Frame) subjects.next();
+                 if (frame instanceof RDFResource) {
+                    result.add((RDFResource) frame);
+                 }
+             }
+         }
+         return result.iterator();
+    }
+
+
+    public Iterator listUserTripleStores() {
+        //TT: This has to be checked whether it is working right.
+        if (mnfs == null && ts.size() == 1) {
+            /**
+             * Probably a client talking to a server and server is using database mode.
+             */
+            return CollectionUtilities.createCollection(ts.get(0)).iterator();
+        }
+        
+        Iterator it = getTripleStores().iterator();
+        it.next(); // drop the system triple store.
+        return it;
+    }
+
+
+    public void replaceJavaObject(RDFResource subject) {
+        mnfs.replaceFrame(subject);
+    }
+
+
+    public void setActiveTripleStore(TripleStore tripleStore) {
+        if (mnfs == null && ts.size() == 1) {
+            /**
+             * Probably a client talking to a server and server is using database mode.
+             * When we will support database inclusion, we should fix this implementation
+             */
+            return;
+        }
+        if (mnfs.getActiveFrameStore() != tripleStore.getNarrowFrameStore()) {
+            mnfs.setActiveFrameStore(tripleStore.getNarrowFrameStore());
+        }
+    }
+
+
+    public void setHomeTripleStore(RDFResource resource, TripleStore tripleStore) {
+        TripleStore home = getHomeTripleStore(resource);
+        if (home != tripleStore) {
+            List values = mnfs.getValues(resource, nameSlot, null, false);
+            String name = (String) values.get(0);
+            home.getNarrowFrameStore().removeValue(resource, nameSlot, null, false, name);
+            tripleStore.getNarrowFrameStore().addValues(resource, nameSlot, null, false, Collections.singleton(name));
+        }
+    }
+
+
+    public void updateEditableResourceState() {
+        Slot nameSlot = ((KnowledgeBase) owlModel).getSlot(Model.Slot.NAME);
+        TripleStoreUtil.updateFrameInclusion(mnfs, nameSlot);
+    }
+    
+    public void dispose() {
+        for (TripleStore tripleStore : ts) {
+            tripleStore.dispose();
+        }
+        
+        if (mnfs != null) {
+            mnfs.close();
+        }
+        
+        ts.clear();
+        ts = null;
+    }
+    
+    
+    
+}
