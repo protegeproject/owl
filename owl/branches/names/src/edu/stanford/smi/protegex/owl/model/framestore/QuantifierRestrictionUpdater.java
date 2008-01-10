7b988cafdf7f5edc5f5fe60fda30f183a5e95458
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-10 20:39:13 +0000

    logging

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/framestore/QuantifierRestrictionUpdater.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/framestore/QuantifierRestrictionUpdater.java
@@ -1,19 +1,23 @@
 package edu.stanford.smi.protegex.owl.model.framestore;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.model.OWLAnonymousClass;
 import edu.stanford.smi.protegex.owl.model.OWLQuantifierRestriction;
 import edu.stanford.smi.protegex.owl.model.OWLUnionClass;
 import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-
 abstract class QuantifierRestrictionUpdater extends AbstractRestrictionUpdater {
+    private static final transient Logger log = Log.getLogger(QuantifierRestrictionUpdater.class);
 
     QuantifierRestrictionUpdater(AbstractOWLModel owlModel) {
         super(owlModel);
@@ -30,7 +34,9 @@ abstract class QuantifierRestrictionUpdater extends AbstractRestrictionUpdater {
                     RDFSClass allCls = (RDFSClass) restriction.getFiller();
                     if (allCls instanceof OWLAnonymousClass) {
                         if (clses.contains(allCls)) {
-                            log("& Clearing filler of " + restriction.getBrowserText());
+                            if (log.isLoggable(Level.FINE)) {
+                                log.fine("& Clearing filler of " + restriction.getBrowserText());
+                            }
                             clearFiller(restriction);
                         }
                         else if (allCls instanceof OWLUnionClass) {
@@ -38,8 +44,10 @@ abstract class QuantifierRestrictionUpdater extends AbstractRestrictionUpdater {
                             for (Iterator oit = new ArrayList(unionCls.getOperands()).iterator(); oit.hasNext();) {
                                 RDFSClass operand = (RDFSClass) oit.next();
                                 if (operand instanceof OWLAnonymousClass && clses.contains(operand)) {
-                                    log("& Clearing operand " + operand.getBrowserText() +
+                                    if (log.isLoggable(Level.FINE)) {
+                                        log.fine("& Clearing operand " + operand.getBrowserText() +
                                             " from: " + restriction.getBrowserText());
+                                    }
                                     unionCls.removeOperand(operand);
                                     //Collection newOperands = new ArrayList(unionCls.getOperands());
                                     //newOperands.remove(operand);
