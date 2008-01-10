27a0831d36cbefc41563f580ce097540d8b20a6f
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-10 20:48:09 +0000

    logging

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/DefaultProtegeOWLParserLogger.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/DefaultProtegeOWLParserLogger.java
@@ -1,5 +1,9 @@
 package edu.stanford.smi.protegex.owl.jena.parser;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
@@ -8,7 +12,7 @@ import edu.stanford.smi.protegex.owl.model.RDFResource;
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class DefaultProtegeOWLParserLogger implements ProtegeOWLParserLogger {
-
+    private static transient final Logger log = Log.getLogger(DefaultProtegeOWLParserLogger.class);
     private int count = 0;
 
 
@@ -20,24 +24,26 @@ public class DefaultProtegeOWLParserLogger implements ProtegeOWLParserLogger {
         if (!uri.equals(physicalURL)) {
             msg += " (from " + physicalURL + ")";
         }       
-        Log.getLogger().info(msg);
+        log.info(msg);
     }
 
 
     public void logTripleAdded(RDFResource subject, RDFProperty predicate, Object object) {
-        //if(object instanceof Frame) {
-        // System.out.println(" + " + subject.getName() + " " + predicate.getName() + " " + ((Frame)object).getName());
-        //}
-        //else {
-        // System.out.println(" + " + subject.getName() + " " + predicate.getName() + " " + object);
-        //}
+        if (log.isLoggable(Level.FINER)) {
+            if(object instanceof Frame) {
+                log.finer(" + " + subject.getName() + " " + predicate.getName() + " " + ((Frame)object).getName());
+            }
+            else {
+                log.finer(" + " + subject.getName() + " " + predicate.getName() + " " + object);
+            }
+        }
         if ((++count % 10000) == 0) {
-            Log.getLogger().info(PREFIX + "Triple " + count);
+            log.info(PREFIX + "Triple " + count);
         }
     }
 
 
     public void logWarning(String message) {
-        Log.getLogger().warning(PREFIX + "Warning: " + message);
+        log.warning(PREFIX + "Warning: " + message);
     }
 }
