f0c9571cb3054aeb4ceba3aff9a24ef4b501c6b7
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-20 00:10:05 +0000

    rearranged and accounted for the fact that the creation of the imported triple store puts one in the context of the imported triple store

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/repository/impl/AbstractStreamBasedRepositoryImpl.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/repository/impl/AbstractStreamBasedRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.net.URI;
 
 import edu.stanford.smi.protege.model.framestore.InMemoryFrameDb;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
+import edu.stanford.smi.protege.util.URIUtilities;
 import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
@@ -24,22 +25,25 @@ public abstract class AbstractStreamBasedRepositoryImpl implements Repository {
      */
     public abstract InputStream getInputStream(URI ontologyName) throws IOException;
     
-    public void addImport(OWLModel owlModel, URI ontologyName) throws IOException {
-        TripleStore importingTripleStore = owlModel.getTripleStoreModel().getActiveTripleStore();
-        NarrowFrameStore frameStore = new InMemoryFrameDb(ontologyName.toString());
-        TripleStore importedTripleStore = owlModel.getTripleStoreModel().createTripleStore(frameStore);
+    public TripleStore addImport(OWLModel owlModel, URI ontologyName) throws IOException {
         ProtegeOWLParser parser = new ProtegeOWLParser(owlModel, true);
         parser.setImporting(true);
         InputStream is = getInputStream(ontologyName);
         URI xmlBase = XMLBaseExtractor.getXMLBase(getInputStream(ontologyName));
-        
+        if (xmlBase == null) {
+            xmlBase = ontologyName;
+        }
+        TripleStore importedTripleStore = null;
+        TripleStore importingTripleStore = owlModel.getTripleStoreModel().getActiveTripleStore();
         try {
-            owlModel.getTripleStoreModel().setActiveTripleStore(importedTripleStore);
+            NarrowFrameStore frameStore = new InMemoryFrameDb(ontologyName.toString());
+            importedTripleStore = owlModel.getTripleStoreModel().createActiveImportedTripleStore(frameStore);
             parser.loadTriples(ontologyName.toString(), xmlBase, is);
         } finally {
             owlModel.getTripleStoreModel().setActiveTripleStore(importingTripleStore);
         }
         owlModel.getOWLFrameStore().copyFacetValuesIntoNamedClses();
+        return importedTripleStore;
     }
 
 }
