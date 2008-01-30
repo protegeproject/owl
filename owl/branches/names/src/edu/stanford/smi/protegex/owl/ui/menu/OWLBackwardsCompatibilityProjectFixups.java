92f058c9a312729f1e359c5fcc09daa754ef5e57
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-30 03:44:26 +0000

    refactored to work in a more general setting

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/menu/OWLBackwardsCompatibilityProjectFixups.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/menu/OWLBackwardsCompatibilityProjectFixups.java
@@ -5,9 +5,13 @@ import edu.stanford.smi.protege.resource.Files;
 import edu.stanford.smi.protege.storage.clips.ClipsKnowledgeBaseFactory;
 import edu.stanford.smi.protege.util.Assert;
 import edu.stanford.smi.protege.util.FileUtilities;
+import edu.stanford.smi.protege.util.PropertyList;
 import edu.stanford.smi.protege.util.SystemUtilities;
 import edu.stanford.smi.protege.widget.*;
 import edu.stanford.smi.protegex.owl.model.*;
+import edu.stanford.smi.protegex.owl.resource.OWLText;
+import edu.stanford.smi.protegex.owl.swrl.SWRLSystemFrames;
+import edu.stanford.smi.protegex.owl.swrl.model.SWRLNames;
 import edu.stanford.smi.protegex.owl.ui.cls.OWLClassesTab;
 import edu.stanford.smi.protegex.owl.ui.individuals.OWLIndividualsTab;
 import edu.stanford.smi.protegex.owl.ui.widget.*;
@@ -27,29 +31,133 @@ import java.util.Iterator;
 public class OWLBackwardsCompatibilityProjectFixups {
 
 
-    private static void changeInstanceValue(KnowledgeBase kb,
-                                            String className,
-                                            String slotNameToCheck,
-                                            String slotValue,
-                                            String slotNameToChange,
-                                            Object oldValue,
-                                            Object newValue) {
-        Cls cls = kb.getCls(className);
-        Slot slotToCheck = kb.getSlot(slotNameToCheck);
-        Slot slotToChange = kb.getSlot(slotNameToChange);
-        Iterator i = cls.getInstances().iterator();
-        while (i.hasNext()) {
-            Instance instance = (Instance) i.next();
-            if (slotValue.equals(instance.getOwnSlotValue(slotToCheck))
-                    && oldValue.equals(instance.getOwnSlotValue(slotToChange))) {
-                instance.setOwnSlotValue(slotToChange, newValue);
-                // Log.trace("set " + slotNameToChange + " to " + newValue, Project.class, "changeInstanceValue");
+    public static final String OWL_BUILD_PROPERTY = "owl_build";  // this implementation was muffed and I am not going to fix it.
+    public static final String OWL_MAJOR_BUILD_PROPERTY = "owl_major_build";
+    public static final String OWL_DATABASE_INCLUSION = "parser_namespace_database_inclusion_version";
+    
+    public static void insertVersionData(PropertyList sources) {
+        sources.setInteger(OWL_BUILD_PROPERTY, OWLText.getBuildNumber());
+        sources.setString(OWL_MAJOR_BUILD_PROPERTY, OWLText.getVersion());
+        sources.setBoolean(OWL_DATABASE_INCLUSION, true);
+    }
+
+    public static void fix(OWLModel owlModel) {
+        Project project = owlModel.getProject();
+        PropertyList sources = project.getSources();
+        KnowledgeBase systemKb = project.getInternalProjectKnowledgeBase();
+        SWRLSystemFrames systemFrames = owlModel.getSystemFrames();
+        
+        if (isReallyAncientVersion(sources)) {
+            fixReallyAncient(owlModel);
+        }
+        if (isBeforeDatabaseInclusion(sources)) {
+            fixForDatabaseInclusion(systemKb, systemFrames);
+        }
+    }
+    
+    /*
+     * =================================================================================================
+     *    Database inclusion fixups...
+     */
+    public static boolean isBeforeDatabaseInclusion(PropertyList sources) {
+        return sources.getBoolean(OWL_DATABASE_INCLUSION) == null;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static void fixForDatabaseInclusion(KnowledgeBase systemKb, SWRLSystemFrames systemFrames) {
+        adjustNamespacePrefix(systemKb);
+        systemFrames.getRdfsNamedClassClass().isVisible();
+        markFramesInvisible(systemFrames);
+    }
+    
+    private static void markFramesInvisible(SWRLSystemFrames systemFrames) {
+        Frame[] invisibleFrames = new Frame[] {
+            systemFrames.getRdfFirstProperty(), systemFrames.getRdfListClass(), 
+            systemFrames.getRdfObjectProperty(), systemFrames.getRdfPredicateProperty(), systemFrames.getRdfPropertyClass(),
+            systemFrames.getRdfRestProperty(), systemFrames.getRdfStatementClass(), systemFrames.getRdfSubjectProperty(),
+            systemFrames.getRdfTypeProperty(), systemFrames.getRdfValueProperty(),
+            
+            systemFrames.getRdfsNamedClassClass(), systemFrames.getRdfsContainerClass(), systemFrames.getRdfsDatatypeClass(),
+            systemFrames.getRdfsLiteralClass(), systemFrames.getRdfsMemberProperty(),
+            
+            systemFrames.getOwlNamedClassClass(),
+            
+            systemFrames.getAtomCls(), systemFrames.getAtomListCls(), systemFrames.getBuiltInCls(),
+            systemFrames.getBuiltinAtomCls(), systemFrames.getClassAtomCls(), systemFrames.getDataRangeAtomCls(),
+            systemFrames.getDataValuedPropertyAtomCls(), systemFrames.getDifferentIndividualsAtomCls(),
+            systemFrames.getImpCls(), systemFrames.getIndividualPropertyAtomCls(), systemFrames.getSameIndividualAtomCls(),
+            systemFrames.getVariableCls()
+        };
+       
+        for (Frame cls : invisibleFrames)  {
+            cls.setVisible(false);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static void adjustNamespacePrefix(KnowledgeBase systemKb) {
+        for (Instance instance : systemKb.getInstances()) {
+            for (Slot slot : instance.getOwnSlots()) {
+                Collection values = instance.getDirectOwnSlotValues(slot);
+                adjustNamespacePrefix(instance, slot,values);
             }
         }
     }
-
-
-    public static void fix(OWLModel owlModel) {
+    
+    @SuppressWarnings("unchecked")
+    private static void adjustNamespacePrefix(Instance instance, Slot slot, Collection values) {
+        Collection fixedValues = new ArrayList();
+        boolean needsFixing = false;
+        for (Object value : values) {
+            if (value instanceof String) {
+                fixedValues.add(adjustNamespacePrefix((String) value));
+                needsFixing = true;
+            }
+            else {
+                fixedValues.add(value);
+            }
+        }
+        if (needsFixing) {
+            instance.setDirectOwnSlotValues(slot, fixedValues);
+        }
+    }
+    
+    private static String adjustNamespacePrefix(String value) {
+        String prefix;
+        prefix = RDFNames.RDF_PREFIX + ":";
+        if  (value.startsWith(prefix)) {
+            return replacePrefix(value, prefix, RDFNames.RDF_NAMESPACE);
+        }
+        prefix = RDFSNames.RDFS_PREFIX + ":";
+        if (value.startsWith(prefix)) {
+            return replacePrefix(value, prefix,  RDFSNames.RDFS_NAMESPACE);
+        }
+        prefix = OWLNames.OWL_PREFIX + ":";
+        if (value.startsWith(prefix)) {
+            return replacePrefix(value, prefix, OWLNames.OWL_NAMESPACE);
+        }
+        prefix = SWRLNames.SWRL_PREFIX + ":";
+        if (value.startsWith(prefix)) {
+            return replacePrefix(value, prefix, SWRLNames.SWRL_NAMESPACE);
+        }
+        return value;
+    }
+    
+    private static String replacePrefix(String value, String prefix, String namespace) {
+        return namespace + value.substring(prefix.length());
+    }
+    
+    /*
+     * =================================================================================================
+     *    Really ancient fixups
+     */
+    
+    public static boolean isReallyAncientVersion(PropertyList sources) {
+        return sources.getInteger(OWLBackwardsCompatibilityProjectFixups.OWL_BUILD_PROPERTY) == null;
+    }
+    
+    
+    private static void fixReallyAncient(OWLModel owlModel) {
         KnowledgeBase systemKB = owlModel.getProject().getInternalProjectKnowledgeBase();
         updateStandardForms(systemKB);
         renameWidget(systemKB, "edu.stanford.smi.protegex.owl.ui.cls.OWLClsesTab", OWLClassesTab.class.getName());
@@ -93,6 +201,29 @@ public class OWLBackwardsCompatibilityProjectFixups {
             resource.setVisible(false);
         }
     }
+
+    private static void changeInstanceValue(KnowledgeBase kb,
+                                            String className,
+                                            String slotNameToCheck,
+                                            String slotValue,
+                                            String slotNameToChange,
+                                            Object oldValue,
+                                            Object newValue) {
+        Cls cls = kb.getCls(className);
+        Slot slotToCheck = kb.getSlot(slotNameToCheck);
+        Slot slotToChange = kb.getSlot(slotNameToChange);
+        Iterator i = cls.getInstances().iterator();
+        while (i.hasNext()) {
+            Instance instance = (Instance) i.next();
+            if (slotValue.equals(instance.getOwnSlotValue(slotToCheck))
+                    && oldValue.equals(instance.getOwnSlotValue(slotToChange))) {
+                instance.setOwnSlotValue(slotToChange, newValue);
+                // Log.trace("set " + slotNameToChange + " to " + newValue, Project.class, "changeInstanceValue");
+            }
+        }
+    }
+
+
 
 
     private static Instance getClsWidgetInstance(String name, KnowledgeBase kb) {
@@ -196,4 +327,6 @@ public class OWLBackwardsCompatibilityProjectFixups {
             // Log.trace(widgetClsName + " form changed= " + changed, BackwardsCompatibilityProjectFixups.class, "updateStandardForms");
         }
     }
+
+
 }
