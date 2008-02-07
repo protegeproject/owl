d6febc90e63f1e8c1cb787ed73524fb73bdfcb66
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-07 00:19:37 +0000

    added getFullName method

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/NamespaceUtil.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/NamespaceUtil.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protegex.owl.model;
 import com.hp.hpl.jena.rdf.model.impl.Util;
 
 import edu.stanford.smi.protegex.owl.model.impl.OWLNamespaceManager;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
 
 public class NamespaceUtil {
 
@@ -31,13 +32,50 @@ public class NamespaceUtil {
 		String localName = getLocalName(fullURI);
 		
 		if (prefix == null) {
-			return fullURI;
+			return fullURI; //do we want another strategy here?
 		} else if (prefix.equals(OWLNamespaceManager.DEFAULT_NAMESPACE_PREFIX)) {
 			return localName;
 		} else {
 			return prefix + ":" + localName; 
-		}
+		}			
+	}
+	
+	public static String getFullName(OWLModel owlModel, String prefixedName) {
+		int ind = prefixedName.indexOf(":");
+		
+		NamespaceManager nsm = owlModel.getNamespaceManager();
+		
+		if (ind == -1) { //no ":" in the prefixed name, this is a name in the default namespace
+			String defaultNamespace = nsm.getDefaultNamespace();
 			
+			if (defaultNamespace == null) { 
+				return null;
+			}
+			
+			return defaultNamespace + prefixedName;			
+		}
+						
+		if (ind == 0) {//e.g. ":protege" - what to do in this case?
+			//TODO: what should happen in this case?
+			return null;
+		}
+		
+		if (ind == prefixedName.length() - 1) { //e.g. "p1:"
+			//TODO: what should happen in this case?
+			return null;
+		}
+		
+		String prefix = prefixedName.substring(0, ind);
+		String localName = prefixedName.substring(ind + 1);
+		
+		String namespace = nsm.getNamespaceForPrefix(prefix);
+		
+		if (namespace == null) { // no namespace for prefix defined
+			return null;
+		}
+		
+		return namespace + localName;
+				
 	}
 	
 }
