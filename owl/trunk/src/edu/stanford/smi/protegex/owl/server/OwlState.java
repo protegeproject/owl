f12466da1ab6c7801673f4a02bbbdbca9daf2ba6
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-08 02:18:01 +0000

    fixed a race condition and added the transition to UserIndividual

--- owl/trunk/src/edu/stanford/smi/protegex/owl/server/OwlState.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/server/OwlState.java
@@ -4,24 +4,29 @@ package edu.stanford.smi.protegex.owl.server;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.framestore.FrameStore;
 import edu.stanford.smi.protege.server.framestore.background.ServerCachedState;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
 
 
 
 public enum OwlState implements ServerCachedState {
-  Start, SubClass, OwlExpr, RDFList, End;
+  Start, SubClass, OwlExpr, RDFList, UserIndividual, End;
   
-  public boolean entryCondition(FrameStore fs, Frame f) {
-    switch (this) {
+  public static boolean allowTransition(FrameStore fs, OwlState startState, Frame startFrame, OwlState endState, Frame endingFrame) {
+    switch (endState) {
     case OwlExpr:
-      return isOWLAnonymous(fs, f);
+      return isOWLAnonymous(fs, endingFrame);
     case SubClass:
-      return !isOWLAnonymous(fs, f);
+      return !isOWLAnonymous(fs, endingFrame);
+    case UserIndividual:
+        return startFrame instanceof OWLNamedClass && !isOWLAnonymous(fs, startFrame) && !startFrame.isSystem();
     default:
       return true;
     }
   }
   
-  private boolean isOWLAnonymous(FrameStore fs, Frame f) {
-    return fs.getFrameName(f).startsWith("@");
+  private static boolean isOWLAnonymous(FrameStore fs, Frame f) {
+      synchronized (f.getKnowledgeBase()) {
+          return fs.getFrameName(f).startsWith("@");
+      }
   }
 }
