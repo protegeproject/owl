621f97374815c2addb63aba3a62f1b7c3b3e8237
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-12 07:30:16 +0000

    modified the wizard to get the ontology name

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/database/OWLDatabaseWizardPage.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/database/OWLDatabaseWizardPage.java
@@ -1,6 +1,16 @@
 package edu.stanford.smi.protegex.owl.database;
 
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Box;
+import javax.swing.JTextField;
+
 import edu.stanford.smi.protege.storage.database.DatabaseWizardPage;
+import edu.stanford.smi.protege.util.ComponentFactory;
+import edu.stanford.smi.protege.util.LabeledComponent;
 import edu.stanford.smi.protege.util.Wizard;
 
 /**
@@ -8,14 +18,59 @@ import edu.stanford.smi.protege.util.Wizard;
  */
 public class OWLDatabaseWizardPage extends DatabaseWizardPage {
 
-    private boolean fromExistingSources;
-
     private OWLDatabasePlugin plugin;
+    
+    private JTextField ontologyName;
 
 
-    public OWLDatabaseWizardPage(Wizard wizard, OWLDatabasePlugin plugin, boolean fromExistingSources) {
+    public OWLDatabaseWizardPage(Wizard wizard, OWLDatabasePlugin plugin) {
         super(wizard, plugin);
-        this.fromExistingSources = fromExistingSources;
         this.plugin = plugin;
+    }
+    
+    @Override
+    protected void layoutComponents(Box panel) {
+        if (!getFromExistingSources()) {
+            ontologyName = ComponentFactory.createTextField("Enter the ontology name as a URL");
+            ontologyName.addFocusListener(new FocusAdapter() {
+                @Override
+                public void focusLost(FocusEvent event) {
+                    updateSetPageComplete();
+                }
+            });
+            panel.add(new LabeledComponent("Ontology Name", ontologyName));
+        }
+        super.layoutComponents(panel);
+    }
+    
+    @Override
+    protected boolean isComplete() {
+        if (getFromExistingSources()) {
+            return super.isComplete();  
+        }
+        return super.isComplete() && isValidURL();
+    }
+    
+    private boolean isValidURL() {
+        try {
+            new URL(ontologyName.getText());
+            return true;
+        }
+        catch (MalformedURLException e) {
+            setErrorText("Need valid URL for ontology name: " + e);
+            return false;
+        }
+    }
+    
+    @Override
+    public void onFinish() {
+        if (!getFromExistingSources()) {
+            plugin.setOntologyName(ontologyName.getText());
+        }
+        super.onFinish();
+    }
+    
+    public boolean getFromExistingSources() {
+        return false;
     }
 }
