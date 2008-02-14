a36c99a210f0aadfc08bb1995d556e2bd60e0a65
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-14 00:16:47 +0000

    refactored utilities from several sources into here.

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/factory/FactoryUtils.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/factory/FactoryUtils.java
@@ -1,9 +1,11 @@
 package edu.stanford.smi.protegex.owl.model.factory;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.logging.Level;
 
+import edu.stanford.smi.protege.util.ApplicationProperties;
 import edu.stanford.smi.protegex.owl.database.DatabaseFactoryUtils;
 import edu.stanford.smi.protegex.owl.database.OWLDatabaseKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.model.NamespaceManager;
@@ -12,8 +14,31 @@ import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.OWLOntology;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStoreModel;
+import edu.stanford.smi.protegex.owl.ui.metadatatab.OntologyURIPanel;
 
 public class FactoryUtils {
+    
+    public static void addOntologyToTripleStore(OWLModel owlModel, TripleStore tripleStore, String ontologyName) {
+        TripleStoreModel tripleStoreModel = owlModel.getTripleStoreModel();
+        TripleStore activeTripleStore = tripleStoreModel.getActiveTripleStore();
+        try {
+            tripleStoreModel.setActiveTripleStore(tripleStore);
+            owlModel.getOWLOntologyClass().createInstance(ontologyName);
+            tripleStore.setName(ontologyName);
+            NamespaceManager names = tripleStore.getNamespaceManager();
+            if (names.getDefaultNamespace() == null) {
+                names.setDefaultNamespace(ontologyName + "#");
+            }
+            if (tripleStore.getOriginalXMLBase() == null) {
+                tripleStore.setOriginalXMLBase(ontologyName);
+            }
+        }
+        finally {
+            tripleStoreModel.setActiveTripleStore(activeTripleStore);
+        }
+        owlModel.resetOntologyCache();
+    }
 
     public static void loadEncodedNamespaceFromModel(OWLModel owlModel, TripleStore tripleStore, Collection errors) {
         OWLOntology ontology = tripleStore.getOWLOntology();
@@ -81,6 +106,46 @@ public class FactoryUtils {
 
     public static String joinEncodedNamespaceEntry(String prefix, String namespace) {
         return prefix + OWLDatabaseKnowledgeBaseFactory.NAMESPACE_PREFIX_SEPARATOR + namespace;
+    }
+
+    public static String getOntologyURIBase(String defaultBase,
+                                            boolean appendYear,
+                                            boolean appendMonth,
+                                            boolean appendDay) {
+        if (defaultBase != null && defaultBase.trim().length() > 0) {
+            if (defaultBase.endsWith("/") == false) {
+                defaultBase += "/";
+            }
+            if (appendYear) {
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+                defaultBase += year + "/";
+                if (appendMonth) {
+                    int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+                    defaultBase += month + "/";
+                    if (appendDay) {
+                        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                        defaultBase += day + "/";
+                    }
+                }
+            }
+        }
+        return defaultBase;
+    }
+
+    public static String generateOntologyURIBase() {
+        String defaultBase = ApplicationProperties.getString(OntologyURIPanel.URI_BASE_PROPERTY);
+        if (defaultBase == null) {
+            defaultBase = OntologyURIPanel.DEFAULT_BASE;
+            ApplicationProperties.setString(OntologyURIPanel.URI_BASE_PROPERTY, OntologyURIPanel.DEFAULT_BASE);
+        }
+        defaultBase = getOntologyURIBase(defaultBase,
+                                         ApplicationProperties.getBooleanProperty(OntologyURIPanel.URI_BASE_APPEND_YEAR_PROPERTY, false),
+                                         ApplicationProperties.getBooleanProperty(OntologyURIPanel.URI_BASE_APPEND_MONTH_PROPERTY, false),
+                                         ApplicationProperties.getBooleanProperty(OntologyURIPanel.URI_BASE_APPEND_DAY_PROPERTY, false));
+        String fileName = "Ontology";
+        fileName += System.currentTimeMillis() / 1000;
+        fileName += ".owl";
+        return defaultBase + fileName;
     }
 
 }
