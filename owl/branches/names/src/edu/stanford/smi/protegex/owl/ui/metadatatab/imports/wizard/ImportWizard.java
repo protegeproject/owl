0d4b29c828d6e3acbda43f9fd9fabf2b99c7e474
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-15 20:52:08 +0000

    main method throws an io exception for owl project creation

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/metadatatab/imports/wizard/ImportWizard.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/metadatatab/imports/wizard/ImportWizard.java
@@ -1,10 +1,12 @@
 package edu.stanford.smi.protegex.owl.ui.metadatatab.imports.wizard;
 
+import java.io.IOException;
+
+import javax.swing.JComponent;
+
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.ui.wizard.OWLWizard;
-
-import javax.swing.*;
 
 /**
  * User: matthewhorridge<br>
@@ -44,7 +46,7 @@ public class ImportWizard extends OWLWizard {
 		importData = new ImportWizardData();
 	}
 
-	public static void main(String  [] args) {
+	public static void main(String  [] args) throws IOException {
 		OWLModel owlModel = ProtegeOWL.createJenaOWLModel();
 		ImportWizard wizard = new ImportWizard(null, owlModel);
 		wizard.show();
