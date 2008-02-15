e4b4ca8b3e7c00269702f6cf8f75f54b8acedb37
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-15 20:58:26 +0000

    harmless

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/database/OWLDatabasePlugin.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/database/OWLDatabasePlugin.java
@@ -8,8 +8,21 @@ import edu.stanford.smi.protege.storage.database.DatabasePlugin;
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public interface OWLDatabasePlugin extends DatabasePlugin {
-
-    void setOntologyFileURI(URI uri);
     
     void setOntologyName(String name);
+    
+    String getUrl();
+    
+    String getDriver();
+
+
+    String getTable();
+
+
+    String getUsername();
+
+
+    String getPassword();
+
+    String getOntologyName();
 }
