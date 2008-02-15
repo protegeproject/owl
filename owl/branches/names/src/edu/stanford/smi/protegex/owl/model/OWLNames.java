a9644aaf488afef7a635652b6cd6c0a266216b26
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-15 07:49:36 +0000

    updated names

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
@@ -141,11 +141,11 @@ public interface OWLNames {
         
         //Non-OWL slots that are used internally by Protege
         
-        public final static String ONTOLOGY_PREFIXES = ":OWL-ONTOLOGY-PREFIXES";
+        public final static String ONTOLOGY_PREFIXES = ProtegeNames.NS + "OWL-ONTOLOGY-PREFIXES";
                       
-        public final static String RESOURCE_URI = ":OWL-RESOURCE-URI";
+        public final static String RESOURCE_URI = ProtegeNames.NS + "OWL-RESOURCE-URI";
         
-        public final static String OWL_ONTOLOGY_POINTER_PROPERTY = ":OWL-ONTOLOGY-POINTER-PROPERTY";
+        public final static String OWL_ONTOLOGY_POINTER_PROPERTY = ProtegeNames.NS + "OWL-ONTOLOGY-POINTER-PROPERTY";
         
     }
 
