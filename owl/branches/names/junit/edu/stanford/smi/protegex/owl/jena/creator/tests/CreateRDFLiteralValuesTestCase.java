39ac59985ea30c8f937fead5b43be9a37adc0f26
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-27 05:32:09 +0000

    fixed one of them

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/creator/tests/CreateRDFLiteralValuesTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/creator/tests/CreateRDFLiteralValuesTestCase.java
@@ -1,29 +1,42 @@
 package edu.stanford.smi.protegex.owl.jena.creator.tests;
 
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.Slot;
-import edu.stanford.smi.protegex.owl.model.*;
-
-import java.net.URI;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.jena.Jena;
+import edu.stanford.smi.protegex.owl.model.NamespaceUtil;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.RDFNames;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSNames;
 
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class CreateRDFLiteralValuesTestCase extends AbstractJenaCreatorTestCase {
+    private static final transient Logger log = Log.getLogger(CreateRDFLiteralValuesTestCase.class);
 
     public void testRDFValueInRDFLiteral() throws Exception {
         loadTestOntology(new URI("http://www.daml.org/2004/04/swrl/swrl.owl"));
-        OWLNamedClass datavaluedPropertyAtomCls = (OWLNamedClass) owlModel.getCls("swrl:DatavaluedPropertyAtom");
+        String defaultNamespace = owlModel.getNamespaceManager().getDefaultNamespace();
+        
+        OWLNamedClass datavaluedPropertyAtomCls = (OWLNamedClass) owlModel.getCls(getFullName("swrl:DatavaluedPropertyAtom"));
         assertNotNull(datavaluedPropertyAtomCls);
-        RDFProperty argument2Property = (RDFProperty) owlModel.getSlot("swrl:argument2");
+        RDFProperty argument2Property = (RDFProperty) owlModel.getSlot(getFullName("swrl:argument2"));
         assertNotNull(argument2Property);
-        RDFResource atom = (RDFResource) datavaluedPropertyAtomCls.createInstance("atom");
+        RDFResource atom = datavaluedPropertyAtomCls.createInstance(defaultNamespace + "atom");
         Cls literalCls = owlModel.getCls(RDFSNames.Cls.LITERAL);
-        Instance literal = literalCls.createDirectInstance("myLiteral");
+        Instance literal = literalCls.createDirectInstance(defaultNamespace  + "myLiteral");
         Slot valueSlot = owlModel.getSlot(RDFNames.Slot.VALUE);
         final String value = "aldi";
         literal.setDirectOwnSlotValue(valueSlot, value);
@@ -33,18 +46,30 @@ public class CreateRDFLiteralValuesTestCase extends AbstractJenaCreatorTestCase 
 
 
     public void testValuesOfImportedProperties() throws Exception {
+        log.setLevel(Level.FINE);
+        
+        // ProtegeOWLPluginFolderRepository.setDefaultOlwRepositoryFolder(new File("etc"));
         loadRemoteOntology("importSWRL.owl");
-        OWLNamedClass datavaluedPropertyAtomCls = (OWLNamedClass) owlModel.getCls("swrl:DatavaluedPropertyAtom");
+        String defaultOntology = owlModel.getNamespaceManager().getDefaultNamespace();
+        
+        OWLNamedClass datavaluedPropertyAtomCls = (OWLNamedClass) owlModel.getCls(getFullName("swrl:DatavaluedPropertyAtom"));
         assertNotNull(datavaluedPropertyAtomCls);
-        RDFProperty argument2RDFProperty = (RDFProperty) owlModel.getSlot("swrl:argument2");
+        RDFProperty argument2RDFProperty = (RDFProperty) owlModel.getSlot(getFullName("swrl:argument2"));
         assertNotNull(argument2RDFProperty);
-        RDFResource atom = (RDFResource) datavaluedPropertyAtomCls.createInstance("atom");
+        RDFResource atom = datavaluedPropertyAtomCls.createInstance(defaultOntology + "atom");
         atom.addPropertyValue(argument2RDFProperty, "aldi");
+        
         OntModel newModel = runJenaCreator();
+        Jena.dumpRDF(newModel, log, Level.FINE);
+        
         Resource atomResource = newModel.getResource(atom.getURI());
         assertNotNull(atomResource);
         Property argument2OntProperty = newModel.getProperty(argument2RDFProperty.getURI());
         assertNotNull(argument2OntProperty);
         assertSize(1, atomResource.listProperties(argument2OntProperty));
+    }
+    
+    private String getFullName(String shortName) {
+        return NamespaceUtil.getFullName(owlModel, shortName);
     }
 }
