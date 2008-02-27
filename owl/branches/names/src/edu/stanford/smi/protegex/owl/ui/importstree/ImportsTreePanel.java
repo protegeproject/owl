799e28e7e7728c6b2c1d2579c02b2e27eaa53592
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-27 01:00:17 +0000

    Refactor of ProtegeNames.PROTEGE_OWL*

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/importstree/ImportsTreePanel.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/ui/importstree/ImportsTreePanel.java
@@ -217,7 +217,7 @@ public class ImportsTreePanel extends JPanel implements HostResourceDisplay, Dis
                 importHelper.importOntologies();
                 
                 //TODO: This should be moved in the import code
-                owlModel.getNamespaceManager().setPrefix(ProtegeNames.NS, ProtegeNames.PROTEGE_PREFIX);
+                owlModel.getNamespaceManager().setPrefix(ProtegeNames.PROTEGE_OWL_NAMESPACE, ProtegeNames.PROTEGE_PREFIX);
                 
                 Collection addedPrefixes = new ArrayList(owlModel.getNamespaceManager().getPrefixes());
                 addedPrefixes.removeAll(prefixes);
