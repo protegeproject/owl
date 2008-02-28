302de55d1a94db9be955871ca16b15d0076ec144
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-28 00:40:25 +0000

    debug

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/creator/tests/CreateAllDifferentTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/creator/tests/CreateAllDifferentTestCase.java
@@ -1,9 +1,15 @@
 package edu.stanford.smi.protegex.owl.jena.creator.tests;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.hp.hpl.jena.ontology.AllDifferent;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Resource;
+
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.jena.Jena;
 import edu.stanford.smi.protegex.owl.model.OWLAllDifferent;
 import edu.stanford.smi.protegex.owl.model.OWLIndividual;
 import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
@@ -12,6 +18,7 @@ import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class CreateAllDifferentTestCase extends AbstractJenaCreatorTestCase {
+    private static transient Logger log = Log.getLogger(CreateAllDifferentTestCase.class);
 
     public void testCreateAllDifferent() {
         OWLNamedClass colorClass = owlModel.createOWLNamedClass("Color");
@@ -22,6 +29,7 @@ public class CreateAllDifferentTestCase extends AbstractJenaCreatorTestCase {
         owlAllDifferent.addDistinctMember(blue);
 
         OntModel ontModel = runJenaCreator();
+        Jena.dumpRDF(ontModel, log, Level.FINE);
 
         Individual redIndividual = ontModel.getIndividual(red.getURI());
         Individual blueIndividual = ontModel.getIndividual(blue.getURI());
