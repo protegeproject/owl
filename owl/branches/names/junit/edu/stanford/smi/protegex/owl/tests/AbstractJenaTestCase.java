dc5f85f67fcffc7b55ad6362e8d0059282568b2e
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-29 16:21:07 +0000

    fixed  the problem that owl thing can be lost on a loadOntology and added a debug statement for reload

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/tests/AbstractJenaTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/tests/AbstractJenaTestCase.java
@@ -6,11 +6,14 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntResource;
 import com.hp.hpl.jena.rdf.model.Model;
 
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
 import edu.stanford.smi.protegex.owl.jena.Jena;
 import edu.stanford.smi.protegex.owl.jena.JenaOWLModel;
@@ -23,6 +26,7 @@ import edu.stanford.smi.protegex.owl.jena.creator.OwlProjectFromUriCreator;
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public abstract class AbstractJenaTestCase extends AbstractOWLTestCase {
+    private static transient final Logger log = Log.getLogger(AbstractJenaTestCase.class);
 
     protected OntModel ontModel;
 
@@ -50,6 +54,8 @@ public abstract class AbstractJenaTestCase extends AbstractOWLTestCase {
         creator.setOntologyUri(uri.toString());
         owlModel = (JenaOWLModel) creator.create(errors).getKnowledgeBase();
         owlModel.setExpandShortNameInMethods(true);
+        project = owlModel.getProject();
+        owlThing = owlModel.getOWLThingClass();
     }
 
 
@@ -72,6 +78,11 @@ public abstract class AbstractJenaTestCase extends AbstractOWLTestCase {
         Jena.dumpRDF(ontModel, stream);
         String str = stream.toString();
         StringReader reader = new StringReader(str);
+        if (log.isLoggable(Level.FINE)) {
+            log.fine("Saved ontology to string");
+            log.fine(str);
+            log.fine("reloading...");
+        }
         OwlProjectFromReaderCreator creator = new OwlProjectFromReaderCreator();
         creator.setReader(reader);
         return (JenaOWLModel) creator.create(errors).getKnowledgeBase();
