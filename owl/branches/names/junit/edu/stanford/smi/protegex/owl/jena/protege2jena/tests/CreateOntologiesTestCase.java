05d9d9fddf2c5aca4808e20bd236429a3693a49c
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-29 22:55:48 +0000

    fixed the rename of the ontology and the check for the broken import

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/protege2jena/tests/CreateOntologiesTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/protege2jena/tests/CreateOntologiesTestCase.java
@@ -5,6 +5,8 @@ import com.hp.hpl.jena.ontology.Ontology;
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.vocabulary.OWL;
 
+import edu.stanford.smi.protegex.owl.model.impl.OWLUtil;
+
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
  */
@@ -32,7 +34,7 @@ public class CreateOntologiesTestCase extends AbstractProtege2JenaTestCase {
     public void testOntologyWithNiceNamespace() {
         owlModel.createOWLNamedClass("Cls");
         String namespace = "http://aldi.de/ont";
-        owlModel.getNamespaceManager().setDefaultNamespace(namespace + "#");
+        OWLUtil.renameOntology(owlModel, owlModel.getDefaultOWLOntology(), namespace);
         OntModel newModel = createOntModel();
         assertSize(1, newModel.listOntologies());
         Ontology ontology = (Ontology) newModel.listOntologies().next();
@@ -44,12 +46,13 @@ public class CreateOntologiesTestCase extends AbstractProtege2JenaTestCase {
         owlModel.getDefaultOWLOntology().addImports(getRemoteOntologyRoot() + "travel.owl");
         owlModel.createOWLNamedClass("Cls");
         String namespace = "http://aldi.de/ont/";
-        owlModel.getNamespaceManager().setDefaultNamespace(namespace);
+        OWLUtil.renameOntology(owlModel, owlModel.getDefaultOWLOntology(), namespace);
         owlModel = reload(owlModel);
         OntModel newModel = createOntModel();
         assertSize(2, newModel.listOntologies());
         Ontology ontology = (Ontology) newModel.getOntology(namespace);
         assertEquals(namespace, ontology.getURI());
-        assertSize(1, ontology.listImports());
+        assertSize(0, ontology.listImports());                                                           // the ontology import is broken but
+        assertNotNull(owlModel.getOWLNamedClass("http://www.owl-ontologies.com/travel.owl#Sunbathing")); // the data has arrived
     }
 }
