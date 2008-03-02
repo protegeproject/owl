a104f947151c72286914e33d3c35120853e889dc
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-02 01:11:13 +0000

    modified to ensure that anonymous class names generated during the parse cannot conflict with anonymous class names generated by the owl  model

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtility.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtility.java
@@ -6,9 +6,16 @@ import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 
 public class ParserUtility {
 	
+    
+    
 	public static String getResourceName(AResource resource) {
-		if (resource.isAnonymous()) {			
-			return AbstractOWLModel.ANONYMOUS_BASE + resource.getAnonymousID();
+		if (resource.isAnonymous()) {	
+		    /*
+		     * Argh...  Must ensure that this cannot conflict with owlModel.getNextAnonymousId();
+		     *          See the stuff involving the creation of logical named classes in the 
+		     *          TripleFrameCache code.  This is *nasty* but I don't yet the better way.
+		     */
+			return AbstractOWLModel.ANONYMOUS_BASE + "PARSE_" + resource.getAnonymousID();
 		} else {
 			return resource.getURI();
 		}
