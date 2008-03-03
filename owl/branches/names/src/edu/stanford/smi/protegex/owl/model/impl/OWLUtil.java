d05aec09d6cad651fded83c09da1cd0f44c72918
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-03 03:19:37 +0000

    refactor of getInternalFullName

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/impl/OWLUtil.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/impl/OWLUtil.java
@@ -1,16 +1,54 @@
 package edu.stanford.smi.protegex.owl.model.impl;
 
-import edu.stanford.smi.protege.model.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+
+import javax.swing.JComponent;
+
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Model;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.model.Reference;
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.MergingNarrowFrameStore;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protege.ui.FrameComparator;
 import edu.stanford.smi.protege.ui.ProjectManager;
-import edu.stanford.smi.protege.ui.ProjectView;
-import edu.stanford.smi.protege.util.FileUtilities;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.URIUtilities;
 import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
-import edu.stanford.smi.protegex.owl.model.*;
+import edu.stanford.smi.protegex.owl.model.NamespaceUtil;
+import edu.stanford.smi.protegex.owl.model.OWLAnonymousClass;
+import edu.stanford.smi.protegex.owl.model.OWLEnumeratedClass;
+import edu.stanford.smi.protegex.owl.model.OWLIntersectionClass;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNAryLogicalClass;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.OWLNames;
+import edu.stanford.smi.protegex.owl.model.OWLOntology;
+import edu.stanford.smi.protegex.owl.model.OWLProperty;
+import edu.stanford.smi.protegex.owl.model.OWLRestriction;
+import edu.stanford.smi.protegex.owl.model.ProtegeNames;
+import edu.stanford.smi.protegex.owl.model.RDFList;
+import edu.stanford.smi.protegex.owl.model.RDFObject;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSClass;
+import edu.stanford.smi.protegex.owl.model.RDFSLiteral;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+import edu.stanford.smi.protegex.owl.model.RDFUntypedResource;
 import edu.stanford.smi.protegex.owl.model.classparser.OWLClassParser;
 import edu.stanford.smi.protegex.owl.model.event.PropertyValueAdapter;
 import edu.stanford.smi.protegex.owl.model.event.PropertyValueListener;
@@ -22,12 +60,6 @@ import edu.stanford.smi.protegex.owl.ui.ProtegeUI;
 import edu.stanford.smi.protegex.owl.ui.profiles.OWLProfiles;
 import edu.stanford.smi.protegex.owl.ui.profiles.ProfilesManager;
 import edu.stanford.smi.protegex.owl.util.OWLFrameStoreUtils;
-
-import java.net.URI;
-import java.util.*;
-import java.util.logging.Level;
-
-import javax.swing.JComponent;
 
 /**
  * A collection of static utility methods for OWL classes.
@@ -73,7 +105,7 @@ public class OWLUtil {
 
 
     public static RDFUntypedResource assignUniqueURI(RDFUntypedResource eri) {
-        OWLModel owlModel = (OWLModel) eri.getOWLModel();
+        OWLModel owlModel = eri.getOWLModel();
         String prefix = "http://protege.stanford.edu/";
         String suffix = "";
         int index = 1;
@@ -87,6 +119,7 @@ public class OWLUtil {
     /**
      * @deprecated use the version with project parameter instead
      */
+    @Deprecated
     public static boolean confirmSaveAndReload() {
         return confirmSaveAndReload(null);
     }
@@ -182,6 +215,7 @@ public class OWLUtil {
      * @return a clone of cls or cls itself if it is a named class
      * @deprecated use CloneFactory and ResourceCopier instead
      */
+    @Deprecated
     public static edu.stanford.smi.protege.model.Cls createClone(edu.stanford.smi.protege.model.Cls cls) {
         String expression = cls.getBrowserText();
         OWLModel owlModel = (OWLModel) cls.getKnowledgeBase();
@@ -196,6 +230,7 @@ public class OWLUtil {
      * @return
      * @deprecated use CloneFactory and ResourceCopier instead
      */
+    @Deprecated
     public static RDFSClass createClone(OWLModel owlModel, String expression) {
         OWLClassParser parser = owlModel.getOWLClassParser();
         try {
@@ -560,6 +595,7 @@ public class OWLUtil {
             List oldSuperclasses = new ArrayList(superCls.getDirectSubclasses());
             List clses = new ArrayList(oldSuperclasses);
             Collections.sort(clses, new FrameComparator() {
+                @Override
                 public int compare(Frame o1, Frame o2) {
                     if (o1 instanceof RDFSNamedClass && o2 instanceof RDFSNamedClass) {
                         return ((RDFSNamedClass) o1).getBrowserText().compareTo(((RDFSNamedClass) o2).getBrowserText());
@@ -654,7 +690,7 @@ public class OWLUtil {
 
 
     public static void addPropertyValue(RDFResource resource, RDFProperty property, Object value) {
-        KnowledgeBase kb = (KnowledgeBase) resource.getOWLModel();
+        KnowledgeBase kb = resource.getOWLModel();
         kb.addOwnSlotValue(resource, property, value);
     }
 
@@ -1109,6 +1145,43 @@ public class OWLUtil {
     		owlModel.resetOntologyCache();
     	}
     }
+
+
+    /*
+     * Get the full name of a resource.  We have to handle the short name instead of full URIs
+     * used in the create & getter methods. This is a backwards compatibility extension.
+     * All the create & get methods will check the expandShortNameInMethods flag, which is default false.
+     */   
+    public static String getInternalFullName(OWLModel owlModel, String name) {
+    	if (name == null) {
+    		return null;
+    	}
+    	if (owlModel.isExpandShortNameInMethods() && !isAbsoluteURI(owlModel, name)) {
+    		name = NamespaceUtil.getFullName(owlModel, name);
+    	}
+    	
+    	return name;
+    }
     
-    
+    protected static boolean isAbsoluteURI(OWLModel owlModel, String uriString) {
+        if (!uriString.contains(":")) {
+            return false;
+        }
+        
+        URI uri = null;
+        
+        try {
+            uri = new URI(uriString);
+        } catch (URISyntaxException e) {            
+            //do nothing - uri will be null. Hopefully this won't be the case
+        }
+        if (uri == null || !uri.isAbsolute()) {
+            return false;
+        }
+        if (uri.isOpaque()) {
+            String scheme = uri.getScheme();
+            return owlModel.getNamespaceManager().getNamespaceForPrefix(scheme) == null ? true : false;
+        }
+        return true;
+    }
 }
