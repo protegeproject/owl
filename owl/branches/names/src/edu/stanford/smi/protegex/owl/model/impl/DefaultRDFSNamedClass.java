60084b23e1f97ba485ade935aeb343e3981f083f
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-03 03:17:07 +0000

    refactor of getInternalFullName

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/impl/DefaultRDFSNamedClass.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/impl/DefaultRDFSNamedClass.java
@@ -1,14 +1,31 @@
 package edu.stanford.smi.protegex.owl.model.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protegex.owl.model.*;
+import edu.stanford.smi.protegex.owl.model.OWLAllValuesFrom;
+import edu.stanford.smi.protegex.owl.model.OWLIntersectionClass;
+import edu.stanford.smi.protegex.owl.model.OWLNames;
+import edu.stanford.smi.protegex.owl.model.OWLUnionClass;
+import edu.stanford.smi.protegex.owl.model.RDFIndividual;
+import edu.stanford.smi.protegex.owl.model.RDFObject;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSClass;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
 import edu.stanford.smi.protegex.owl.model.visitor.OWLModelVisitor;
 import edu.stanford.smi.protegex.owl.ui.icons.OWLIcons;
-
-import javax.swing.*;
-import java.util.*;
 
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
@@ -41,6 +58,7 @@ public class DefaultRDFSNamedClass extends AbstractRDFSClass implements RDFSName
     }
 
 
+    @Override
     public Icon getIcon() {
         if (isMetaCls()) {
             return isEditable() ? OWLIcons.getMetaclassIcon() : OWLIcons.getReadOnlyClsIcon(OWLIcons.getImageIcon(OWLIcons.RDFS_METACLASS));
@@ -75,7 +93,7 @@ public class DefaultRDFSNamedClass extends AbstractRDFSClass implements RDFSName
 
 
     public RDFIndividual createRDFIndividual(String name) {
-        return (RDFIndividual) createInstance(((AbstractOWLModel) getOWLModel()).getInternalFullName(name));
+        return (RDFIndividual) createInstance(OWLUtil.getInternalFullName(getOWLModel(), name));
     }
 
 
