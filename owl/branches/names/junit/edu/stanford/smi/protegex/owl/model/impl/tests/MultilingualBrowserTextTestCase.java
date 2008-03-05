bfaa94393cbba1dbb3be4516791fc0dfba3ac122
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-05 01:25:33 +0000

    half of the multi-lingual test fix

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/model/impl/tests/MultilingualBrowserTextTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/model/impl/tests/MultilingualBrowserTextTestCase.java
@@ -11,7 +11,7 @@ public class MultilingualBrowserTextTestCase extends AbstractJenaTestCase {
     public void testDefaultBrowserText() {
         OWLNamedClass cls = owlModel.createOWLNamedClass("Cls");
         RDFIndividual instance = cls.createRDFIndividual("Instance");
-        assertEquals(instance.getName(), instance.getBrowserText());
+        assertEquals(instance.getLocalName(), instance.getBrowserText());
     }
 
 
