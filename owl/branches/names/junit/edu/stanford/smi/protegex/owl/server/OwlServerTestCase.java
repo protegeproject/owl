240d59ab64dadfdfa38b3b6511f41b7665fc27fd
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-08 01:47:44 +0000

    this one should pass

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/server/OwlServerTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/server/OwlServerTestCase.java
@@ -6,6 +6,7 @@ import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.server.RemoteProjectManager;
 import edu.stanford.smi.protege.server.Server_Test;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.tests.AbstractJenaTestCase;
 
 public class OwlServerTestCase extends AbstractJenaTestCase {
@@ -31,8 +32,8 @@ public class OwlServerTestCase extends AbstractJenaTestCase {
     }
     Project p = RemoteProjectManager.getInstance().getProject(Server_Test.HOST, USER, PASSWORD, PROJECT_NAME, true);
     assertNotNull(p);
-    KnowledgeBase kb = p.getKnowledgeBase();
-    Cls cls = kb.getCls("Pizza");
+    OWLModel kb = (OWLModel) p.getKnowledgeBase();
+    Cls cls = kb.getOWLNamedClass(kb.getNamespaceManager().getDefaultNamespace() + "Pizza");
     Log.getLogger().info("found " + cls + " with frame id = " + cls.getFrameID());
     assertNotNull(cls);
     p.dispose();
