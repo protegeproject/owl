44929c4cbfdaa5268efdde7d7bd6d1e5dd14a59d
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-13 14:43:57 +0000

    an improvement but not a fix yet

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/protege2jena/tests/CreateProtegeFramesTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/jena/protege2jena/tests/CreateProtegeFramesTestCase.java
@@ -1,22 +1,39 @@
 package edu.stanford.smi.protegex.owl.jena.protege2jena.tests;
 
+import java.util.logging.Level;
+
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntProperty;
+
 import edu.stanford.smi.protege.model.Model;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.jena.parser.FrameCreatorUtility;
+import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
+import edu.stanford.smi.protegex.owl.jena.parser.TripleFrameCache;
 import edu.stanford.smi.protegex.owl.model.RDFIndividual;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+import edu.stanford.smi.protegex.owl.tests.AbstractJenaTestCase;
 
 /**
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class CreateProtegeFramesTestCase extends AbstractProtege2JenaTestCase {
+    
+    public static void enableDebug() {
+        Log.setLoggingLevel(AbstractJenaTestCase.class, Level.FINE);
+        Log.setLoggingLevel(ProtegeOWLParser.class, Level.FINEST);
+        Log.setLoggingLevel(TripleFrameCache.class, Level.FINEST);
+        Log.setLoggingLevel(FrameCreatorUtility.class, Level.FINEST);
+    }
 
     public void testCreateUnlinkedPALConstraint() throws Exception {
 
+        
         ensureProtegeMetaOntologyImported();
+        owlModel.getFrameStoreManager().setProtegeOwlFrameStoreEnabled(true);
 
         RDFSNamedClass constraintClass = owlModel.getRDFSNamedClass(Model.Cls.PAL_CONSTRAINT);
         RDFIndividual constraint = constraintClass.createRDFIndividual("MyIndividual");
@@ -44,9 +61,10 @@ public class CreateProtegeFramesTestCase extends AbstractProtege2JenaTestCase {
 
 
     public void testCreateLinkedPALConstraint() throws Exception {
+        Log.setLoggingLevel(AbstractJenaTestCase.class, Level.FINE);
 
         ensureProtegeMetaOntologyImported();
-
+        owlModel.getFrameStoreManager().setProtegeOwlFrameStoreEnabled(true);
         owlModel = reloadWithJenaLoader(owlModel);
 
         RDFSNamedClass constraintClass = owlModel.getRDFSNamedClass(Model.Cls.PAL_CONSTRAINT);
