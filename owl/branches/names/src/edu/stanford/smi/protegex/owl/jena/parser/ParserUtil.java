7d65a4fb1275ace57bb37e1d6c0459b210b7aba4
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-14 01:35:54 +0000

    refactored the getSimpleFS

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtil.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtil.java
@@ -2,6 +2,11 @@ package edu.stanford.smi.protegex.owl.jena.parser;
 
 import com.hp.hpl.jena.rdf.arp.AResource;
 
+import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.framestore.SimpleFrameStore;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 
 public class ParserUtil {
@@ -18,6 +23,15 @@ public class ParserUtil {
 		} else {
 			return resource.getURI();
 		}
+	}
+
+	public static SimpleFrameStore getSimpleFrameStore(KnowledgeBase kb) {
+	    return (SimpleFrameStore) ((DefaultKnowledgeBase) kb).getTerminalFrameStore();
+	}
+
+	
+	public static SimpleFrameStore getSimpleFrameStore(Frame frame) {
+	    return getSimpleFrameStore(frame.getKnowledgeBase());
 	}
 	
 }
