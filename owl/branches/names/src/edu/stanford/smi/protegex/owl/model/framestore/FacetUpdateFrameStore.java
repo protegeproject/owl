da3e0640e2d2e8dacea08171cbf77db9ad0a215a
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-21 07:35:16 +0000

    - copyFacetedValues - goes through all triplestores and does the operation on all instances of OWLRestriction

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/model/framestore/FacetUpdateFrameStore.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/model/framestore/FacetUpdateFrameStore.java
@@ -1,5 +1,6 @@
 package edu.stanford.smi.protegex.owl.model.framestore;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -10,6 +11,7 @@ import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.Model;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.FrameStoreAdapter;
+import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
 import edu.stanford.smi.protegex.owl.model.OWLRestriction;
@@ -20,172 +22,215 @@ import edu.stanford.smi.protegex.owl.model.framestore.updater.AllValuesFromRestr
 import edu.stanford.smi.protegex.owl.model.framestore.updater.CardinalityRestrictionUpdater;
 import edu.stanford.smi.protegex.owl.model.framestore.updater.HasValueRestrictionUpdater;
 import edu.stanford.smi.protegex.owl.model.framestore.updater.RestrictionUpdater;
+import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLAllValuesFrom;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLCardinality;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLHasValue;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLMaxCardinality;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLMinCardinality;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
+import edu.stanford.smi.protegex.owl.model.triplestore.TripleStoreModel;
 
 public class FacetUpdateFrameStore extends FrameStoreAdapter {
-    
-    private OWLModel owlModel;
-    /**
-     * A flag to prevent infinite recursion when a facet override has been changed.
-     */
-    protected boolean superclassHandlingBlocked;
-    
-    /**
-     * A Hashtable from Java restriction Class objects to the responsible RestrictionUpdaters
-     */
-    private Hashtable<Class, AbstractRestrictionUpdater> class2Updater = new Hashtable<Class, AbstractRestrictionUpdater>();
 
-    /**
-     * A Hashtable from Facets to the responsible RestrictionUpdaters
-     */
-    private Hashtable<Facet, AbstractRestrictionUpdater> facet2Updater = new Hashtable<Facet, AbstractRestrictionUpdater>();
+	private OWLModel owlModel;
+	/**
+	 * A flag to prevent infinite recursion when a facet override has been changed.
+	 */
+	protected boolean superclassHandlingBlocked;
 
-    /**
-     * A flag to prevent infinite recursion when a superclass has been added or removed
-     */
-    protected boolean facetHandlingBlocked;
+	/**
+	 * A Hashtable from Java restriction Class objects to the responsible RestrictionUpdaters
+	 */
+	private Hashtable<Class, AbstractRestrictionUpdater> class2Updater = new Hashtable<Class, AbstractRestrictionUpdater>();
 
-    private AllValuesFromRestrictionUpdater allValuesFromRestrictionUpdater;
+	/**
+	 * A Hashtable from Facets to the responsible RestrictionUpdaters
+	 */
+	private Hashtable<Facet, AbstractRestrictionUpdater> facet2Updater = new Hashtable<Facet, AbstractRestrictionUpdater>();
 
-    private CardinalityRestrictionUpdater cardinalityRestrictionUpdater;
+	/**
+	 * A flag to prevent infinite recursion when a superclass has been added or removed
+	 */
+	protected boolean facetHandlingBlocked;
 
-    private HasValueRestrictionUpdater hasValueRestrictionUpdater;
-    
-    public FacetUpdateFrameStore(OWLModel owlModel) {
-        this.owlModel = owlModel;
-        initRestrictionUpdaters();
-    }
-    
-    private void initRestrictionUpdaters() {
+	private AllValuesFromRestrictionUpdater allValuesFromRestrictionUpdater;
 
-        allValuesFromRestrictionUpdater = new AllValuesFromRestrictionUpdater(owlModel);
-        cardinalityRestrictionUpdater = new CardinalityRestrictionUpdater(owlModel);
-        hasValueRestrictionUpdater = new HasValueRestrictionUpdater(owlModel);
+	private CardinalityRestrictionUpdater cardinalityRestrictionUpdater;
 
-        facet2Updater.put(owlModel.getSystemFrames().getValueTypeFacet(), allValuesFromRestrictionUpdater);
-        facet2Updater.put(owlModel.getSystemFrames().getMaximumCardinalityFacet(), cardinalityRestrictionUpdater);
-        facet2Updater.put(owlModel.getSystemFrames().getMinimumCardinalityFacet(), cardinalityRestrictionUpdater);
-        facet2Updater.put(owlModel.getSystemFrames().getValuesFacet(), hasValueRestrictionUpdater);
+	private HasValueRestrictionUpdater hasValueRestrictionUpdater;
 
-        // TODO: This should be generalized, independent from Default implementation classes
-        class2Updater.put(DefaultOWLAllValuesFrom.class, allValuesFromRestrictionUpdater);
-        class2Updater.put(DefaultOWLCardinality.class, cardinalityRestrictionUpdater);
-        class2Updater.put(DefaultOWLHasValue.class, hasValueRestrictionUpdater);
-        class2Updater.put(DefaultOWLMaxCardinality.class, cardinalityRestrictionUpdater);
-        class2Updater.put(DefaultOWLMinCardinality.class, cardinalityRestrictionUpdater);
-    }
-    
+	public FacetUpdateFrameStore(OWLModel owlModel) {
+		this.owlModel = owlModel;
+		initRestrictionUpdaters();
+	}
 
-    
-    public void copyFacetValuesIntoNamedClses() {
-        boolean oldUndo = owlModel.isUndoEnabled();
-        owlModel.setUndoEnabled(false);
-        try {
-            for (Iterator it = (owlModel).getRDFSClasses().iterator(); it.hasNext();) {
-                Cls cls = (Cls) it.next();
-                if (cls instanceof OWLRestriction) {  // Convert restrictions into facet overrides
-                    copyFacetValuesIntoOWLNamedClass((OWLRestriction) cls);
-                }
-            }
-        } finally {
-            owlModel.setUndoEnabled(oldUndo);
-        }
-    }
-      
-      private void copyFacetValuesIntoOWLNamedClass(OWLRestriction restriction) {
-        if (restriction.getSubclasses(false).size() == 1) {
-            RDFSNamedClass namedCls = (RDFSNamedClass) restriction.getSubclasses(false).toArray()[0];
-            copyFacetValuesIntoOWLNamedClass(namedCls, restriction);
-        }
-      }
-      
-      private void copyFacetValuesIntoOWLNamedClass(RDFSNamedClass cls, OWLRestriction restriction) {
-        Class clazz = restriction.getClass();
-        RestrictionUpdater ru = class2Updater.get(clazz);
-        if (ru != null) {
-            facetHandlingBlocked = true;
-            ru.copyFacetValuesIntoNamedClass(cls, restriction);
-            facetHandlingBlocked = false;
-        }
-      }
-      
+	private void initRestrictionUpdaters() {
 
-    private void updateRestrictions(OWLNamedClass cls, RDFProperty slot, Facet facet) {
-        RestrictionUpdater ru = facet2Updater.get(facet);
-        if (ru != null) {
-            superclassHandlingBlocked = true;
-            ru.updateRestrictions(cls, slot, facet);
-            superclassHandlingBlocked = false;
-        }
-    }
-    
-    /*
-     * FrameStore implementations
-     */
-    
-    @Override
-    public Slot createSlot(FrameID id, Collection directTypes, Collection directSuperslots, boolean loadDefaults) {
-        Slot slot = super.createSlot(id, directTypes, directSuperslots, loadDefaults);
-        if (slot instanceof RDFProperty) {
-            RDFProperty rdfProperty = (RDFProperty) slot;
-            slot.setAllowsMultipleValues(true);
-        }
-        return slot;
-    }
-    
-    @Override
-    public void addDirectSuperclass(Cls cls, Cls superCls) {
-        if (!superclassHandlingBlocked && cls instanceof OWLNamedClass) {
-            OWLNamedClass namedCls = (OWLNamedClass) cls;
-            if (superCls instanceof OWLRestriction) {
-                copyFacetValuesIntoOWLNamedClass(namedCls, (OWLRestriction) superCls);
-            }
-        }
-        super.addDirectSuperclass(cls, superCls);
-    }
-    
-    @Override
-    public void removeDirectSuperclass(Cls cls, Cls superCls) {
-        super.removeDirectSuperclass(cls, superCls);
-        if (!superclassHandlingBlocked) {
-            if (cls instanceof OWLNamedClass && superCls instanceof OWLRestriction) {
-                copyFacetValuesIntoOWLNamedClass((OWLNamedClass) cls, (OWLRestriction) superCls);
-            }
-        }
-    }
-    
-    @Override
-    public void setDirectTemplateFacetValues(Cls cls, Slot slot, Facet facet, Collection values) {
+		allValuesFromRestrictionUpdater = new AllValuesFromRestrictionUpdater(owlModel);
+		cardinalityRestrictionUpdater = new CardinalityRestrictionUpdater(owlModel);
+		hasValueRestrictionUpdater = new HasValueRestrictionUpdater(owlModel);
 
-        super.setDirectTemplateFacetValues(cls, slot, facet, values);
+		facet2Updater.put(owlModel.getSystemFrames().getValueTypeFacet(), allValuesFromRestrictionUpdater);
+		facet2Updater.put(owlModel.getSystemFrames().getMaximumCardinalityFacet(), cardinalityRestrictionUpdater);
+		facet2Updater.put(owlModel.getSystemFrames().getMinimumCardinalityFacet(), cardinalityRestrictionUpdater);
+		facet2Updater.put(owlModel.getSystemFrames().getValuesFacet(), hasValueRestrictionUpdater);
 
-        if (!facetHandlingBlocked) {
-            if (cls instanceof OWLNamedClass && slot instanceof RDFProperty) {
-                updateRestrictions((OWLNamedClass) cls, (RDFProperty) slot, facet);
-            }
-        }
+		// TODO: This should be generalized, independent from Default implementation classes
+		class2Updater.put(DefaultOWLAllValuesFrom.class, allValuesFromRestrictionUpdater);
+		class2Updater.put(DefaultOWLCardinality.class, cardinalityRestrictionUpdater);
+		class2Updater.put(DefaultOWLHasValue.class, hasValueRestrictionUpdater);
+		class2Updater.put(DefaultOWLMaxCardinality.class, cardinalityRestrictionUpdater);
+		class2Updater.put(DefaultOWLMinCardinality.class, cardinalityRestrictionUpdater);
+	}
 
-        if (!superclassHandlingBlocked) {
-            if (cls instanceof OWLRestriction) {
-                OWLRestriction restriction = (OWLRestriction) cls;
-                copyFacetValuesIntoOWLNamedClass(restriction);
-            }
-        }
-    }
-    
-    @Override
-    public void setDirectTemplateSlotValues(Cls cls, Slot slot, Collection values) {
-        super.setDirectTemplateSlotValues(cls, slot, values);
 
-        if (!facetHandlingBlocked) {
-            if (cls instanceof OWLNamedClass && slot instanceof RDFProperty) {
-                updateRestrictions((OWLNamedClass) cls, (RDFProperty) slot, owlModel.getFacet(Model.Facet.VALUES));
-            }
-        }
-    }
-    
+
+	@SuppressWarnings("deprecation")
+	public void copyFacetValuesIntoNamedClses() {
+		boolean oldUndo = owlModel.isUndoEnabled();
+		TripleStoreModel tsm = owlModel.getTripleStoreModel();
+		
+		TripleStore activeTripleStore = tsm.getActiveTripleStore();		
+		
+		owlModel.setUndoEnabled(false);
+		
+		try {			
+			for (Iterator iterator = tsm.getTripleStores().iterator(); iterator.hasNext();) {
+				TripleStore ts = (TripleStore) iterator.next();				
+				tsm.setActiveTripleStore(ts);
+				
+				for (Iterator<OWLRestriction> iterator2 = getRestrictionsForTS(ts).iterator(); iterator2.hasNext();) {
+					OWLRestriction restriction = (OWLRestriction) iterator2.next();
+					copyFacetValuesIntoOWLNamedClass(restriction);
+				}				
+			}			
+		} finally {
+			owlModel.setUndoEnabled(oldUndo);
+			owlModel.getTripleStoreModel().setActiveTripleStore(activeTripleStore);
+		}
+	}
+
+
+	/**
+	 * Funky method to get all the restrictions defined in a triplestore
+	 * @param ts
+	 * @return
+	 */
+	private Collection getRestrictionsForTS(TripleStore ts) {
+		Collection<OWLRestriction> restrictions = new ArrayList<OWLRestriction>();
+		
+		RDFSNamedClass restrictionClass = ((AbstractOWLModel) owlModel).getOWLRestrictionClass();
+
+		for (Iterator iterator = restrictionClass.getSubclasses(true).iterator(); iterator.hasNext();) {
+			RDFSNamedClass restrictionType = (RDFSNamedClass) iterator.next();
+			
+			NarrowFrameStore nfs = ts.getNarrowFrameStore();
+			Collection insts = nfs.getValues(restrictionClass, owlModel.getSystemFrames().getDirectInstancesSlot(), null, false);
+			
+			for (Iterator iterator2 = insts.iterator(); iterator2.hasNext();) {
+				Object inst = iterator2.next();				
+				if (inst instanceof OWLRestriction) {
+					restrictions.add((OWLRestriction)inst);
+				}
+			}			
+		}
+				
+		return restrictions;
+	}
+
+
+
+	private void copyFacetValuesIntoOWLNamedClass(OWLRestriction restriction) {
+		if (restriction.getSubclasses(false).size() == 1) {
+			RDFSNamedClass namedCls = (RDFSNamedClass) restriction.getSubclasses(false).toArray()[0];
+			copyFacetValuesIntoOWLNamedClass(namedCls, restriction);
+		}
+	}
+
+	private void copyFacetValuesIntoOWLNamedClass(RDFSNamedClass cls, OWLRestriction restriction) {
+		Class clazz = restriction.getClass();
+		RestrictionUpdater ru = class2Updater.get(clazz);
+		if (ru != null) {
+			facetHandlingBlocked = true;
+			ru.copyFacetValuesIntoNamedClass(cls, restriction);
+			facetHandlingBlocked = false;
+		}
+	}
+
+
+	private void updateRestrictions(OWLNamedClass cls, RDFProperty slot, Facet facet) {
+		RestrictionUpdater ru = facet2Updater.get(facet);
+		if (ru != null) {
+			superclassHandlingBlocked = true;
+			ru.updateRestrictions(cls, slot, facet);
+			superclassHandlingBlocked = false;
+		}
+	}
+
+	/*
+	 * FrameStore implementations
+	 */
+
+	 @Override
+	 public Slot createSlot(FrameID id, Collection directTypes, Collection directSuperslots, boolean loadDefaults) {
+		 Slot slot = super.createSlot(id, directTypes, directSuperslots, loadDefaults);
+		 if (slot instanceof RDFProperty) {
+			 RDFProperty rdfProperty = (RDFProperty) slot;
+			 slot.setAllowsMultipleValues(true);
+		 }
+		 return slot;
+	 }
+
+	 @Override
+	 public void addDirectSuperclass(Cls cls, Cls superCls) {
+		 if (!superclassHandlingBlocked && cls instanceof OWLNamedClass) {
+			 OWLNamedClass namedCls = (OWLNamedClass) cls;
+			 if (superCls instanceof OWLRestriction) {
+				 copyFacetValuesIntoOWLNamedClass(namedCls, (OWLRestriction) superCls);
+			 }
+		 }
+		 super.addDirectSuperclass(cls, superCls);
+	 }
+
+	 @Override
+	 public void removeDirectSuperclass(Cls cls, Cls superCls) {
+		 super.removeDirectSuperclass(cls, superCls);
+		 if (!superclassHandlingBlocked) {
+			 if (cls instanceof OWLNamedClass && superCls instanceof OWLRestriction) {
+				 copyFacetValuesIntoOWLNamedClass((OWLNamedClass) cls, (OWLRestriction) superCls);
+			 }
+		 }
+	 }
+
+	 @Override
+	 public void setDirectTemplateFacetValues(Cls cls, Slot slot, Facet facet, Collection values) {
+
+		 super.setDirectTemplateFacetValues(cls, slot, facet, values);
+
+		 if (!facetHandlingBlocked) {
+			 if (cls instanceof OWLNamedClass && slot instanceof RDFProperty) {
+				 updateRestrictions((OWLNamedClass) cls, (RDFProperty) slot, facet);
+			 }
+		 }
+
+		 if (!superclassHandlingBlocked) {
+			 if (cls instanceof OWLRestriction) {
+				 OWLRestriction restriction = (OWLRestriction) cls;
+				 copyFacetValuesIntoOWLNamedClass(restriction);
+			 }
+		 }
+	 }
+
+	 @Override
+	 public void setDirectTemplateSlotValues(Cls cls, Slot slot, Collection values) {
+		 super.setDirectTemplateSlotValues(cls, slot, values);
+
+		 if (!facetHandlingBlocked) {
+			 if (cls instanceof OWLNamedClass && slot instanceof RDFProperty) {
+				 updateRestrictions((OWLNamedClass) cls, (RDFProperty) slot, owlModel.getFacet(Model.Facet.VALUES));
+			 }
+		 }
+	 }
+
 }
