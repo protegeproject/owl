a1fdb0c276ea6e06339487f60095a4a092e79185
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-21 07:31:31 +0000

    added the triplestore as argument to the undef triples

--- owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/TripleProcessor.java
+++ owl/branches/names/src/edu/stanford/smi/protegex/owl/jena/parser/TripleProcessor.java
@@ -1,27 +1,21 @@
 package edu.stanford.smi.protegex.owl.jena.parser;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.logging.Level;
 
 import com.hp.hpl.jena.rdf.arp.ALiteral;
 import com.hp.hpl.jena.rdf.arp.AResource;
 
 import edu.stanford.smi.protegex.owl.model.OWLModel;
-import edu.stanford.smi.protegex.owl.model.RDFProperty;
-import edu.stanford.smi.protegex.owl.model.RDFResource;
-import edu.stanford.smi.protegex.owl.model.RDFSClass;
-import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
 import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
+import edu.stanford.smi.protegex.owlx.examples.UntypedResourcesExample;
 
+//TODO: Duplicate supercalsses
 //TODO: Postprocess classes without superclasses: add owl:Thing in all triplestores where the class has a name
 //TODO: ProtegeOWLParser: is the process undef triples and postprocess in the right place?
 //TODO: Postprocessing GCI - refactor in their own class
-//TODO: Postprcessing undef triples -> create them as Untyped resources (swizzle)
 //TODO: Solution for copyFacetedValues not to happen too many times. Maybe use flag in AbstractOWLModel
-//TODO: Check range and domain of properties
 //TODO: Timing logger for the parser
 
 //-- later --
@@ -37,6 +31,7 @@ public class TripleProcessor {
 	
 	private TripleProcessorForResourceObjects processorResourceObjs;
 	private TripleProcessorForLiteralObjects processorLiteralObjs;
+	private TripleProcessorForUntypedResources untypedProcessor;
 	
 	private UndefTripleManager undefTripleManager;
 	
@@ -51,11 +46,13 @@ public class TripleProcessor {
 		this.owlModel = owlModel;
 		this.tripleStore = tripleStore;
 
-		this.undefTripleManager = new UndefTripleManager();
+		this.undefTripleManager = ((AbstractOWLModel)owlModel).getUndefTripleManager();
 		this.importing = !tripleStore.equals(owlModel.getTripleStoreModel().getTopTripleStore());
+		
 		//should come as the last in the initialization
 		this.processorResourceObjs = new TripleProcessorForResourceObjects(this);
-		this.processorLiteralObjs = new TripleProcessorForLiteralObjects(this);		
+		this.processorLiteralObjs = new TripleProcessorForLiteralObjects(this);
+		this.untypedProcessor = new TripleProcessorForUntypedResources(this);
 	}
 
 
@@ -72,10 +69,10 @@ public class TripleProcessor {
 		return owlModel;
 	}
 	
-	public void addUndefTriple(AResource subj, AResource pred, AResource obj, String undefName, boolean alreadyInUndef) {
+	public void addUndefTriple(AResource subj, AResource pred, AResource obj, String undefName, boolean alreadyInUndef, TripleStore ts) {
 		if (!alreadyInUndef) {		  
-			undefTripleManager.addUndefTriple(new UndefTriple(subj, pred, obj, undefName)); //check this!!
-		}		
+			undefTripleManager.addUndefTriple(new UndefTriple(subj, pred, obj, undefName, ts));
+		}
 	}
 	
 	protected void checkUndefinedResources(String uri) {		
@@ -114,16 +111,20 @@ public class TripleProcessor {
 		return importing;
 	}
 
+	
 	public void doPostProcessing() {
+		processUndefTriples();
 		processorResourceObjs.doPostProcessing();
 		processorLiteralObjs.doPostProcessing();
 	}
 
 
 	public void processUndefTriples() {		
-		TripleProcessorForUntypedResources untypedProcessor = new TripleProcessorForUntypedResources(this);
-		untypedProcessor.processUndefTriples();
-		
+		untypedProcessor.processUndefTriples();		
+	}
+	
+	public void createUntypedResources() {
+		untypedProcessor.createUntypedResources();
 	}
 	
 
