b61b42513d4feae1ad898f7da7b3d8479361b9fc
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-03-24 18:23:36 +0000

    Debugged difference between junit in eclipse and junit in ant.
    Changed the initialization of the plugin directory so that it only
    happens in the ide case.  Also added exception so that I would see the
    failure if that was the problems.

--- owl/branches/names/junit/edu/stanford/smi/protegex/owl/model/classparser/compact/tests/ParseEnumeratedClassesTestCase.java
+++ owl/branches/names/junit/edu/stanford/smi/protegex/owl/model/classparser/compact/tests/ParseEnumeratedClassesTestCase.java
@@ -20,53 +20,70 @@ import edu.stanford.smi.protegex.owl.ui.profiles.ProfilesManager;
 public class ParseEnumeratedClassesTestCase extends AbstractOWLModelTestCase {
     
     static {
-        ProtegeOWL.setPluginFolder(new File("etc"));
+  	  if (!ProtegeOWL.getPluginFolder().exists()) { // hack for users in ide's
+		  ProtegeOWL.setPluginFolder(new File("etc"));
+	  }
     }
 
     private OWLClassParser parser = new CompactOWLClassParser();
 
 
-    public void testParseEnumerationOfIndividuals() throws Exception {
-        ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_DL.getURI());
-        OWLNamedClass genderCls = owlModel.createOWLNamedClass("Gender");
-        genderCls.createInstance("male");
-        genderCls.createInstance("female");
-        String expression = "{male female}";
-        RDFSClass aClass = parser.parseClass(owlModel, expression);
-        assertTrue(aClass instanceof OWLEnumeratedClass);
-        OWLEnumeratedClass enumeratedClass = (OWLEnumeratedClass) aClass;
-        Collection resources = enumeratedClass.getOneOf();
-        assertEquals(2, resources.size());
-        assertTrue(resources.contains(owlModel.getRDFResourceByBrowserText("male")));
-        assertTrue(resources.contains(owlModel.getRDFResourceByBrowserText("female")));
+    public void testParseEnumerationOfIndividuals()  {
+    	try {
+    		ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_DL.getURI());
+    		OWLNamedClass genderCls = owlModel.createOWLNamedClass("Gender");
+    		genderCls.createInstance("male");
+    		genderCls.createInstance("female");
+    		String expression = "{male female}";
+    		RDFSClass aClass = parser.parseClass(owlModel, expression);
+    		assertTrue(aClass instanceof OWLEnumeratedClass);
+    		OWLEnumeratedClass enumeratedClass = (OWLEnumeratedClass) aClass;
+    		Collection resources = enumeratedClass.getOneOf();
+    		assertEquals(2, resources.size());
+    		assertTrue(resources.contains(owlModel.getRDFResourceByBrowserText("male")));
+    		assertTrue(resources.contains(owlModel.getRDFResourceByBrowserText("female")));
+    	}
+    	catch (Exception e) {
+    		fail();
+    	}
     }
 
 
-    public void testParseEnumerationOfClassesAndSlots() throws Exception {
-        ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_Full.getURI());
-        OWLNamedClass maleCls = owlModel.createOWLNamedClass("Male");
-        OWLObjectProperty slot = owlModel.createOWLObjectProperty("hasChildren");
-        String expression = "{Male hasChildren}";
-        RDFSClass aClass = parser.parseClass(owlModel, expression);
-        assertTrue(aClass instanceof OWLEnumeratedClass);
-        OWLEnumeratedClass enumeratedClass = (OWLEnumeratedClass) aClass;
-        Collection resources = enumeratedClass.getOneOf();
-        assertEquals(2, resources.size());
-        assertTrue(resources.contains(maleCls));
-        assertTrue(resources.contains(slot));
+    public void testParseEnumerationOfClassesAndSlots() {
+    	try {
+    		ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_Full.getURI());
+    		OWLNamedClass maleCls = owlModel.createOWLNamedClass("Male");
+    		OWLObjectProperty slot = owlModel.createOWLObjectProperty("hasChildren");
+    		String expression = "{Male hasChildren}";
+    		RDFSClass aClass = parser.parseClass(owlModel, expression);
+    		assertTrue(aClass instanceof OWLEnumeratedClass);
+    		OWLEnumeratedClass enumeratedClass = (OWLEnumeratedClass) aClass;
+    		Collection resources = enumeratedClass.getOneOf();
+    		assertEquals(2, resources.size());
+    		assertTrue(resources.contains(maleCls));
+    		assertTrue(resources.contains(slot));
+    	}
+    	catch (Exception e) {
+    		fail();
+    	}
     }
 
 
-    public void testParseEnumerationOfClassesAndSlotsInOWLDL() throws Exception {
-        ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_DL.getURI());
-        owlModel.createOWLNamedClass("Male");
-        owlModel.createOWLObjectProperty("hasChildren");
-        String expression = "{Male hasChildren}";
-        try {
-            parser.parseClass(owlModel, expression);
-            assertFalse("Expected to fail parsing!", true);
-        }
-        catch (Exception ex) {
-        }
+    public void testParseEnumerationOfClassesAndSlotsInOWLDL() {
+    	try {
+    		ProfilesManager.setProfile(owlModel, OWLProfiles.OWL_DL.getURI());
+    		owlModel.createOWLNamedClass("Male");
+    		owlModel.createOWLObjectProperty("hasChildren");
+    		String expression = "{Male hasChildren}";
+    		try {
+    			parser.parseClass(owlModel, expression);
+    			assertFalse("Expected to fail parsing!", true);
+    		}
+    		catch (Exception ex) {
+    		}
+    	}
+    	catch (Exception e) {
+    		fail();
+    	}
     }
 }
