cb8c4d0f709476c33ddbeb0a80006ae283a11ed9
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-04-30 23:38:53 +0000

    can add repositories which take effect before inputs

--- owl/trunk/src/edu/stanford/smi/protegex/owl/jena/creator/OwlProjectFromStreamCreator.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/jena/creator/OwlProjectFromStreamCreator.java
@@ -4,16 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.factory.AbstractOwlProjectCreator;
+import edu.stanford.smi.protegex.owl.repository.Repository;
 
 public class OwlProjectFromStreamCreator extends AbstractOwlProjectCreator {
     private InputStream stream;
     private String xmlBase;
+    private List<Repository> repositories = new ArrayList<Repository>();
+
 
     public OwlProjectFromStreamCreator() {
         this(new JenaKnowledgeBaseFactory());
@@ -28,6 +33,7 @@ public class OwlProjectFromStreamCreator extends AbstractOwlProjectCreator {
     public Project create(Collection errors) throws IOException {
         Project project = Project.createNewProject(factory, errors);
         OWLModel owlModel = (OWLModel) project.getKnowledgeBase();
+        insertRepositoriesIntoOwlModel(owlModel);
         
         ProtegeOWLParser parser = new ProtegeOWLParser(owlModel);
         try {
@@ -45,6 +51,18 @@ public class OwlProjectFromStreamCreator extends AbstractOwlProjectCreator {
 
     public void setXmlBase(String xmlBase) {
         this.xmlBase = xmlBase;
+    }
+    
+    public void addRepository(Repository repository) {
+    	repositories.add(repository);
+    }
+    
+    public void clearRepositories() {
+    	repositories.clear();
+    }
+    
+    public List<Repository> getRepositories() {
+    	return Collections.unmodifiableList(repositories);
     }
 
 }
