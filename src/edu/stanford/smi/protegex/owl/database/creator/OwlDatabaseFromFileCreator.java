a63b646a0de960f717fb39e233f92dc9447f8f8e
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-04-30 23:39:58 +0000

    can add repositories which take effect before the imports

--- src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
+++ src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
@@ -1,7 +1,10 @@
 package edu.stanford.smi.protegex.owl.database.creator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 import edu.stanford.smi.protege.model.Project;
@@ -12,9 +15,13 @@ import edu.stanford.smi.protegex.owl.database.OWLDatabaseKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.factory.AlreadyImportedException;
+import edu.stanford.smi.protegex.owl.repository.Repository;
 
 public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
     private static transient Logger log = Log.getLogger(OwlDatabaseFromFileCreator.class);
+    
+    private List<Repository> repositories = new ArrayList<Repository>();
+
     
     private String ontologySource;
     
@@ -32,7 +39,7 @@ public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
         initializeTable(errors);
         Project project = super.create(errors);
         OWLModel owlModel = (OWLModel) project.getKnowledgeBase();
-        
+        insertRepositoriesIntoOwlModel(owlModel);
         ProtegeOWLParser parser = new ProtegeOWLParser(owlModel);
         parser.run(URIUtilities.createURI(ontologySource));
 
@@ -57,6 +64,18 @@ public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
     
     public void setOntologySource(String uri) {
         ontologySource = uri;
+    }
+    
+    public void addRepository(Repository repository) {
+    	repositories.add(repository);
+    }
+    
+    public void clearRepositories() {
+    	repositories.clear();
+    }
+    
+    public List<Repository> getRepositories() {
+    	return Collections.unmodifiableList(repositories);
     }
 
 }
