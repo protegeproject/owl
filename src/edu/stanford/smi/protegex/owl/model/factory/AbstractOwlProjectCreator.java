c01b8dc8f1f006040fd77ef897b1832d60898df2
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-04-30 23:41:23 +0000

    infrastructure for repositories

--- src/edu/stanford/smi/protegex/owl/model/factory/AbstractOwlProjectCreator.java
+++ src/edu/stanford/smi/protegex/owl/model/factory/AbstractOwlProjectCreator.java
@@ -1,10 +1,16 @@
 package edu.stanford.smi.protegex.owl.model.factory;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.util.PropertyList;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.repository.Repository;
+import edu.stanford.smi.protegex.owl.repository.RepositoryManager;
 import edu.stanford.smi.protegex.owl.storage.OWLKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.ui.cls.SwitchClassDefinitionResourceDisplayPlugin;
 import edu.stanford.smi.protegex.owl.ui.profiles.ProfilesManager;
@@ -12,6 +18,7 @@ import edu.stanford.smi.protegex.owl.ui.profiles.ProfilesManager;
 public abstract class AbstractOwlProjectCreator {
     private Class defaultClassView;
     private String profileURI;
+
     protected OWLKnowledgeBaseFactory factory;
     
     public AbstractOwlProjectCreator(OWLKnowledgeBaseFactory factory) {
@@ -36,6 +43,13 @@ public abstract class AbstractOwlProjectCreator {
         }
     }
     
+    protected void insertRepositoriesIntoOwlModel(OWLModel owlModel) {
+    	RepositoryManager manager = owlModel.getRepositoryManager();
+        for (Repository  repository : getRepositories()) {
+        	manager.addProjectRepository(repository);
+        }
+    }
+    
     /*
      * ------------------------------------------------------
      * setters and getters
@@ -47,6 +61,10 @@ public abstract class AbstractOwlProjectCreator {
 
     public void setProfileURI(String profileURI) {
         this.profileURI = profileURI;
+    }
+    
+    public List<Repository> getRepositories() {
+    	return Collections.emptyList();
     }
     
 }
