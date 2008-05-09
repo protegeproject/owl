42fe6489d9ad89b2157cc2a9f442185b6d1203a8
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-05-09 16:34:28 +0000

    finally interned some of the constants

--- owl/trunk/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/model/OWLNames.java
@@ -1,7 +1,6 @@
 package edu.stanford.smi.protegex.owl.model;
 
 import edu.stanford.smi.protege.model.FrameID;
-import edu.stanford.smi.protege.model.Model;
 
 
 /**
@@ -19,133 +18,133 @@ public interface OWLNames {
 
     public static interface Cls {
 
-        public final static String ALL_DIFFERENT = OWL_NAMESPACE + "AllDifferent";
+        public final static String ALL_DIFFERENT = (OWL_NAMESPACE + "AllDifferent").intern();
 
-        public final static String ALL_VALUES_FROM_RESTRICTION = OWL_NAMESPACE + "AllValuesFromRestriction";
+        public final static String ALL_VALUES_FROM_RESTRICTION = (OWL_NAMESPACE + "AllValuesFromRestriction").intern();
 
-        public final static String ANNOTATION_PROPERTY = OWL_NAMESPACE + "AnnotationProperty";
+        public final static String ANNOTATION_PROPERTY = (OWL_NAMESPACE + "AnnotationProperty").intern();
 
-        public final static String ANONYMOUS_CLASS = OWL_NAMESPACE + "AnonymousClass";
+        public final static String ANONYMOUS_CLASS = (OWL_NAMESPACE + "AnonymousClass").intern();
 
-        public final static String ANONYMOUS_ROOT = ":OWL-ANONYMOUS-ROOT";
+        public final static String ANONYMOUS_ROOT = ":OWL-ANONYMOUS-ROOT".intern();
 
-        public final static String CARDINALITY_RESTRICTION = OWL_NAMESPACE + "CardinalityRestriction";
+        public final static String CARDINALITY_RESTRICTION = (OWL_NAMESPACE + "CardinalityRestriction").intern();
 
-        public final static String COMPLEMENT_CLASS = OWL_NAMESPACE + "ComplementClass";
+        public final static String COMPLEMENT_CLASS = (OWL_NAMESPACE + "ComplementClass").intern();
 
-        public final static String DATATYPE_PROPERTY = OWL_NAMESPACE + "DatatypeProperty";
+        public final static String DATATYPE_PROPERTY = (OWL_NAMESPACE + "DatatypeProperty").intern();
 
-        public final static String DATA_RANGE = OWL_NAMESPACE + "DataRange";
+        public final static String DATA_RANGE = (OWL_NAMESPACE + "DataRange").intern();
 
-        public final static String DEPRECATED_CLASS = OWL_NAMESPACE + "DeprecatedClass";
+        public final static String DEPRECATED_CLASS = (OWL_NAMESPACE + "DeprecatedClass").intern();
 
-        public final static String DEPRECATED_PROPERTY = OWL_NAMESPACE + "DeprecatedProperty";
+        public final static String DEPRECATED_PROPERTY = (OWL_NAMESPACE + "DeprecatedProperty").intern();
 
-        public final static String ENUMERATED_CLASS = OWL_NAMESPACE + "EnumeratedClass";
+        public final static String ENUMERATED_CLASS = (OWL_NAMESPACE + "EnumeratedClass").intern();
 
-        public final static String FUNCTIONAL_PROPERTY = OWL_NAMESPACE + "FunctionalProperty";
+        public final static String FUNCTIONAL_PROPERTY = (OWL_NAMESPACE + "FunctionalProperty").intern();
 
-        public final static String HAS_VALUE_RESTRICTION = OWL_NAMESPACE + "HasValueRestriction";
+        public final static String HAS_VALUE_RESTRICTION = (OWL_NAMESPACE + "HasValueRestriction").intern();
 
-        public final static String INTERSECTION_CLASS = OWL_NAMESPACE + "IntersectionClass";
+        public final static String INTERSECTION_CLASS = (OWL_NAMESPACE + "IntersectionClass").intern();
 
-        public final static String INVERSE_FUNCTIONAL_PROPERTY = OWL_NAMESPACE + "InverseFunctionalProperty";
+        public final static String INVERSE_FUNCTIONAL_PROPERTY = (OWL_NAMESPACE + "InverseFunctionalProperty").intern();
 
-        public final static String LOGICAL_CLASS = OWL_NAMESPACE + "LogicalClass";
+        public final static String LOGICAL_CLASS = (OWL_NAMESPACE + "LogicalClass").intern();
 
-        public final static String MAX_CARDINALITY_RESTRICTION = OWL_NAMESPACE + "MaxCardinalityRestriction";
+        public final static String MAX_CARDINALITY_RESTRICTION = (OWL_NAMESPACE + "MaxCardinalityRestriction").intern();
 
-        public final static String MIN_CARDINALITY_RESTRICTION = OWL_NAMESPACE + "MinCardinalityRestriction";
+        public final static String MIN_CARDINALITY_RESTRICTION = (OWL_NAMESPACE + "MinCardinalityRestriction").intern();
 
-        public final static String NAMED_CLASS = OWL_NAMESPACE + "Class";
+        public final static String NAMED_CLASS = (OWL_NAMESPACE + "Class").intern();
 
-        public final static String NOTHING = OWL_NAMESPACE + "Nothing";
+        public final static String NOTHING = (OWL_NAMESPACE + "Nothing").intern();
 
-        public final static String ONTOLOGY = OWL_NAMESPACE + "Ontology";
+        public final static String ONTOLOGY = (OWL_NAMESPACE + "Ontology").intern();
 
-        public final static String OBJECT_PROPERTY = OWL_NAMESPACE + "ObjectProperty";
+        public final static String OBJECT_PROPERTY = (OWL_NAMESPACE + "ObjectProperty").intern();
 
-        public final static String OWL_CLASS = ":OWL-CLASS";
+        public final static String OWL_CLASS = ":OWL-CLASS".intern();
 
-        public final static String RESTRICTION = OWL_NAMESPACE + "Restriction";
+        public final static String RESTRICTION = (OWL_NAMESPACE + "Restriction").intern();
 
-        public final static String SOME_VALUES_FROM_RESTRICTION = OWL_NAMESPACE + "SomeValuesFromRestriction";
+        public final static String SOME_VALUES_FROM_RESTRICTION = (OWL_NAMESPACE + "SomeValuesFromRestriction").intern();
 
-        public final static String SYMMETRIC_PROPERTY = OWL_NAMESPACE + "SymmetricProperty";
+        public final static String SYMMETRIC_PROPERTY = (OWL_NAMESPACE + "SymmetricProperty").intern();
 
-        public final static String THING = OWL_NAMESPACE + "Thing";
+        public final static String THING = (OWL_NAMESPACE + "Thing").intern();
 
-        public final static String TRANSITIVE_PROPERTY = OWL_NAMESPACE + "TransitiveProperty";
+        public final static String TRANSITIVE_PROPERTY = (OWL_NAMESPACE + "TransitiveProperty").intern();
 
-        public final static String UNION_CLASS = OWL_NAMESPACE + "UnionClass";
+        public final static String UNION_CLASS = (OWL_NAMESPACE + "UnionClass").intern();
         
-        public final static String OWL_ONTOLOGY_POINTER_CLASS = ":OWL-ONTOLOGY-POINTER-CLASS";
+        public final static String OWL_ONTOLOGY_POINTER_CLASS = ":OWL-ONTOLOGY-POINTER-CLASS".intern();
     }
 
     public static interface Slot {
 
-        public final static String ALL_VALUES_FROM = OWL_NAMESPACE + "allValuesFrom";
+        public final static String ALL_VALUES_FROM = (OWL_NAMESPACE + "allValuesFrom").intern();
 
-        public final static String BACKWARD_COMPATIBLE_WITH = OWL_NAMESPACE + "backwardCompatibleWith";
+        public final static String BACKWARD_COMPATIBLE_WITH = (OWL_NAMESPACE + "backwardCompatibleWith").intern();
 
-        public final static String CARDINALITY = OWL_NAMESPACE + "cardinality";
+        public final static String CARDINALITY = (OWL_NAMESPACE + "cardinality").intern();
 
-        public final static String COMPLEMENT_OF = OWL_NAMESPACE + "complementOf";
+        public final static String COMPLEMENT_OF = (OWL_NAMESPACE + "complementOf").intern();
 
-        public final static String DIFFERENT_FROM = OWL_NAMESPACE + "differentFrom";
+        public final static String DIFFERENT_FROM = (OWL_NAMESPACE + "differentFrom").intern();
 
-        public final static String DISJOINT_WITH = OWL_NAMESPACE + "disjointWith";
+        public final static String DISJOINT_WITH = (OWL_NAMESPACE + "disjointWith").intern();
 
-        public final static String DISTINCT_MEMBERS = OWL_NAMESPACE + "distinctMembers";
+        public final static String DISTINCT_MEMBERS = (OWL_NAMESPACE + "distinctMembers").intern();
 
-        public final static String EQUIVALENT_CLASS = OWL_NAMESPACE + "equivalentClass";
+        public final static String EQUIVALENT_CLASS = (OWL_NAMESPACE + "equivalentClass").intern();
 
-        public final static String EQUIVALENT_PROPERTY = OWL_NAMESPACE + "equivalentProperty";
+        public final static String EQUIVALENT_PROPERTY = (OWL_NAMESPACE + "equivalentProperty").intern();
 
         /**
          * @deprecated use EQUIVALENT_PROPERTY instead
          */
-        public final static String EQUIVALENT_PROPERTIES = OWL_NAMESPACE + "equivalentProperty";
+        public final static String EQUIVALENT_PROPERTIES = (OWL_NAMESPACE + "equivalentProperty").intern();
 
-        public final static String HAS_VALUE = OWL_NAMESPACE + "hasValue";
+        public final static String HAS_VALUE = (OWL_NAMESPACE + "hasValue").intern();
 
-        public final static String INCOMPATIBLE_WITH = OWL_NAMESPACE + "incompatibleWith";
+        public final static String INCOMPATIBLE_WITH = (OWL_NAMESPACE + "incompatibleWith").intern();
 
-        public final static String INTERSECTION_OF = OWL_NAMESPACE + "intersectionOf";
+        public final static String INTERSECTION_OF = (OWL_NAMESPACE + "intersectionOf").intern();
 
-        public final static String INVERSE_OF = OWL_NAMESPACE + "inverseOf";
+        public final static String INVERSE_OF = (OWL_NAMESPACE + "inverseOf").intern();
 
-        public final static String MAX_CARDINALITY = OWL_NAMESPACE + "maxCardinality";
+        public final static String MAX_CARDINALITY = (OWL_NAMESPACE + "maxCardinality").intern();
 
-        public final static String MIN_CARDINALITY = OWL_NAMESPACE + "minCardinality";
+        public final static String MIN_CARDINALITY = (OWL_NAMESPACE + "minCardinality").intern();
 
-        public final static String ON_PROPERTY = OWL_NAMESPACE + "onProperty";
+        public final static String ON_PROPERTY = (OWL_NAMESPACE + "onProperty").intern();
 
-        public final static String IMPORTS = OWL_NAMESPACE + "imports";
+        public final static String IMPORTS = (OWL_NAMESPACE + "imports").intern();
 
-        public final static String ONE_OF = OWL_NAMESPACE + "oneOf";
+        public final static String ONE_OF = (OWL_NAMESPACE + "oneOf").intern();
 
-        public final static String PRIOR_VERSION = OWL_NAMESPACE + "priorVersion";
+        public final static String PRIOR_VERSION = (OWL_NAMESPACE + "priorVersion").intern();
         
-        public final static String SAME_AS = OWL_NAMESPACE + "sameAs";
+        public final static String SAME_AS = (OWL_NAMESPACE + "sameAs").intern();
 
-        public final static String SOME_VALUES_FROM = OWL_NAMESPACE + "someValuesFrom";
+        public final static String SOME_VALUES_FROM = (OWL_NAMESPACE + "someValuesFrom").intern();
 
-        public final static String SUBCLASSES_DISJOINT = ProtegeNames.PREFIX + "subclassesDisjoint";
+        public final static String SUBCLASSES_DISJOINT = (ProtegeNames.PREFIX + "subclassesDisjoint").intern();
 
-        public final static String VALUES_FROM = OWL_NAMESPACE + "valuesFrom";
+        public final static String VALUES_FROM = (OWL_NAMESPACE + "valuesFrom").intern();
 
-        public final static String VERSION_INFO = OWL_NAMESPACE + "versionInfo";
+        public final static String VERSION_INFO = (OWL_NAMESPACE + "versionInfo").intern();
 
-        public final static String UNION_OF = OWL_NAMESPACE + "unionOf";
+        public final static String UNION_OF = (OWL_NAMESPACE + "unionOf").intern();
         
         //Non-OWL slots that are used internally by Protege
         
-        public final static String ONTOLOGY_PREFIXES = ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-ONTOLOGY-PREFIXES";
+        public final static String ONTOLOGY_PREFIXES = (ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-ONTOLOGY-PREFIXES").intern();
                       
-        public final static String RESOURCE_URI = ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-RESOURCE-URI";
+        public final static String RESOURCE_URI = (ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-RESOURCE-URI").intern();
         
-        public final static String OWL_ONTOLOGY_POINTER_PROPERTY = ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-ONTOLOGY-POINTER-PROPERTY";
+        public final static String OWL_ONTOLOGY_POINTER_PROPERTY = (ProtegeNames.PROTEGE_OWL_NAMESPACE + "OWL-ONTOLOGY-POINTER-PROPERTY").intern();
         
     }
 
--- owl/trunk/src/edu/stanford/smi/protegex/owl/model/XSDNames.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/model/XSDNames.java
@@ -11,55 +11,55 @@ public interface XSDNames {
     final static String XSD_NAMESPACE = "http://www.w3.org/2001/XMLSchema#";
 
 
-    final static String ANY_URI = XSD_NAMESPACE + "anyURI";
+    final static String ANY_URI = (XSD_NAMESPACE + "anyURI").intern();
 
-    final static String BASE_64_BINARY = XSD_NAMESPACE + "base64Binary";
+    final static String BASE_64_BINARY = (XSD_NAMESPACE + "base64Binary").intern();
 
-    final static String BOOLEAN = XSD_NAMESPACE + "boolean";
+    final static String BOOLEAN = (XSD_NAMESPACE + "boolean").intern();
 
-    final static String BYTE = XSD_NAMESPACE + "byte";
+    final static String BYTE = (XSD_NAMESPACE + "byte").intern();
 
-    final static String DATE = XSD_NAMESPACE + "date";
+    final static String DATE = (XSD_NAMESPACE + "date").intern();
 
-    final static String DATE_TIME = XSD_NAMESPACE + "dateTime";
+    final static String DATE_TIME = (XSD_NAMESPACE + "dateTime").intern();
 
-    final static String DECIMAL = XSD_NAMESPACE + "decimal";
+    final static String DECIMAL = (XSD_NAMESPACE + "decimal").intern();
 
-    final static String DOUBLE = XSD_NAMESPACE + "double";
+    final static String DOUBLE = (XSD_NAMESPACE + "double").intern();
 
-    final static String DURATION = XSD_NAMESPACE + "duration";
+    final static String DURATION = (XSD_NAMESPACE + "duration").intern();
 
-    final static String FLOAT = XSD_NAMESPACE + "float";
+    final static String FLOAT = (XSD_NAMESPACE + "float").intern();
 
-    final static String INT = XSD_NAMESPACE + "int";
+    final static String INT = (XSD_NAMESPACE + "int").intern();
 
-    final static String INTEGER = XSD_NAMESPACE + "integer";
+    final static String INTEGER = (XSD_NAMESPACE + "integer").intern();
 
-    final static String LONG = XSD_NAMESPACE + "long";
+    final static String LONG = (XSD_NAMESPACE + "long").intern();
 
-    final static String SHORT = XSD_NAMESPACE + "short";
+    final static String SHORT = (XSD_NAMESPACE + "short").intern();
 
-    final static String STRING = XSD_NAMESPACE + "string";
+    final static String STRING = (XSD_NAMESPACE + "string").intern();
 
-    final static String TIME = XSD_NAMESPACE + "time";
+    final static String TIME = (XSD_NAMESPACE + "time").intern();
 
 
     static interface Facet {
 
-        final static String LENGTH = XSD_NAMESPACE + "length";
+        final static String LENGTH = (XSD_NAMESPACE + "length").intern();
 
-        final static String MAX_EXCLUSIVE = XSD_NAMESPACE + "maxExclusive";
+        final static String MAX_EXCLUSIVE = (XSD_NAMESPACE + "maxExclusive").intern();
 
-        final static String MAX_INCLUSIVE = XSD_NAMESPACE + "maxInclusive";
+        final static String MAX_INCLUSIVE = (XSD_NAMESPACE + "maxInclusive").intern();
 
-        final static String MAX_LENGTH = XSD_NAMESPACE + "maxLength";
+        final static String MAX_LENGTH = (XSD_NAMESPACE + "maxLength").intern();
 
-        final static String MIN_EXCLUSIVE = XSD_NAMESPACE + "minExclusive";
+        final static String MIN_EXCLUSIVE = (XSD_NAMESPACE + "minExclusive").intern();
 
-        final static String MIN_INCLUSIVE = XSD_NAMESPACE + "minInclusive";
+        final static String MIN_INCLUSIVE = (XSD_NAMESPACE + "minInclusive").intern();
 
-        final static String MIN_LENGTH = XSD_NAMESPACE + "minLength";
+        final static String MIN_LENGTH = (XSD_NAMESPACE + "minLength").intern();
 
-        final static String PATTERN = XSD_NAMESPACE + "pattern";
+        final static String PATTERN = (XSD_NAMESPACE + "pattern").intern();
     }
 }
