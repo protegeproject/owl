a32c5da505f7d0c72c3cfe28a0e0433eca5de406
Tania Tudorache <tudorache@users.noreply.github.com>
2008-06-18 22:30:17 +0000

    fix for double post-processing

--- src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
+++ src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
@@ -8,28 +8,25 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import edu.stanford.smi.protege.exception.OntologyLoadException;
-import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.URIUtilities;
-import edu.stanford.smi.protegex.owl.database.DatabaseFactoryUtils;
 import edu.stanford.smi.protegex.owl.database.OWLDatabaseKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
-import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.factory.AlreadyImportedException;
 import edu.stanford.smi.protegex.owl.repository.Repository;
 
 public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
     private static transient Logger log = Log.getLogger(OwlDatabaseFromFileCreator.class);
-    
+
     private List<Repository> repositories = new ArrayList<Repository>();
 
-    
+
     private String ontologySource;
-    
+
     public OwlDatabaseFromFileCreator() {
         this(new OWLDatabaseKnowledgeBaseFactory());
     }
-    
+
     public OwlDatabaseFromFileCreator(OWLDatabaseKnowledgeBaseFactory factory) {
         super(factory);
     }
@@ -54,30 +51,29 @@ public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
         catch (AlreadyImportedException e) {
             throw new RuntimeException("This shouldn't happen", e);
         }
-        DatabaseFactoryUtils.loadImports(getOwlModel(), errors);
-        ProtegeOWLParser.doFinalPostProcessing(getOwlModel());
-        
+
         errors.addAll(ProtegeOWLParser.getErrors());
     }
-    
-    
+
+
     /*
      * setters and getters
      */
-    
+
     public void setOntologySource(String uri) {
         ontologySource = uri;
     }
-    
+
     public void addRepository(Repository repository) {
     	repositories.add(repository);
     }
-    
+
     public void clearRepositories() {
     	repositories.clear();
     }
-    
-    public List<Repository> getRepositories() {
+
+    @Override
+	public List<Repository> getRepositories() {
     	return Collections.unmodifiableList(repositories);
     }
 
