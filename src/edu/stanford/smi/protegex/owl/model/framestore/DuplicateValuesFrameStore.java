813d76cc51445e00793a9d20178b47c9061868e0
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-06-19 04:05:41 +0000

    System.err.println

--- src/edu/stanford/smi/protegex/owl/model/framestore/DuplicateValuesFrameStore.java
+++ src/edu/stanford/smi/protegex/owl/model/framestore/DuplicateValuesFrameStore.java
@@ -3,12 +3,15 @@ package edu.stanford.smi.protegex.owl.model.framestore;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.logging.Logger;
 
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.FrameStoreAdapter;
+import edu.stanford.smi.protege.util.Log;
 
 public class DuplicateValuesFrameStore extends FrameStoreAdapter {
+    private static final transient Logger log = Log.getLogger(DuplicateValuesFrameStore.class);
 
     /*
      * FrameStore implementation
@@ -20,11 +23,11 @@ public class DuplicateValuesFrameStore extends FrameStoreAdapter {
 
         if (valueCount > 1 &&
             valueCount != new HashSet(values).size()) {
-            System.err.println("[OWLFrameStore] Warning: Attempted to assign duplicate value to " +
+            log.warning("[OWLFrameStore] Warning: Attempted to assign duplicate value to " +
                                frame.getBrowserText() + "." + slot.getBrowserText());
             for (Iterator it = values.iterator(); it.hasNext();) {
                 Object o = it.next();
-                System.err.println("[OWLFrameStore]  - " + o);
+                log.warning("[OWLFrameStore]  - " + o);
             }
             values = new HashSet(values);
         }
