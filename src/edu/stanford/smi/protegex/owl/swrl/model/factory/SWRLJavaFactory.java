e66c63d9f2a70b87db5d079a65134fa29174609d
Tania Tudorache <tudorache@users.noreply.github.com>
2008-06-21 01:37:15 +0000

    fix to incorporate all swrl entities

--- src/edu/stanford/smi/protegex/owl/swrl/model/factory/SWRLJavaFactory.java
+++ src/edu/stanford/smi/protegex/owl/swrl/model/factory/SWRLJavaFactory.java
@@ -10,27 +10,12 @@ import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.DefaultSimpleInstance;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.FrameID;
-import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.SimpleInstance;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
-import edu.stanford.smi.protegex.owl.model.factory.FrameTypeId2OWLJavaClass;
 import edu.stanford.smi.protegex.owl.model.factory.OWLJavaFactory;
 import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
-import edu.stanford.smi.protegex.owl.swrl.model.SWRLIndividual;
-import edu.stanford.smi.protegex.owl.swrl.model.SWRLNames;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLAtomList;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLBuiltin;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLBuiltinAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLClassAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLDataRangeAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLDatavaluedPropertyAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLDifferentIndividualsAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLImp;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLIndividualPropertyAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLSameIndividualAtom;
-import edu.stanford.smi.protegex.owl.swrl.model.impl.DefaultSWRLVariable;
 
 public class SWRLJavaFactory extends OWLJavaFactory {
 
@@ -39,35 +24,27 @@ public class SWRLJavaFactory extends OWLJavaFactory {
     /**
      * A Hashtable from Protege classes to Java classes from this package
      */
-    private HashMap<String, Class<? extends SWRLIndividual>> classMap = new HashMap<String, Class<? extends SWRLIndividual>>();
+    private HashMap<String, Class<? extends SimpleInstance>> classMap = new HashMap<String, Class<? extends SimpleInstance>>();
 
     public SWRLJavaFactory(OWLModel owlModel) {
         super((AbstractOWLModel) owlModel);
         this.owlModel = owlModel;
 
-        classMap.put(SWRLNames.Cls.ATOM_LIST, DefaultSWRLAtomList.class);
-        classMap.put(SWRLNames.Cls.BUILTIN_ATOM, DefaultSWRLBuiltinAtom.class);
-        classMap.put(SWRLNames.Cls.BUILTIN, DefaultSWRLBuiltin.class);
-        classMap.put(SWRLNames.Cls.CLASS_ATOM, DefaultSWRLClassAtom.class);
-        classMap.put(SWRLNames.Cls.DATA_RANGE_ATOM, DefaultSWRLDataRangeAtom.class);
-        classMap.put(SWRLNames.Cls.DATAVALUED_PROPERTY_ATOM, DefaultSWRLDatavaluedPropertyAtom.class);
-        classMap.put(SWRLNames.Cls.DIFFERENT_INDIVIDUALS_ATOM, DefaultSWRLDifferentIndividualsAtom.class);
-        classMap.put(SWRLNames.Cls.IMP, DefaultSWRLImp.class);
-        classMap.put(SWRLNames.Cls.INDIVIDUAL_PROPERTY_ATOM, DefaultSWRLIndividualPropertyAtom.class);
-        classMap.put(SWRLNames.Cls.SAME_INDIVIDUAL_ATOM, DefaultSWRLSameIndividualAtom.class);
-        classMap.put(SWRLNames.Cls.VARIABLE, DefaultSWRLVariable.class);
+        for (SWRLFrameTypeId swrlType : SWRLFrameTypeId.values()) {
+        	classMap.put(swrlType.getProtegeName(), swrlType.getJavaClass());
+        }
     } // SWRLJavaFactory
 
 
     @Override
     public SimpleInstance createSimpleInstance(FrameID id, Collection directTypes) {
-        Constructor<? extends SWRLIndividual> constructor;
+        Constructor<? extends SimpleInstance> constructor;
         Object[] args = {owlModel, id};
 
 
         if (directTypes.size() == 1) {
             Cls typeCls = (Cls) directTypes.iterator().next();
-            Class<? extends SWRLIndividual> c = getJavaTypeFromSWRLType(typeCls);
+            Class<? extends SimpleInstance> c = getJavaTypeFromSWRLType(typeCls);
             if (c != null) {
                 try {
                     constructor = c.getConstructor(new Class[]{
@@ -90,9 +67,9 @@ public class SWRLJavaFactory extends OWLJavaFactory {
     } // createSimpleInstance
 
 
-    private Class<? extends SWRLIndividual> getJavaTypeFromSWRLType(Cls type) {
+    private Class<? extends SimpleInstance> getJavaTypeFromSWRLType(Cls type) {
         String typeClsName = type.getName();
-        Class<? extends SWRLIndividual> c = classMap.get(typeClsName);
+        Class<? extends SimpleInstance> c = classMap.get(typeClsName);
         if (c == null) {
             Iterator supers = type.getSuperclasses().iterator();
             while (c == null && supers.hasNext()) {
@@ -102,8 +79,8 @@ public class SWRLJavaFactory extends OWLJavaFactory {
         }
         return c;
     }
-    
-    
+
+
     @Override
     public boolean isCorrectJavaImplementationClass(FrameID id, Collection types, Class clas) {
         for (Object o : types) {
@@ -115,10 +92,10 @@ public class SWRLJavaFactory extends OWLJavaFactory {
         }
         return super.isCorrectJavaImplementationClass(id, types, clas);
     }
-    
+
     @Override
     public Frame createFrameFromClassId(int javaClassId, FrameID id) {
- 
+
         if (javaClassId >= SWRLFrameTypeId.SWRL_FRAME_TYPE_ID_BASE &&
                 javaClassId <= SWRLFrameTypeId.SWRL_FRAME_TYPE_ID_END) {
             int index = javaClassId - SWRLFrameTypeId.SWRL_FRAME_TYPE_ID_BASE;
@@ -130,12 +107,12 @@ public class SWRLJavaFactory extends OWLJavaFactory {
         }
         return super.createFrameFromClassId(javaClassId, id);
     }
-    
+
     @Override
-    public int getJavaClassId(Frame frame) {    
+    public int getJavaClassId(Frame frame) {
         for (SWRLFrameTypeId frameTypeId : SWRLFrameTypeId.values()) {
             try {
-                Class<? extends Instance> javaType = frameTypeId.getJavaClass();
+                Class<? extends SimpleInstance> javaType = frameTypeId.getJavaClass();
                 if (javaType.isInstance(frame)) {
                     return frameTypeId.getFrameTypeId();
                 }
@@ -146,7 +123,7 @@ public class SWRLJavaFactory extends OWLJavaFactory {
         }
 
         return super.getJavaClassId(frame);
-    
+
     }
 
 } // SWRLJavaFactory
