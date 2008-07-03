dd19b9e32bbbe666525a2c199a11addfb70939c6
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-07-03 23:17:11 +0000

    added a complete set of junits for the creators - do we want to expand this

---
+++ owl/trunk/junit/edu/stanford/smi/protegex/owl/database/creator/DbCreatorTestCase.java
@@ -0,0 +1,97 @@
+package edu.stanford.smi.protegex.owl.database.creator;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import edu.stanford.smi.protege.exception.OntologyLoadException;
+import edu.stanford.smi.protege.test.APITestCase;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.jena.creator.notont.AbstractCreatorTestCase;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.repository.impl.LocalFolderRepository;
+
+
+public class DbCreatorTestCase extends AbstractCreatorTestCase {
+    private Logger log = Log.getLogger(DbCreatorTestCase.class);
+
+    public void testOwlDatabaseCreator() throws OntologyLoadException {
+        for (DBType dbt : DBType.values()) {
+            APITestCase.setDBType(dbt);
+            if (!APITestCase.dbConfigured()) {
+                continue;
+            }
+            OwlDatabaseCreator creator = new OwlDatabaseCreator(true);
+            configureDbCreator(creator);
+            creator.setTable("JunitImported");
+            creator.create(errors);
+            handleErrors();
+            OWLModel owlModel = creator.getOwlModel();
+            
+            String name = owlModel.getDefaultOWLOntology().getName();
+            checkBase(owlModel, name, name);
+        }
+    }
+    
+    public void testOwlDatabaseFromFileCreatorBaseline() throws OntologyLoadException {
+        for (DBType dbt : DBType.values()) {
+            APITestCase.setDBType(dbt);
+            if (!APITestCase.dbConfigured()) {
+                continue;
+            }
+            OwlDatabaseFromFileCreator creator = new OwlDatabaseFromFileCreator();
+            configureDbCreator(creator);
+            creator.setTable("JunitImporting");
+            creator.setOntologySource(new File(IMPORTING_LOCATION).toURI().toString());
+            creator.create(errors);
+            handleErrors();
+            OWLModel owlModel = creator.getOwlModel();
+            
+            checkUnsucessfulImport(owlModel);
+            checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+        }
+    }
+    
+    public void testOwlDatabaseFromFileCreator01() throws OntologyLoadException {
+        for (DBType dbt : DBType.values()) {
+            APITestCase.setDBType(dbt);
+            if (!APITestCase.dbConfigured()) {
+                continue;
+            }
+            OwlDatabaseFromFileCreator creator = new OwlDatabaseFromFileCreator();
+            configureDbCreator(creator);
+            creator.setTable("JunitImporting");
+            creator.setOntologySource(new File(IMPORTING_LOCATION).toURI().toString());
+            creator.addRepository(new LocalFolderRepository(new File(DIRECTORY)));
+            creator.create(errors);
+            handleErrors();
+            OWLModel owlModel = creator.getOwlModel();
+            
+            checkSuccessfulImport(owlModel);
+            checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+        }
+    }
+    
+    public void testOwlDatabaseFromFileCreator02() throws OntologyLoadException, SQLException, ClassNotFoundException {
+        for (DBType dbt : DBType.values()) {
+            APITestCase.setDBType(dbt);
+            if (!APITestCase.dbConfigured()) {
+                continue;
+            }
+            createImportedDbProject();
+            OwlDatabaseFromFileCreator creator = new OwlDatabaseFromFileCreator();
+            configureDbCreator(creator);
+            creator.setTable("JunitImporting");
+            creator.setOntologySource(new File(IMPORTING_LOCATION).toURI().toString());
+            creator.addRepository(getDbRepository());
+            creator.create(errors);
+            handleErrors();
+            OWLModel owlModel = creator.getOwlModel();
+            
+            checkSuccessfulImport(owlModel);
+            checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+            checkIsDatabaseModel(owlModel, IMPORTING_BASE);
+            checkIsDatabaseModel(owlModel, IMPORTED_BASE);
+        }
+    }
+}
--- owl/trunk/junit/edu/stanford/smi/protegex/owl/jena/creator/notont/CreatorTestCase.java
+++ owl/trunk/junit/edu/stanford/smi/protegex/owl/jena/creator/notont/CreatorTestCase.java
@@ -3,27 +3,41 @@ package edu.stanford.smi.protegex.owl.jena.creator.notont;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import junit.framework.TestCase;
 import edu.stanford.smi.protege.exception.OntologyLoadException;
+import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
+import edu.stanford.smi.protege.storage.database.DatabaseFrameDb;
+import edu.stanford.smi.protege.test.APITestCase;
+import edu.stanford.smi.protege.test.APITestCase.DBType;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.database.creator.OwlDatabaseFromFileCreator;
+import edu.stanford.smi.protegex.owl.jena.creator.OwlProjectFromReaderCreator;
 import edu.stanford.smi.protegex.owl.jena.creator.OwlProjectFromStreamCreator;
 import edu.stanford.smi.protegex.owl.jena.creator.OwlProjectFromUriCreator;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.triplestore.TripleStore;
+import edu.stanford.smi.protegex.owl.repository.impl.DatabaseRepository;
 import edu.stanford.smi.protegex.owl.repository.impl.LocalFolderRepository;
 
 
-public class CreatorTestCase extends TestCase {
+public class CreatorTestCase extends AbstractCreatorTestCase {
     private Logger log = Log.getLogger(CreatorTestCase.class);
+    private EnumSet<DBType> initializedDb = EnumSet.noneOf(DBType.class);
     
     public static String DIRECTORY = "junit/projects/creator";
+    
     public static String IMPORTING_LOCATION = "junit/projects/creator/Importing.owl";
     public static String BAD_LOCATION = "junit/projects/creator/NoSuchOntology.owl";
+    public static String IMPORTED_LOCATION = "junit/projects/creator/Imported.owl";
+    
     public static String IMPORTING_BASE = "http://www.tigraworld.com/protege/Importing.owl";
     public static String IMPORTED_BASE  = "http://www.tigraworld.com/protege/Imported.owl";
     
@@ -55,6 +69,44 @@ public class CreatorTestCase extends TestCase {
         checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
     }
     
+    public void testUriCreatorRepositoryImportsDb() 
+    throws OntologyLoadException, SQLException, ClassNotFoundException {
+        for (DBType dbt : DBType.values()) {
+            APITestCase.setDBType(dbt);
+            if (!APITestCase.dbConfigured()) {
+                continue;
+            }
+            createImportedDbProject();
+            
+            File ontology = new File(IMPORTING_LOCATION);
+            OwlProjectFromUriCreator creator = new OwlProjectFromUriCreator();
+            creator.setOntologyUri(ontology.toURI().toString());
+
+            creator.addRepository(getDbRepository());
+            creator.create(errors);
+            handleErrors();
+            OWLModel owlModel = creator.getOwlModel();
+
+            checkSuccessfulImport(owlModel);
+            checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+            
+            checkIsDatabaseModel(owlModel, IMPORTED_BASE);
+        }
+    }
+
+    public void testStreamCreatorRepositoryBaseline() throws OntologyLoadException, FileNotFoundException {
+        FileInputStream in = new FileInputStream(new File(IMPORTING_LOCATION));
+        OwlProjectFromStreamCreator creator = new OwlProjectFromStreamCreator();
+        creator.setStream(in);
+        creator.setXmlBase(IMPORTING_BASE);
+        creator.create(errors);
+        handleErrors();
+        OWLModel owlModel = creator.getOwlModel();
+        
+        checkUnsucessfulImport(owlModel);
+        checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+    }
+    
     public void testStreamCreatorRepository01() throws OntologyLoadException, FileNotFoundException {
         FileInputStream in = new FileInputStream(new File(IMPORTING_LOCATION));
         OwlProjectFromStreamCreator creator = new OwlProjectFromStreamCreator();
@@ -83,10 +135,38 @@ public class CreatorTestCase extends TestCase {
         checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
     }
     
-    public void testStreamCreatorRepositoryBaseline() throws OntologyLoadException, FileNotFoundException {
-        FileInputStream in = new FileInputStream(new File(IMPORTING_LOCATION));
-        OwlProjectFromStreamCreator creator = new OwlProjectFromStreamCreator();
-        creator.setStream(in);
+    public void testReaderCreatorRepository01() throws OntologyLoadException, FileNotFoundException {
+        FileReader in = new FileReader(new File(IMPORTING_LOCATION));
+        OwlProjectFromReaderCreator creator = new OwlProjectFromReaderCreator();
+        creator.setReader(in);
+        creator.setXmlBase(new File(BAD_LOCATION).toURI().toString());
+        creator.addRepository(new LocalFolderRepository(new File(DIRECTORY)));
+        creator.create(errors);
+        handleErrors();
+        OWLModel owlModel = creator.getOwlModel();
+        
+        checkSuccessfulImport(owlModel);
+        checkBase(owlModel, IMPORTING_BASE, new File(BAD_LOCATION).toURI().toString());
+    }
+    
+    public void testReaderCreatorRepository02() throws OntologyLoadException, FileNotFoundException {
+        FileReader in = new FileReader(new File(IMPORTING_LOCATION));
+        OwlProjectFromReaderCreator creator = new OwlProjectFromReaderCreator();
+        creator.setReader(in);
+        creator.setXmlBase(IMPORTING_BASE);
+        creator.addRepository(new LocalFolderRepository(new File(DIRECTORY)));
+        creator.create(errors);
+        handleErrors();
+        OWLModel owlModel = creator.getOwlModel();
+        
+        checkSuccessfulImport(owlModel);
+        checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
+    }
+    
+    public void testReaderCreatorRepositoryBaseline() throws OntologyLoadException, FileNotFoundException {
+        FileReader in = new FileReader(new File(IMPORTING_LOCATION));
+        OwlProjectFromReaderCreator creator = new OwlProjectFromReaderCreator();
+        creator.setReader(in);
         creator.setXmlBase(IMPORTING_BASE);
         creator.create(errors);
         handleErrors();
@@ -96,48 +176,6 @@ public class CreatorTestCase extends TestCase {
         checkBase(owlModel, IMPORTING_BASE, IMPORTING_BASE);
     }
     
-    private void checkSuccessfulImport(OWLModel owlModel) {
-        assertNotNull(owlModel.getOWLNamedClass("A"));
-        assertNotNull(owlModel.getOWLNamedClass("imported:X"));
-        
-        assertEquals(1, owlModel.getDefaultOWLOntology().getImports().size());
-        assertEquals(IMPORTED_BASE, owlModel.getDefaultOWLOntology().getImports().iterator().next());
-        assertEquals(3, owlModel.getTripleStoreModel().getTripleStores().size());
-        assertNotNull(owlModel.getTripleStoreModel().getTripleStore(IMPORTED_BASE));
-    }
-    
-    private void checkUnsucessfulImport(OWLModel owlModel) {
-        assertNotNull(owlModel.getOWLNamedClass("A"));
-        assertNull(owlModel.getOWLNamedClass("imported:X"));
-        
-        assertEquals(1, owlModel.getDefaultOWLOntology().getImports().size());
-        assertEquals(IMPORTED_BASE, owlModel.getDefaultOWLOntology().getImports().iterator().next());
-        assertEquals(2, owlModel.getTripleStoreModel().getTripleStores().size());
-        assertNull(owlModel.getTripleStoreModel().getTripleStore(IMPORTED_BASE));
-    }
-    
-    private void checkBase(OWLModel owlModel, String expectedNamespace, String xmlBase) {
-        TripleStore topTripleStore = owlModel.getTripleStoreModel().getTopTripleStore();
-        assertEquals(expectedNamespace + "#", owlModel.getNamespaceManager().getDefaultNamespace()); 
-        assertEquals(expectedNamespace + "#", topTripleStore.getNamespaceManager().getDefaultNamespace());
-        assertEquals(xmlBase, topTripleStore.getOriginalXMLBase());
-        assertEquals(expectedNamespace, owlModel.getDefaultOWLOntology().getName());
-    }
-    
-    private void handleErrors() {
-        if (errors.isEmpty()) {
-            return;
-        }
-        for (Object o : errors) {
-            if (o instanceof Throwable) {
-                log.log(Level.SEVERE, "exception caught", ((Throwable) o));
-            }
-            else {
-                log.severe("Error = " + o);
-            }
-        }
-        errors.clear();
-        fail();
-    }
+ 
     
 }
