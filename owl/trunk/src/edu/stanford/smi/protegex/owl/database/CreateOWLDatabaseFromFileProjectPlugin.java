d66f58839c971aef4cc6ca3d774dc6cfb71c63ef
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-08-02 20:18:10 +0000

    added option for merge import mode

--- owl/trunk/src/edu/stanford/smi/protegex/owl/database/CreateOWLDatabaseFromFileProjectPlugin.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/database/CreateOWLDatabaseFromFileProjectPlugin.java
@@ -8,6 +8,7 @@ import edu.stanford.smi.protege.exception.OntologyLoadException;
 import edu.stanford.smi.protege.model.KnowledgeBaseFactory;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.plugin.CreateProjectWizard;
+import edu.stanford.smi.protege.util.ApplicationProperties;
 import edu.stanford.smi.protege.util.URIUtilities;
 import edu.stanford.smi.protege.util.WizardPage;
 import edu.stanford.smi.protegex.owl.database.creator.OwlDatabaseFromFileCreator;
@@ -17,15 +18,18 @@ import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
  * @author Holger Knublauch  <holger@knublauch.com>
  */
 public class CreateOWLDatabaseFromFileProjectPlugin extends CreateOWLDatabaseProjectPlugin {
-    
-    protected URI ontologyInputSource;
 
-    public CreateOWLDatabaseFromFileProjectPlugin() {
+	private static String MERGE_MODE_PROP = "protege.owl.parser.convert.db.merge.mode";
+
+    protected URI ontologyInputSource;
+    private boolean isMergeImportMode = ApplicationProperties.getBooleanProperty(MERGE_MODE_PROP, false);
+
+	public CreateOWLDatabaseFromFileProjectPlugin() {
         super("OWL File (.owl or .rdf)");
     }
 
-
-    @SuppressWarnings("unchecked")
+    @Override
+	@SuppressWarnings("unchecked")
     protected Project buildNewProject(KnowledgeBaseFactory factory) {
         JenaKnowledgeBaseFactory.useStandalone = false;
         Collection errors = new ArrayList();
@@ -36,6 +40,7 @@ public class CreateOWLDatabaseFromFileProjectPlugin extends CreateOWLDatabasePro
         creator.setUsername(getUsername());
         creator.setPassword(getPassword());
         creator.setOntologySource(getOntologyInputSource().toString());
+        creator.setMergeImportMode(isMergeImportMode);
 
         try {
             creator.create(errors);
@@ -48,36 +53,46 @@ public class CreateOWLDatabaseFromFileProjectPlugin extends CreateOWLDatabasePro
     }
 
 
-    public boolean canCreateProject(KnowledgeBaseFactory factory, boolean useExistingSources) {
+    @Override
+	public boolean canCreateProject(KnowledgeBaseFactory factory, boolean useExistingSources) {
         return super.canCreateProject(factory, useExistingSources) && useExistingSources;
     }
 
 
-    public WizardPage createCreateProjectWizardPage(CreateProjectWizard wizard, boolean useExistingSources) {
+    @Override
+	public WizardPage createCreateProjectWizardPage(CreateProjectWizard wizard, boolean useExistingSources) {
         return new InitOWLDatabaseFromFileWizardPage(wizard, this);
     }
-    
-    
+
+
 
     public void setOntologyInputSource(URI uri) {
         this.ontologyInputSource = uri;
     }
-    
-    
+
+
     /**
      * @deprecated Use {@link #setOntologyInputSource(URI)}
      */
     @Deprecated
     public void setOntologyFileURI(String uri) {
-    	URI uritemp = URIUtilities.createURI(uri); 
+    	URI uritemp = URIUtilities.createURI(uri);
     	if (uritemp == null) {
     		throw new RuntimeException("Invalid uri " + uri);
     	}
     	setOntologyInputSource(uritemp);
     }
-    
+
     public URI getOntologyInputSource() {
         return ontologyInputSource;
     }
+
+    public boolean isMergeImportMode() {
+		return isMergeImportMode;
+	}
+
+	public void setMergeImportMode(boolean isMergeImportMode) {
+		this.isMergeImportMode = isMergeImportMode;
+	}
 
 }
