0b793ea7924af979ce26c1429be6c3c5ec249243
Tania Tudorache <tudorache@users.noreply.github.com>
2008-08-06 19:08:37 +0000

    added support for the mergin imports mode

--- src/edu/stanford/smi/protegex/owl/jena/creator/OwlProjectFromUriCreator.java
+++ src/edu/stanford/smi/protegex/owl/jena/creator/OwlProjectFromUriCreator.java
@@ -1,6 +1,5 @@
 package edu.stanford.smi.protegex.owl.jena.creator;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,47 +10,61 @@ import com.hp.hpl.jena.util.FileUtils;
 
 import edu.stanford.smi.protege.exception.OntologyLoadException;
 import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.util.ApplicationProperties;
 import edu.stanford.smi.protege.util.FileUtilities;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.PropertyList;
 import edu.stanford.smi.protegex.owl.jena.JenaKnowledgeBaseFactory;
 import edu.stanford.smi.protegex.owl.jena.JenaOWLModel;
-import edu.stanford.smi.protegex.owl.jena.parser.ProtegeOWLParser;
 import edu.stanford.smi.protegex.owl.model.factory.AbstractOwlProjectCreator;
 import edu.stanford.smi.protegex.owl.repository.Repository;
 
 public class OwlProjectFromUriCreator extends AbstractOwlProjectCreator {
 
-    private String ontologyUri;
+	private static String MERGE_MODE_PROP = "protege.owl.parser.convert.file.merge.mode";
+
+	private boolean isMergeImportMode = ApplicationProperties.getBooleanProperty(MERGE_MODE_PROP, false);
+
+	private String ontologyUri;
 
     private String lang = FileUtils.langXMLAbbrev;
-    
+
     private Project project;
-    
+
     private List<Repository> repositories = new ArrayList<Repository>();
 
     public  OwlProjectFromUriCreator() {
         this(new JenaKnowledgeBaseFactory());
     }
-    
+
     public OwlProjectFromUriCreator(JenaKnowledgeBaseFactory factory) {
         super(factory);
     }
 
-    
-    @SuppressWarnings("unchecked")
-    public void create(Collection errors) throws OntologyLoadException {
-        project = Project.createBuildProject(factory, errors);
 
-        initializeSources(project.getSources());
-        URI uri = getBuildProjectURI();
-        if (uri != null) {
-            project.setProjectURI(uri);
-        }
-        project.createDomainKnowledgeBase(factory, errors, true);
+    @Override
+	@SuppressWarnings("unchecked")
+    public void create(Collection errors) throws OntologyLoadException {
+
+    	boolean initialMergeMode = JenaKnowledgeBaseFactory.isMergingImportMode();
+
+    	try {
+    		JenaKnowledgeBaseFactory.setMergingImportMode(isMergeImportMode);
+
+    		project = Project.createBuildProject(factory, errors);
+
+    		initializeSources(project.getSources());
+    		URI uri = getBuildProjectURI();
+    		if (uri != null) {
+    			project.setProjectURI(uri);
+    		}
+    		project.createDomainKnowledgeBase(factory, errors, true);
+    	} finally {
+    		JenaKnowledgeBaseFactory.setMergingImportMode(initialMergeMode);
+    	}
     }
-    
-    
+
+
     @Override
     public JenaOWLModel getOwlModel() {
         if (project != null) {
@@ -61,13 +74,13 @@ public class OwlProjectFromUriCreator extends AbstractOwlProjectCreator {
             return null;
         }
     }
-    
+
     @Override
     public Project getProject() {
         return project;
     }
-    
-    
+
+
     protected void initializeSources(PropertyList sources) {
         JenaKnowledgeBaseFactory.setOWLFileName(sources, ontologyUri);
         JenaKnowledgeBaseFactory.setOWLFileLanguage(sources, lang);
@@ -76,7 +89,7 @@ public class OwlProjectFromUriCreator extends AbstractOwlProjectCreator {
         }
         addViewSettings(sources);
     }
-    
+
     protected URI getBuildProjectURI() {
         if (ontologyUri != null) {
             if (ontologyUri.startsWith("file:")) {
@@ -94,7 +107,7 @@ public class OwlProjectFromUriCreator extends AbstractOwlProjectCreator {
         }
         return null;
     }
-    
+
     /*
      * ------------------------------------------------------
      * setters and getters
@@ -103,27 +116,37 @@ public class OwlProjectFromUriCreator extends AbstractOwlProjectCreator {
     public void setOntologyUri(String fileURI) {
         this.ontologyUri = fileURI;
     }
-   
+
 
     public void setLang(String lang) {
         this.lang = lang;
     }
-    
-    public JenaKnowledgeBaseFactory getFactory() {
+
+    @Override
+	public JenaKnowledgeBaseFactory getFactory() {
         return (JenaKnowledgeBaseFactory) super.getFactory();
     }
 
     public void addRepository(Repository repository) {
         repositories.add(repository);
     }
-    
+
     public void clearRepositories() {
         repositories.clear();
     }
-    
-    public Collection<Repository> getRepositories() {
+
+    @Override
+	public Collection<Repository> getRepositories() {
         return Collections.unmodifiableCollection(repositories);
     }
 
+
+    public boolean isMergeImportMode() {
+		return isMergeImportMode;
+	}
+
+	public void setMergeImportMode(boolean isMergeImportMode) {
+		this.isMergeImportMode = isMergeImportMode;
+	}
 
 }
