7f66589f1d6e26fe5e125a1e9f00575f9adf07e8
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-19 00:01:07 +0000

    He has his own id generator with a different id.

--- src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtil.java
+++ src/edu/stanford/smi/protegex/owl/jena/parser/ParserUtil.java
@@ -1,5 +1,7 @@
 package edu.stanford.smi.protegex.owl.jena.parser;
 
+import java.util.UUID;
+
 import com.hp.hpl.jena.rdf.arp.AResource;
 
 import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
@@ -9,6 +11,8 @@ import edu.stanford.smi.protege.model.framestore.SimpleFrameStore;
 import edu.stanford.smi.protegex.owl.model.impl.AbstractOWLModel;
 
 public class ParserUtil {
+    
+    public static final String PARSERS_UNIQUE_SESSION_ID = UUID.randomUUID().toString().replace("-", "_");
 
 
 	public static String getResourceName(AResource resource) {
@@ -19,8 +23,9 @@ public class ParserUtil {
 		     *          TripleFrameCache code.  This is *nasty* but I don't yet the better way.
 		     */
 			StringBuffer buffer = new StringBuffer(AbstractOWLModel.ANONYMOUS_BASE);
-			buffer.append("_");
 			buffer.append(resource.getAnonymousID());
+			buffer.append("_");
+			buffer.append(PARSERS_UNIQUE_SESSION_ID);
 			return buffer.toString();
 		} else {
 			return resource.getURI();
