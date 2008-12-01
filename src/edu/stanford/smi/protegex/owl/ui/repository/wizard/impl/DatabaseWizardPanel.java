8e3028d6ec26c12775e8c6134543e824256057c5
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-12-01 19:40:49 +0000

    fixed some problems with excessive error reporting and logging during repository setup.

--- src/edu/stanford/smi/protegex/owl/ui/repository/wizard/impl/DatabaseWizardPanel.java
+++ src/edu/stanford/smi/protegex/owl/ui/repository/wizard/impl/DatabaseWizardPanel.java
@@ -4,17 +4,19 @@
 package edu.stanford.smi.protegex.owl.ui.repository.wizard.impl;
 
 import java.awt.GridLayout;
-import java.sql.SQLException;
 import java.util.EnumMap;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+import edu.stanford.smi.protege.storage.database.AbstractDatabaseFrameDb;
 import edu.stanford.smi.protege.storage.database.DatabaseProperty;
 import edu.stanford.smi.protege.util.ComponentFactory;
 import edu.stanford.smi.protege.util.LabeledComponent;
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.util.WizardPage;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.repository.Repository;
@@ -23,6 +25,8 @@ import edu.stanford.smi.protegex.owl.ui.repository.wizard.RepositoryCreatorWizar
 
 public class DatabaseWizardPanel extends RepositoryCreatorWizardPanel {
     private static final long serialVersionUID = 8313995336416582467L;
+    
+    private static Logger log = Log.getLogger(DatabaseWizardPanel.class);
     
     private EnumMap<DatabaseProperty, JTextField> textMap 
             = new EnumMap<DatabaseProperty, JTextField>(DatabaseProperty.class);
@@ -52,31 +56,26 @@ public class DatabaseWizardPanel extends RepositoryCreatorWizardPanel {
 
     @Override
     public Repository createRepository() {
+        Level loggingLevel = AbstractDatabaseFrameDb.log.getLevel();
+        AbstractDatabaseFrameDb.log.setLevel(Level.WARNING);
         try {
             return new DatabaseRepository(getText(DatabaseProperty.DRIVER_PROPERTY),
                                           getText(DatabaseProperty.URL_PROPERTY),
                                           getText(DatabaseProperty.USERNAME_PROPERTY),
                                           getText(DatabaseProperty.PASSWORD_PROPERTY));
         }
-        catch (SQLException e) {
-            if (DatabaseRepositoryCreatorWizardPlugin.log.isLoggable(Level.FINE))  {
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("driver = "  + getText(DatabaseProperty.DRIVER_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("url = " + getText(DatabaseProperty.URL_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("username = " + getText(DatabaseProperty.USERNAME_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("password = " + getText(DatabaseProperty.PASSWORD_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.log(Level.FINE, "Create Repository failed", e);
-            }
-            return null;
-        }
-        catch (ClassNotFoundException e) {
-            if (DatabaseRepositoryCreatorWizardPlugin.log.isLoggable(Level.FINE))  {
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("driver = "  + getText(DatabaseProperty.DRIVER_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("url = " + getText(DatabaseProperty.URL_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("username = " + getText(DatabaseProperty.USERNAME_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.fine("password = " + getText(DatabaseProperty.PASSWORD_PROPERTY));
-                DatabaseRepositoryCreatorWizardPlugin.log.log(Level.FINE, "Create Repository failed", e);
+        catch (Throwable t) {
+            if (log.isLoggable(Level.FINE))  {
+                log.fine("driver = "  + getText(DatabaseProperty.DRIVER_PROPERTY));
+                log.fine("url = " + getText(DatabaseProperty.URL_PROPERTY));
+                log.fine("username = " + getText(DatabaseProperty.USERNAME_PROPERTY));
+                log.fine("password = " + getText(DatabaseProperty.PASSWORD_PROPERTY));
+                log.log(Level.FINE, "Create Repository failed", t);
             }
             return null; 
+        }
+        finally {
+            AbstractDatabaseFrameDb.log.setLevel(loggingLevel);
         }
     }
     
