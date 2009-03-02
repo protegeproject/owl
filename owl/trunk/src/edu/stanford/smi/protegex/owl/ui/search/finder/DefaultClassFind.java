49fc2edf77938f173d14acab85b0d8ace21d7d50
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-03-02 19:19:43 +0000

    changed the condition of whether a thing is a class to instanceof RDFSNamedClass

--- owl/trunk/src/edu/stanford/smi/protegex/owl/ui/search/finder/DefaultClassFind.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/ui/search/finder/DefaultClassFind.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protegex.owl.ui.search.finder;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
 
 /**
  * @author Nick Drummond, Medical Informatics Group, University of Manchester
@@ -15,7 +16,7 @@ public class DefaultClassFind extends ResultsViewModelFind {
     }
 
     protected boolean isValidFrameToSearch(Frame f) {
-        return f instanceof OWLNamedClass && f.isVisible();
+        return f instanceof RDFSNamedClass && f.isVisible();
     }
 
     public String getDescription() {
