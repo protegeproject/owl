7e684b9fa86a4dda4e264b41595ada1efa9e8762
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-03-03 20:32:11 +0000

    policy for setting active import

---
+++ src/edu/stanford/smi/protegex/owl/server/metaproject/OwlMetaProjectConstants.java
@@ -0,0 +1,9 @@
+package edu.stanford.smi.protegex.owl.server.metaproject;
+
+import edu.stanford.smi.protege.server.metaproject.Operation;
+import edu.stanford.smi.protege.server.metaproject.impl.UnbackedOperationImpl;
+
+public class OwlMetaProjectConstants {
+    public final static Operation SET_ACTIVE_IMPORT = new  UnbackedOperationImpl("SetActiveImport", null);
+
+}
