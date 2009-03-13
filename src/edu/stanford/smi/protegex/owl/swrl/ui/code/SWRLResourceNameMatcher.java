2d9b938b1aa8c1d1af20f7bdaadaabe36d2ba62b
Tania Tudorache <tudorache@users.noreply.github.com>
2009-03-13 22:49:32 +0000

    moving the filtering of swrl rules to the superclass

--- src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLResourceNameMatcher.java
+++ src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLResourceNameMatcher.java
@@ -3,12 +3,10 @@ package edu.stanford.smi.protegex.owl.swrl.ui.code;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
-import edu.stanford.smi.protegex.owl.swrl.model.SWRLFactory;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLNames;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLVariable;
 import edu.stanford.smi.protegex.owl.ui.code.OWLResourceNameMatcher;
@@ -58,27 +56,5 @@ public class SWRLResourceNameMatcher extends OWLResourceNameMatcher
 	protected boolean couldBeDatatype(OWLModel owlModel, String prefix) {
 		return true;
 	}
-
-	@Override
-	protected void removeFilteredElements(Set<RDFResource> frames,
-			List<Class<? extends RDFResource>> classes) {
-		Set<RDFResource> toRemove = new HashSet<RDFResource>();
-		for (RDFResource resource  : frames) {        	
-			boolean remove = true;
-			if (!resource.isAnonymous() && !SWRLFactory.isSWRLResource(resource)) {
-				for (Class<? extends RDFResource> clazz : classes) {
-					if (clazz.isAssignableFrom(resource.getClass()) && isVisible(resource)) {
-						remove = false;
-						break;
-					}
-				}        	
-			}
-			if (remove) {
-				toRemove.add(resource);
-			}
-		}
-		frames.removeAll(toRemove);
-	}
-
 
 } // SWRLResourceNameMatcher
