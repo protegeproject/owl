54c89ee87204e47147e970968ac9c3abc141306f
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-03-13 22:20:28 +0000

    java 5

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLResourceNameMatcher.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/ui/code/SWRLResourceNameMatcher.java
@@ -10,18 +10,19 @@ import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLNames;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLVariable;
 import edu.stanford.smi.protegex.owl.ui.code.OWLResourceNameMatcher;
-import edu.stanford.smi.protegex.owl.ui.code.SymbolTextField;
 
 public class SWRLResourceNameMatcher extends OWLResourceNameMatcher
 {
 
-  public String getInsertString(RDFResource resource) 
+  @Override
+public String getInsertString(RDFResource resource) 
   {
     if (resource instanceof SWRLVariable) return "?" + resource.getPrefixedName();
     else return super.getInsertString(resource);
   } // getInsertString
 
-  public Set<RDFResource> getMatchingResources(String prefix, String leftString, OWLModel owlModel) 
+  @Override
+public Set<RDFResource> getMatchingResources(String prefix, String leftString, OWLModel owlModel) 
   {
       if (leftString.endsWith("?")) {
           Set<RDFResource> resources = new HashSet<RDFResource>();
