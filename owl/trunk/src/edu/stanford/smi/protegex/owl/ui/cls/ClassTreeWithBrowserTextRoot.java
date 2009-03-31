43e6c8529e7da77d5841d71f22d05dd44903062e
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-03-31 22:54:45 +0000

    filter roots of tree to visible roots merged from NCI 1.4 13615

--- owl/trunk/src/edu/stanford/smi/protegex/owl/ui/cls/ClassTreeWithBrowserTextRoot.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/ui/cls/ClassTreeWithBrowserTextRoot.java
@@ -1,17 +1,20 @@
 package edu.stanford.smi.protegex.owl.ui.cls;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
 
 import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.util.CollectionUtilities;
 import edu.stanford.smi.protege.util.FrameWithBrowserText;
 import edu.stanford.smi.protege.util.LazyTreeNode;
 import edu.stanford.smi.protege.util.LazyTreeRoot;
 
 public class ClassTreeWithBrowserTextRoot extends LazyTreeRoot {
 
-    public ClassTreeWithBrowserTextRoot(Collection roots, boolean isSorted) {
-        super(roots, isSorted);
+    public ClassTreeWithBrowserTextRoot(Collection<? extends Cls> roots, boolean isSorted) {
+        super(FrameWithBrowserText.getFramesWithBrowserText(filter(roots)), isSorted);
     }
     
     public ClassTreeWithBrowserTextRoot(Cls rootCls, boolean isSorted) {
@@ -27,6 +30,21 @@ public class ClassTreeWithBrowserTextRoot extends LazyTreeRoot {
     protected Comparator getComparator() {
         // TODO Auto-generated method stub
         return null;
-    }   
+    }  
+    
+    private static Collection filter(Collection roots) {
+        Collection visibleRoots = new ArrayList(roots);
+        Cls firstRoot = (Cls) CollectionUtilities.getFirstItem(roots);
+        if (firstRoot != null && !firstRoot.getProject().getDisplayHiddenClasses()) {
+            Iterator i = visibleRoots.iterator();
+            while (i.hasNext()) {
+                Cls cls = (Cls) i.next();
+                if (!cls.isVisible()) {
+                    i.remove();
+                }
+            }
+        }
+        return visibleRoots;
+    }
 
 }
