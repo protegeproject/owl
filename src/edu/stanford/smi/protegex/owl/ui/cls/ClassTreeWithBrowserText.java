f1492103e7bce33bb6227a8eae3aa82c557926fa
Tania Tudorache <tudorache@users.noreply.github.com>
2009-04-17 19:07:42 +0000

    - added methods for selection - set the renderer

--- src/edu/stanford/smi/protegex/owl/ui/cls/ClassTreeWithBrowserText.java
+++ src/edu/stanford/smi/protegex/owl/ui/cls/ClassTreeWithBrowserText.java
@@ -1,21 +1,32 @@
 package edu.stanford.smi.protegex.owl.ui.cls;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.TreeSet;
 
 import javax.swing.Action;
+import javax.swing.JTree;
+import javax.swing.tree.TreePath;
 
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.ModelUtilities;
+import edu.stanford.smi.protege.util.CollectionUtilities;
 import edu.stanford.smi.protege.util.ComponentUtilities;
 import edu.stanford.smi.protege.util.FrameWithBrowserText;
 import edu.stanford.smi.protege.util.LazyTreeRoot;
+import edu.stanford.smi.protege.util.SelectableTree;
+import edu.stanford.smi.protege.util.WaitCursor;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.ui.ResourceRendererWithBrowserText;
+import edu.stanford.smi.protegex.owl.ui.widget.OWLUI;
 
 public class ClassTreeWithBrowserText extends ClassTree {
     private static final long serialVersionUID = -841459836070720483L;
     
     public ClassTreeWithBrowserText(Action doubleClickAction, LazyTreeRoot root) {
         super(doubleClickAction, root);
+        setCellRenderer(new ResourceRendererWithBrowserText());
     }
 
     /*
@@ -36,5 +47,44 @@ public class ClassTreeWithBrowserText extends ClassTree {
         }
         return selectedClses;
     }
+    
+    
+    public void setSelectedCls(Cls cls) {
+    	 if (!getSelection().contains(cls) && !cls.isDeleted()) {
+    		 SelectableTree tree = this;
+             if (cls instanceof RDFResource) {                 
+                 if (tree instanceof ClassTreeWithBrowserText) {
+                     Collection path = (Collection) CollectionUtilities.getFirstItem(OWLUI.getPathsToRoot((RDFResource) cls));
+                     Collection<FrameWithBrowserText> fbtPath = new ArrayList<FrameWithBrowserText>();
+                     for (Object object : path) {
+                         if (object instanceof Frame) {
+                             fbtPath.add(new FrameWithBrowserText((Frame)object));
+                         }
+                     }
+                     setSelectedObjectPath(tree, fbtPath);              
+                 } else {
+                     OWLUI.setSelectedNodeInTree(tree, (RDFResource) cls);
+                 }
+             }
+             else {
+                 Collection path = ModelUtilities.getPathToRoot(cls);
+                 setSelectedObjectPath(tree, path);
+             }
+         }
+    }
+    
+    protected void setSelectedObjectPath(final JTree tree, Collection objectPath) {    	
+        final TreePath path = ComponentUtilities.getTreePath(tree, objectPath);        
+        
+        if (path != null) {
+        	final WaitCursor cursor = new WaitCursor(tree);       	
+            tree.scrollPathToVisible(path);          
+            tree.setSelectionPath(path);
+            cursor.hide();
+            tree.updateUI();
+        }
+    }
+
+    
 
 }
