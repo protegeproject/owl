ef9c83c097aa7958968259577c7cdd3df3b482f8
Tania Tudorache <tudorache@users.noreply.github.com>
2009-11-11 21:50:13 +0000

    sorting the subclasses for the extraction

--- src/edu/stanford/smi/protegex/owl/ui/cls/ExtractTaxonomyAction.java
+++ src/edu/stanford/smi/protegex/owl/ui/cls/ExtractTaxonomyAction.java
@@ -5,8 +5,10 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -14,6 +16,7 @@ import javax.swing.JFileChooser;
 
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.resource.Icons;
+import edu.stanford.smi.protege.ui.FrameComparator;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
@@ -21,7 +24,7 @@ import edu.stanford.smi.protegex.owl.ui.ProtegeUI;
 import edu.stanford.smi.protegex.owl.ui.actions.ResourceAction;
 
 /**
- * @author Holger Knublauch  <holger@knublauch.com>
+ * @author Holger Knublauch <holger@knublauch.com>
  */
 public class ExtractTaxonomyAction extends ResourceAction {
 
@@ -29,11 +32,9 @@ public class ExtractTaxonomyAction extends ResourceAction {
 
     public static final String GROUP = "Extract/";
 
-
     public ExtractTaxonomyAction() {
         super("Extract (sub) taxonomy to text file...", Icons.getBlankIcon(), GROUP);
     }
-
 
     public void actionPerformed(ActionEvent e) {
 
@@ -49,8 +50,7 @@ public class ExtractTaxonomyAction extends ResourceAction {
                 fw.close();
                 ProtegeUI.getModalDialogFactory().showMessageDialog(getOWLModel(),
                         "Taxonomy has been exported to " + file);
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 Log.getLogger().log(Level.SEVERE, "Exception caught", ex);
                 ProtegeUI.getModalDialogFactory().showErrorMessageDialog(getOWLModel(),
                         "File could not be exported:\n" + ex);
@@ -58,29 +58,28 @@ public class ExtractTaxonomyAction extends ResourceAction {
         }
     }
 
-
+    @Override
     public boolean isSuitable(Component component, RDFResource resource) {
         return component instanceof OWLSubclassPane && resource instanceof RDFSNamedClass;
     }
-
 
     public static void performAction(RDFSNamedClass rootClass, PrintWriter pw) {
         Set reached = new HashSet();
         performAction(rootClass, reached, "", pw);
     }
 
-
     public static void performAction(Cls cls, Set reached, String baseStr, PrintWriter pw) {
         reached.add(cls);
         pw.println(baseStr + cls.getBrowserText());
         baseStr += "\t";
-        for (Iterator it = cls.getDirectSubclasses().iterator(); it.hasNext();) {
-            Cls subCls = (Cls) it.next();
+        List<Cls> directSubclasses = new ArrayList<Cls>(cls.getDirectSubclasses());
+        Collections.sort(directSubclasses, new FrameComparator());
+        for (Object element : directSubclasses) {
+            Cls subCls = (Cls) element;
             if (subCls instanceof RDFSNamedClass && subCls.isVisible()) {
                 if (reached.contains(subCls)) {
                     pw.println(baseStr + "(" + subCls.getBrowserText() + ")...");
-                }
-                else {
+                } else {
                     performAction(subCls, reached, baseStr, pw);
                 }
             }
