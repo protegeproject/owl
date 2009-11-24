646155228507bd566599a0ef30288dae82aeeef3
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-11-24 02:45:21 +0000

    fixed javadoc warnings

--- owl/trunk/src/edu/stanford/smi/protegex/owl/testing/style/SingleAssertedSuperclassTest.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/testing/style/SingleAssertedSuperclassTest.java
@@ -10,41 +10,31 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Normalisation rule - classes should only have a single asserted named superclass
- * (ie be a pure tree)
+ * Normalisation rule - classes should only have a single asserted named superclass, i.e., be a pure tree
  *
- * @TODO Provide an untangling wizard to allow users to fix this
- *
- * @author Nick Drummond, Medical Informatics Group, University of Manchester
- *         06-Feb-2006
+ * @author Nick Drummond, Medical Informatics Group, University of Manchester, 06-Feb-2006
+ * 
  */
-public class SingleAssertedSuperclassTest extends AbstractOWLTest
-    implements RDFSClassTest {
+public class SingleAssertedSuperclassTest extends AbstractOWLTest implements RDFSClassTest {
 
     public String getDocumentation() {
         return "Checks that named classes only have one asserted parent";
     }
 
-
     public String getGroup() {
         return "Style";
     }
-
 
     public String getName() {
         return "Normalisation: Single Asserted Superclass";
     }
 
-
-    public List test(RDFSClass aClass) {
-        if (aClass.getNamedSuperclasses().size() > 1){
-            return Collections.singletonList(
-                    new DefaultOWLTestResult("This class has multiple asserted parents",
-                                             aClass,
-                                             OWLTestResult.TYPE_WARNING,
-                                             this)
-            );
-        }
-        return Collections.EMPTY_LIST;
-    }
+	public List test(RDFSClass aClass) {
+		if (aClass.getNamedSuperclasses().size() > 1) {
+			return Collections.singletonList(new DefaultOWLTestResult(
+					"This class has multiple asserted parents", aClass,
+					OWLTestResult.TYPE_WARNING, this));
+		}
+		return Collections.EMPTY_LIST;
+	}
 }
