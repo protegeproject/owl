facf224cef79a21ff7efeb5426c36edf68ae729d
Martin O'Connor <martinjoconnor@users.noreply.github.com>
2010-07-21 21:39:40 +0000

    More refactoring for P4 port - no additional functionality.

--- src/edu/stanford/smi/protegex/owl/swrl/bridge/SWRLRuleEngineBridgeController.java
+++ src/edu/stanford/smi/protegex/owl/swrl/bridge/SWRLRuleEngineBridgeController.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import edu.stanford.smi.protegex.owl.swrl.owlapi.OWLAxiom;
 import edu.stanford.smi.protegex.owl.swrl.owlapi.OWLNamedIndividual;
 
+/**
+ * This interface provides access methods to retrieve knowledge inferred by a rule engine.
+ */
 public interface SWRLRuleEngineBridgeController 
 {
   int getNumberOfInferredOWLIndividuals();
