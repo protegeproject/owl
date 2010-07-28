116ff467012437565e89d89712408c4f331612dc
moconnor <moconnor@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-07-28 12:46:09 +0000

    Added temporal:ExtendedProposition as temporal built-in argument. Some clean up.

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/owlapi/OWLObjectPropertyAssertionAxiom.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/owlapi/OWLObjectPropertyAssertionAxiom.java
@@ -4,4 +4,4 @@ package edu.stanford.smi.protegex.owl.swrl.owlapi;
 public interface OWLObjectPropertyAssertionAxiom extends OWLPropertyAssertionAxiom
 {
   OWLNamedIndividual getObject();
-} // OWLObjectPropertyAssertionAxiom
+}
