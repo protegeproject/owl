79035fa16c2accd984370cd5fb8628c573292000
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-10-25 22:32:25 +0000

    change to the error handling in the ProtegeOWLParser

--- owl/trunk/src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/database/creator/OwlDatabaseFromFileCreator.java
@@ -66,7 +66,7 @@ public class OwlDatabaseFromFileCreator extends AbstractOwlDatabaseCreator {
         }
         FactoryUtils.adjustBrowserTextBasedOnPreferences(getOwlModel());
 
-        errors.addAll(ProtegeOWLParser.getErrors());
+        errors.addAll(getOwlModel().getParserErrors());
     }
 
 
