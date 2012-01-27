c1e545303ef7967648637363d7a15006682d5e66
Martin O'Connor <martinjoconnor@users.noreply.github.com>
2012-01-27 19:09:05 +0000

    initial SWRLAPI extracted version

--- src/edu/stanford/smi/protegex/owl/swrl/sqwrl/impl/DataPropertyValueImpl.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.impl;
-
-import edu.stanford.smi.protegex.owl.swrl.sqwrl.DataPropertyValue;
-
-public class DataPropertyValueImpl extends PropertyValueImpl implements DataPropertyValue
-{
-  public DataPropertyValueImpl(String propertyURI) { super(propertyURI); }
-}
