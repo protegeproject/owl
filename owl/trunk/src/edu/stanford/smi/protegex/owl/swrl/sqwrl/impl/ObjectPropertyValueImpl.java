21855a5544ff3c888905144ab020885390a9a36e
moconnor <moconnor@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-01-27 19:09:05 +0000

    initial SWRLAPI extracted version

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/sqwrl/impl/ObjectPropertyValueImpl.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.impl;
-
-import edu.stanford.smi.protegex.owl.swrl.sqwrl.ObjectPropertyValue;
-
-public class ObjectPropertyValueImpl extends PropertyValueImpl implements ObjectPropertyValue
-{
-  public ObjectPropertyValueImpl(String propertyURI) { super(propertyURI); }
-}
