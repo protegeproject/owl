c6e5967c6931582b5304e40abd39893eb264edc1
Martin O'Connor <martinjoconnor@users.noreply.github.com>
2012-01-27 19:09:33 +0000

    initial SWRLAPI extracted version

--- src/edu/stanford/smi/protegex/owl/swrl/sqwrl/exceptions/InvalidRowIndexException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.exceptions;
-
-public class InvalidRowIndexException extends SQWRLException
-{
-  public InvalidRowIndexException(String message) { super(message); }
-
-  public InvalidRowIndexException(String message, Throwable cause) { super(message, cause); }
-} // InvalidRowIndexException
--- src/edu/stanford/smi/protegex/owl/swrl/sqwrl/exceptions/SQWRLException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.exceptions;
-
-public class SQWRLException extends ResultException
-{
-  public SQWRLException(String message) { super(message); }
-
-  public SQWRLException(String message, Throwable cause) { super(message, cause); }
-}
