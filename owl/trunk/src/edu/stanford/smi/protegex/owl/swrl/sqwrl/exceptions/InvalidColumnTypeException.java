e0e09284d11c63c0a312b08a12da5d9d4d4f3bb5
moconnor <moconnor@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-01-27 19:09:33 +0000

    initial SWRLAPI extracted version

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/sqwrl/exceptions/InvalidColumnTypeException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.exceptions;
-
-public class InvalidColumnTypeException extends SQWRLException
-{
-  public InvalidColumnTypeException(String message) { super(message); }
-
-  public InvalidColumnTypeException(String message, Throwable cause) { super(message, cause); }
-} // InvalidColumnTypeException
--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/sqwrl/exceptions/ResultStateException.java
+++
@@ -1,9 +0,0 @@
-
-package edu.stanford.smi.protegex.owl.swrl.sqwrl.exceptions;
-
-public class ResultStateException extends SQWRLException
-{
-  public ResultStateException(String message) { super(message); }
-
-  public ResultStateException(String message, Throwable cause) { super(message, cause); }
-} // ResultStateException
