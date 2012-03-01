8ecbc85be2d36a1567bfa4b0998c9c0ee6114227
moconnor <moconnor@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-03-01 23:57:28 +0000

    Fixed SQWRL default rule engine selection.

--- owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/ui/P3SWRLTabPlugin.java
+++ owl/trunk/src/edu/stanford/smi/protegex/owl/swrl/ui/P3SWRLTabPlugin.java
@@ -8,12 +8,12 @@ import javax.swing.Icon;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 
 /**
+ * This interface must thus be implemented by a SWRLTab plugin to create a Java Component that represents the plugin's GUI. Thus GUI will be activated in the
+ * SWRLTab the provide a means of interacting with the plugin.
+ * <p>
  * Each plugin should register itself with the bridge using the registerPlugin method of the P3SWRLTabPluginManager class. In addition to a plugin display name,
  * a tool tip string, and a set of icons, this method is expecting an instance of a class that implements this interface. The plugin manager uses this
  * implementation to get the GUI for the plugin, which will be displayed in the SWRLTab when the plugin is activated.
- * <p>
- * This interface must thus be implemented by a SWRLTab plugin to create a Java Component that represents the plugin's GUI. Thus GUI will be activated in the
- * SWRLTab the provide a means of interacting with the plugin.
  * <p>
  * The createSWRLPluginGUI method is called once when the plugin is registered and is expecting a Java Swing component that is a subclass of a Java AWT
  * Container class. The getSWRLPluginGUI may be called repeatedly after registration and should return the Container instance created on initialization.
