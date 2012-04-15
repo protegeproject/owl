bbd33afe2e082e6af579986cdceaaac25cec4bd7
Martin O'Connor <martinjoconnor@users.noreply.github.com>
2012-04-15 01:05:25 +0000

    Cleaned up P4 extractors and converters

--- src/edu/stanford/smi/protegex/owl/swrl/parser/SWRLIncompleteRuleException.java
+++ src/edu/stanford/smi/protegex/owl/swrl/parser/SWRLIncompleteRuleException.java
@@ -1,15 +1,13 @@
 package edu.stanford.smi.protegex.owl.swrl.parser;
 
-/**
- * @author Holger Knublauch  <holger@knublauch.com>
- */
-public class SWRLIncompleteRuleException extends SWRLParseException {
+public class SWRLIncompleteRuleException extends SWRLParseException
+{
+	public SWRLIncompleteRuleException()
+	{
+	}
 
-    public SWRLIncompleteRuleException() {
-    }
-
-
-    public SWRLIncompleteRuleException(String message) {
-        super(message);
-    }
+	public SWRLIncompleteRuleException(String message)
+	{
+		super(message);
+	}
 }
